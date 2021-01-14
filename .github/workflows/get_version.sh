700545fdeff39eca46defa5a0969eb4ad53d9e8c
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-01-14 10:42:25 -0800

    added workflow files for generating a build.

---
+++ .github/workflows/get_version.sh
@@ -0,0 +1,4 @@
+VERSION=`./gradlew properties -q | grep "version:" | awk '{ print $2}'`
+WARFILE="cwms_radar_tomcat/build/libs/cwms_radar_tomcat-$VERSION.war"
+echo "::set-output name=VERSION::$VERSION"
+echo "::set-output name=WAR_FILE::$WARFILE"
