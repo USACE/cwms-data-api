a3aee456774e1bec89c3562f635339dfa7041781
ryan <ryan@rmanet.com>
2021-08-03 16:51:38 -0700

    Using AV_POOL view instead of pl/sql call and now doing pagination based on offset instead of fetching all the result from the pl/sql

--- cwms_radar_api/src/main/java/cwms/radar/data/dao/PoolDao.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/PoolDao.java
@@ -2,29 +2,28 @@ package cwms.radar.data.dao;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import cwms.radar.data.dto.Pool;
 import cwms.radar.data.dto.Pools;
 import org.jetbrains.annotations.NotNull;
+import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
 import org.jooq.exception.TooManyRowsException;
+import org.jooq.impl.DSL;
 
 import usace.cwms.db.dao.ifc.pool.PoolNameType;
 import usace.cwms.db.dao.ifc.pool.PoolType;
 import usace.cwms.db.dao.util.OracleTypeMap;
 import usace.cwms.db.jooq.codegen.packages.CWMS_POOL_PACKAGE;
 import usace.cwms.db.jooq.codegen.packages.cwms_pool.RETRIEVE_POOL;
-import usace.cwms.db.jooq.dao.util.PoolTypeUtil;
+import usace.cwms.db.jooq.codegen.tables.AV_POOL;
 
-import static java.util.Comparator.nullsLast;
 import static java.util.stream.Collectors.toList;
 
 public class PoolDao extends JooqDao<PoolType>
@@ -36,25 +35,71 @@ public class PoolDao extends JooqDao<PoolType>
 		super(dsl);
 	}
 
-	public List<Pool> catalogPools(String projectIdMask, String poolNameMask,
-									   String bottomLevelMask, String topLevelMask, boolean includeExplicit,
-									   boolean includeImplicit, String officeIdMask)
-	{
-		List<Pool> output = new ArrayList<>();
 
+	public List<Pool> catalogPools(String projectIdMask, String poolNameMask,
+								   String bottomLevelMask, String topLevelMask, boolean includeExplicit,
+								   boolean includeImplicit, String officeIdMask)
+	{
+		AV_POOL view = AV_POOL.AV_POOL;
+
+		List<String> types = getTypes(includeExplicit, includeImplicit);
+		Condition condition = getCondition(projectIdMask, poolNameMask, bottomLevelMask, topLevelMask, officeIdMask, types);
+
+		return dsl.select(DSL.asterisk()).from(view)
+				.where(condition)
+				.orderBy(view.DEFINITION_TYPE,
+					view.OFFICE_ID.upper(), view.PROJECT_ID.upper(), view.ATTRIBUTE, view.POOL_NAME.upper())
+				.stream()
+				.map(r -> toPool(r, true))
+				.collect(toList());
+	}
+
+	@NotNull
+	private List<String> getTypes(boolean includeExplicit, boolean includeImplicit)
+	{
+		List<String> types = new ArrayList<>();
 		if (includeExplicit)
 		{
-			output.addAll(catalogPoolsInternal( true, false, projectIdMask, poolNameMask, bottomLevelMask, topLevelMask, officeIdMask));
+			types.add("EXPLICIT");
 		}
 
-		//CWMS_POOL does not differentiate between implicit or explicit.
-		if (includeImplicit)
+		if(includeImplicit)
 		{
-			output.addAll(catalogPoolsInternal( false, true, projectIdMask, poolNameMask, bottomLevelMask, topLevelMask, officeIdMask));
+			types.add("IMPLICIT");
+		}
+		return types;
+	}
+
+
+
+	private Condition getCondition(String projectIdMask, String poolNameMask, String bottomLevelMask,
+								   String topLevelMask, String officeIdMask, List<String> types)
+	{
+		AV_POOL view = AV_POOL.AV_POOL;
+		Condition condition = view.DEFINITION_TYPE.in(types);
+
+		if(projectIdMask != null){
+			condition = condition.and(view.PROJECT_ID.like(projectIdMask));
 		}
 
-		return output;
+		if(poolNameMask != null){
+			condition = condition.and(view.POOL_NAME.like(poolNameMask));
+		}
+
+		if(bottomLevelMask != null){
+			condition = condition.and(view.BOTTOM_LEVEL.like(bottomLevelMask));
+		}
+
+		if(topLevelMask != null){
+			condition = condition.and(view.TOP_LEVEL.like(topLevelMask));
+		}
+
+		if(officeIdMask != null){
+			condition = condition.and(view.OFFICE_ID.like(officeIdMask));
+		}
+		return condition;
 	}
+
 
 	private List<Pool> catalogPoolsInternal( boolean includeExplicit, boolean includeImplicit,
 												String projectIdMask, String poolNameMask, String bottomLevelMask,
@@ -88,25 +133,31 @@ public class PoolDao extends JooqDao<PoolType>
 		String clobText = catRecord.get("CLOB_TEXT", String.class);
 
 		PoolNameType name = new PoolNameType(poolId, officeId);
-		Pool pool = new Pool(name, projectId, bottomLevelId, topLevelId, isImplicit);
-		pool.setAttribute(attribute);
-		pool.setDescription(description);
-
-		pool.setClobText(clobText);
-		return pool;
+		Pool.Builder b = Pool.Builder.newInstance();
+		b.withPoolName(name);
+		b.withProjectId(projectId);
+		b.withBottomLevelId(bottomLevelId);
+		b.withTopLevelId(topLevelId);
+		b.withImplicit(isImplicit);
+		b.withAttribute(attribute);
+		b.withDescription( description);
+		b.withClobText(clobText);
+		return b.build();
 	}
 
 
 	public Pool retrievePool( String projectId, String poolName, String officeId)
 	{
-		Pool retval = null;
 		RETRIEVE_POOL pool = CWMS_POOL_PACKAGE.call_RETRIEVE_POOL(dsl.configuration(), projectId, poolName,	officeId);
 
-		PoolType poolType = PoolTypeUtil.toPoolType(pool, projectId, poolName, officeId, false);
-		if(poolType != null){
-			retval = new Pool(poolType);
-		}
-		return retval;
+		Pool.Builder b = Pool.Builder.newInstance();
+		b.withPoolName(new PoolNameType(poolName, officeId));
+		b.withProjectId(projectId);
+		b.withBottomLevelId(pool.getP_BOTTOM_LEVEL_ID());
+		b.withTopLevelId(pool.getP_TOP_LEVEL_ID());
+		b.withImplicit(false);
+
+		return b.build();
 	}
 
 	public Pool retrievePoolFromCatalog(String projectId, String poolName,
@@ -126,7 +177,7 @@ public class PoolDao extends JooqDao<PoolType>
 						"PoolController.getOne is expected to be called with arguments that return a single unique pool. " +
 								"Arguments:[projectId:%s poolId:%s, bottomMask:%s, topMask:%s, implicit:%b, " +
 								"explicit:%b, office:%s] returned the following %d pools:%s",
-						projectId, poolName, bottomMask, topMask, includeImplicit, includeExplicit, officeIdMask, pools.size(), pools.toString()));
+						projectId, poolName, bottomMask, topMask, includeImplicit, includeExplicit, officeIdMask, pools.size(), pools));
 			}
 		}
 		return pool;
@@ -136,52 +187,43 @@ public class PoolDao extends JooqDao<PoolType>
 							   String projectIdMask, String poolNameMask,
 							   String bottomLevelMask, String topLevelMask, boolean includeExplicit,
 							   boolean includeImplicit, String officeIdMask){
-		int total = 0;
+		Integer total = null;
+		int offset = 0;
 
-		final Pool lastItem;
+		AV_POOL view = AV_POOL.AV_POOL;
 
-		List<Pool> pools = catalogPools(projectIdMask, poolNameMask, bottomLevelMask, topLevelMask, includeExplicit, includeImplicit, officeIdMask);
-
-		if( cursor == null || cursor.isEmpty() ){
-			total = pools.size();
-			lastItem = null;
-		} else {
+		if(cursor != null && !cursor.isEmpty())
+		{
 			String[] parts = Pools.decodeCursor(cursor);
 
 			logger.info("decoded cursor: " + Arrays.toString(parts));
 
 			if(parts.length > 2) {
-				String itemPortion = parts[0];
-				lastItem = Pools.decodeItem(itemPortion);
-				total = Integer.parseInt(parts[1]);
-				pageSize = Integer.parseInt(parts[2]);
-			} else {
-				lastItem = null;
+				offset = Integer.parseInt(parts[0]);
+				if(!"null".equals(parts[1])){
+					try {
+						total = Integer.valueOf(parts[1]);
+					} catch(NumberFormatException e){
+						logger.log(Level.INFO, "Could not parse " + parts[1]);
+					}
+				}
+				pageSize = Integer.parseInt(parts[2]); // Why are we taking pageSize as an arg and also pulling it from cursor?
 			}
 		}
 
-		// Apart from the initial isImplicit, the rest are the way the pl/sql already sorts.
-		//
-		Comparator<Pool> comparator = Comparator.comparing((Function<Pool, Boolean>) PoolType::isImplicit)
-				.thenComparing(p -> p.getPoolName().getOfficeId(),  String.CASE_INSENSITIVE_ORDER)
-				.thenComparing(Pool::getProjectId,  String.CASE_INSENSITIVE_ORDER)
-				.thenComparing(p-> p.getAttribute()==null?null:p.getAttribute().doubleValue(),
-						nullsLast(Comparator.naturalOrder()))
-				.thenComparing((Pool p) -> p.getPoolName().getPoolName(), String.CASE_INSENSITIVE_ORDER);
+		List<String> types = getTypes(includeExplicit, includeImplicit);
+		Condition condition = getCondition(projectIdMask, poolNameMask, bottomLevelMask, topLevelMask, officeIdMask, types);
 
-		Stream<Pool> stream = pools.stream();
-		if(lastItem != null)
-		{
-			// filter first so that there is hopefully less to sort.
-			stream = stream.filter(p -> comparator.compare(lastItem, p) < 0);
-		}
-
-		List<Pool> collected = stream.sorted(comparator)
+		List<Pool> pools = dsl.select(DSL.asterisk()).from(view)
+				.where(condition)
+				.orderBy(view.DEFINITION_TYPE,
+						view.OFFICE_ID.upper(), view.PROJECT_ID.upper(), view.ATTRIBUTE, view.POOL_NAME.upper())
+				.offset(offset)
 				.limit(pageSize)
-				.collect(toList());
+				.stream().map(r -> toPool(r, true)).collect(toList());
 
-		Pools.Builder builder = new Pools.Builder(lastItem, pageSize, total);
-		builder.addAll(collected);
+		Pools.Builder builder = new Pools.Builder(offset, pageSize, total);
+		builder.addAll(pools);
 		return builder.build();
 	}
 
