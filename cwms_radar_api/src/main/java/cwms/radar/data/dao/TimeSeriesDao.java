3f7649ebf85c441e2489d4cd794f0223aecdfb72
ryan <ryan@rmanet.com>
2021-08-05 15:19:24 -0700

    Moved TimeSeries methods into TimeSeriesDao.

--- cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesDao.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesDao.java
@@ -10,23 +10,36 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import cwms.radar.data.dto.Catalog;
+import cwms.radar.data.dto.CwmsDTOPaginated;
 import cwms.radar.data.dto.TimeSeries;
+import cwms.radar.data.dto.Tsv;
+import cwms.radar.data.dto.TsvDqu;
+import cwms.radar.data.dto.TsvDquId;
+import cwms.radar.data.dto.TsvId;
 import cwms.radar.data.dto.catalog.CatalogEntry;
 import cwms.radar.data.dto.catalog.TimeseriesCatalogEntry;
+import org.jetbrains.annotations.NotNull;
+import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.Record;
 import org.jooq.Record1;
 import org.jooq.Record3;
 import org.jooq.Record5;
 import org.jooq.Result;
 import org.jooq.SQL;
 import org.jooq.SelectConditionStep;
+import org.jooq.SelectHavingStep;
 import org.jooq.SelectJoinStep;
 import org.jooq.SelectSelectStep;
 import org.jooq.conf.ParamType;
@@ -35,9 +48,15 @@ import org.jooq.impl.DSL;
 import usace.cwms.db.jooq.codegen.packages.CWMS_ROUNDING_PACKAGE;
 import usace.cwms.db.jooq.codegen.packages.CWMS_TS_PACKAGE;
 import usace.cwms.db.jooq.codegen.packages.CWMS_UTIL_PACKAGE;
+import usace.cwms.db.jooq.codegen.tables.AV_CWMS_TS_ID2;
+import usace.cwms.db.jooq.codegen.tables.AV_TSV;
+import usace.cwms.db.jooq.codegen.tables.AV_TSV_DQU;
 
 import static org.jooq.impl.DSL.asterisk;
 import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.max;
+import static org.jooq.impl.DSL.partitionBy;
 import static usace.cwms.db.jooq.codegen.tables.AV_CWMS_TS_ID2.AV_CWMS_TS_ID2;
 
 public class TimeSeriesDao extends JooqDao<TimeSeries>
@@ -62,13 +81,17 @@ public class TimeSeriesDao extends JooqDao<TimeSeries>
 		Integer total = null;
 
 		if(begin == null)
+		{
 			begin = ZonedDateTime.now().minusDays(1).toLocalDateTime().toString();
+		}
 		if(end == null)
+		{
 			end = ZonedDateTime.now().toLocalDateTime().toString();
+		}
 
 		if(page != null && !page.isEmpty())
 		{
-			String[] parts = TimeSeries.decodeCursor(page);
+			String[] parts = CwmsDTOPaginated.decodeCursor(page);
 
 			logger.info("Decoded cursor");
 			for( String p: parts){
@@ -177,13 +200,11 @@ public class TimeSeriesDao extends JooqDao<TimeSeries>
 
 			logger.info( query.getSQL(ParamType.INLINED));
 
-			query.fetchInto(tsRecord -> {
-						timeseries.addValue(
-								tsRecord.getValue("DATE_TIME", Timestamp.class),
-								tsRecord.getValue("VALUE", Double.class),
-								tsRecord.getValue("QUALITY_CODE", Integer.class)
-						);
-					}
+			query.fetchInto(tsRecord -> timeseries.addValue(
+					tsRecord.getValue("DATE_TIME", Timestamp.class),
+					tsRecord.getValue("VALUE", Double.class),
+					tsRecord.getValue("QUALITY_CODE", Integer.class)
+			)
 			);
 		}
 		return timeseries;
@@ -201,7 +222,7 @@ public class TimeSeriesDao extends JooqDao<TimeSeries>
 		} else {
 			logger.info("getting non-default page");
 			// get totally from page
-			String[] parts = Catalog.decodeCursor(page, "|||");
+			String[] parts = CwmsDTOPaginated.decodeCursor(page, "|||");
 
 			logger.info("decoded cursor: " + String.join("|||", parts));
 			for( String p: parts){
@@ -209,7 +230,7 @@ public class TimeSeriesDao extends JooqDao<TimeSeries>
 			}
 
 			if(parts.length > 1) {
-				tsCursor = parts[0].split("\\/")[1];
+				tsCursor = parts[0].split("/")[1];
 				total = Integer.parseInt(parts[1]);
 			}
 		}
@@ -237,8 +258,137 @@ public class TimeSeriesDao extends JooqDao<TimeSeries>
 						e.get(AV_CWMS_TS_ID2.UNIT_ID) )
 				)
 				.collect(Collectors.toList());
-		Catalog cat = new Catalog(tsCursor,total,pageSize,entries);
-		return cat;
+		return new Catalog(tsCursor,total,pageSize,entries);
+	}
+
+
+	// Finds the single most recent TsvDqu within the time window.
+	public TsvDqu findMostRecent(String tOfficeId, String tsId, String unit, Timestamp twoWeeksFromNow, Timestamp twoWeeksAgo)
+	{
+		TsvDqu retval = null;
+
+		AV_TSV_DQU view = AV_TSV_DQU.AV_TSV_DQU;
+
+		Condition nestedCondition = view.ALIASED_ITEM.isNull()
+				.and(view.VALUE.isNotNull())
+				.and(view.CWMS_TS_ID.eq(tsId))
+				.and(view.OFFICE_ID.eq(tOfficeId));
+
+		if(twoWeeksFromNow != null){
+			nestedCondition = nestedCondition.and(view.DATE_TIME.lt(twoWeeksFromNow));
+		}
+
+		// Is this really optional?
+		if(twoWeeksAgo != null){
+			nestedCondition = nestedCondition.and(view.DATE_TIME.gt(twoWeeksAgo));
+		}
+
+		String maxFieldName = "MAX_DATE_TIME";
+		SelectHavingStep<Record1<Timestamp>> select = dsl.select(max(view.DATE_TIME).as(maxFieldName)).from(
+				view).where(nestedCondition).groupBy(view.TS_CODE);
+
+		Record dquRecord = dsl.select(asterisk()).from(view).where(view.DATE_TIME.in(select)).and(
+				view.CWMS_TS_ID.eq(tsId)).and(view.OFFICE_ID.eq(tOfficeId)).and(view.UNIT_ID.eq(unit)).and(
+				view.VALUE.isNotNull()).and(view.ALIASED_ITEM.isNull()).fetchOne();
+
+		if(dquRecord != null)
+		{
+			retval = dquRecord.map(r -> {
+				usace.cwms.db.jooq.codegen.tables.records.AV_TSV_DQU dqu = r.into(view);
+				TsvDqu tsv = null;
+				if(r != null)
+				{
+					TsvDquId id = new TsvDquId(dqu.getOFFICE_ID(), dqu.getTS_CODE(), dqu.getUNIT_ID(), dqu.getDATE_TIME());
+					tsv = new TsvDqu(id, dqu.getCWMS_TS_ID(), dqu.getVERSION_DATE(), dqu.getDATA_ENTRY_DATE(), dqu.getVALUE(), dqu.getQUALITY_CODE(), dqu.getSTART_DATE(), dqu.getEND_DATE());
+				}
+				return tsv;
+			});
+		}
+
+		return retval;
+	}
+
+
+	// This is similar to the code used for sparklines...
+	// Finds all the Tsv data points in the time range for all the specified tsIds.
+	public List<Tsv> findInDateRange(Collection<String> tsIds, Date startDate, Date endDate) {
+		List<Tsv> retval = Collections.emptyList();
+
+		if (tsIds != null && !tsIds.isEmpty()) {
+
+			Timestamp start = new Timestamp(startDate.getTime());
+			Timestamp end = new Timestamp(endDate.getTime());
+
+			AV_TSV tsvView = AV_TSV.AV_TSV;
+			usace.cwms.db.jooq.codegen.tables.AV_CWMS_TS_ID2 tsView = AV_CWMS_TS_ID2;
+			retval = dsl.select(tsvView.asterisk(), tsView.CWMS_TS_ID)
+					.from(tsvView.join(tsView).on(tsvView.TS_CODE.eq(tsView.TS_CODE.cast(Long.class))))
+					.where(
+							tsView.CWMS_TS_ID.in(tsIds).and(tsvView.DATE_TIME.ge(start)).and(tsvView.DATE_TIME.lt(end)).and(
+									tsvView.START_DATE.le(end)).and(tsvView.END_DATE.gt(start))).orderBy(tsvView.DATE_TIME).fetch(
+							jrecord -> buildTsvFromViewRow(jrecord.into(tsvView)));
+		}
+		return retval;
+	}
+
+	@NotNull
+	private Tsv buildTsvFromViewRow(usace.cwms.db.jooq.codegen.tables.records.AV_TSV into)
+	{
+		TsvId id = new TsvId(into.getTS_CODE(), into.getDATE_TIME(), into.getVERSION_DATE(), into.getDATA_ENTRY_DATE());
+
+		return new Tsv(id, into.getVALUE(), into.getQUALITY_CODE(), into.getSTART_DATE(), into.getEND_DATE());
+	}
+
+	// Finds single most recent value within the window for each of the tsCodes
+	public List<Tsv> findMostRecentsInRange(List<String> tsIds,  Timestamp futuredate, Timestamp pastdate) {
+		List<Tsv> retval = Collections.emptyList();
+
+		if (tsIds != null && !tsIds.isEmpty()) {
+			AV_TSV tsvView = AV_TSV.AV_TSV;
+			AV_CWMS_TS_ID2 tsView = AV_CWMS_TS_ID2;
+			SelectConditionStep<Record> innerSelect
+					= dsl.select(tsvView.asterisk(),
+					max(tsvView.DATE_TIME).over(partitionBy(tsvView.TS_CODE)).as("max_date_time"), tsView.CWMS_TS_ID)
+					.from(tsvView.join(tsView).on(tsvView.TS_CODE.eq(tsView.TS_CODE.cast(Long.class))))
+					.where(
+							tsView.CWMS_TS_ID.in(tsIds)
+									.and(tsvView.VALUE.isNotNull())
+									.and(tsvView.DATE_TIME.lt(futuredate))
+									.and(tsvView.DATE_TIME.gt(pastdate))
+									.and(tsvView.START_DATE.le(futuredate))
+									.and(tsvView.END_DATE.gt(pastdate)));
+
+
+			String[] columns = new String[]{"ts_code","date_time","version_date","data_entry_date","value",
+					"quality_code","start_date","end_date"};
+			List<Field<Object>> fields = Arrays.stream(columns).map(c -> field(c)).collect(Collectors.toList());
+
+			// I want to select tsvView.asterisk but we are selecting from an inner select and
+			// even though the inner select selects tsvView.asterisk it isn't the same.
+			// So we will just select the fields we want.  Unfortunately that means our results
+			// won't map into AV_TSV.AV_TSV
+			retval = dsl.select(fields)
+					.from(innerSelect)
+					.where(field("DATE_TIME").eq(innerSelect.field("max_date_time")))
+
+					.fetch( jrecord -> {
+						Timestamp dataEntryDate;
+						// !!! skipping DATA_ENTRY_DATE for now.  Need to figure out how to fix mapping in jooq.
+						//	!! dataEntryDate= jrecord.getValue("data_entry_date", Timestamp.class); // maps to oracle.sql.TIMESTAMP
+						// !!!
+						dataEntryDate = null;
+						// !!!
+						TsvId id = new TsvId(jrecord.getValue("ts_code", Long.class),
+								jrecord.getValue("date_time", Timestamp.class),
+								jrecord.getValue("version_date", Timestamp.class), dataEntryDate); // oracle timestamp?
+
+						return new Tsv(id, jrecord.getValue("value", Double.class),
+								jrecord.getValue("quality_code", Long.class),
+								jrecord.getValue("start_date", Timestamp.class),
+								jrecord.getValue("end_date", Timestamp.class));
+					});
+		}
+		return retval;
 	}
 
 
