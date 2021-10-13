3a6d806268d274f18655809732e12036f28b699b
bryson <bryson@rmanet.com>
2021-10-13 12:18:52 -0700

    Updated variable names to remove underscores

--- cwms_radar_api/src/main/java/cwms/radar/data/dto/LocationLevel.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dto/LocationLevel.java
@@ -3,7 +3,6 @@ package cwms.radar.data.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import io.swagger.models.auth.In;
 import usace.cwms.db.dao.ifc.level.LocationLevelPojo;
 import usace.cwms.db.dao.ifc.level.SeasonalValueBean;
 
@@ -16,28 +15,28 @@ import java.util.function.Consumer;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LocationLevel
 {
-    private String _seasonalTimeSeriesId;
-    private List<SeasonalValueBean> _seasonalValues;
-    private String _specifiedLevelId;
-    private String _parameterTypeId;
-    private String _parameterId;
-    private Double _siParameterUnitsConstantValue;
-    private String _levelUnitsId;
-    private Date _levelDate;
-    private String _levelComment;
-    private Date _intervalOrigin;
-    private Integer _intervalMonths;
-    private Integer _intervalMinutes;
-    private String _interpolateString;
-    private String _durationId;
-    private BigDecimal _attributeValue;
-    private String _attributeUnitsId;
-    private String _attributeParameterTypeId;
-    private String _attributeParameterId;
-    private String _attributeDurationId;
-    private String _attributeComment;
-    private String _locationId;
-    private String _officeId;
+    private String seasonalTimeSeriesId;
+    private List<SeasonalValueBean> seasonalValues;
+    private String specifiedLevelId;
+    private String parameterTypeId;
+    private String parameterId;
+    private Double siParameterUnitsConstantValue;
+    private String levelUnitsId;
+    private Date levelDate;
+    private String levelComment;
+    private Date intervalOrigin;
+    private Integer intervalMonths;
+    private Integer intervalMinutes;
+    private String interpolateString;
+    private String durationId;
+    private BigDecimal attributeValue;
+    private String attributeUnitsId;
+    private String attributeParameterTypeId;
+    private String attributeParameterId;
+    private String attributeDurationId;
+    private String attributeComment;
+    private String locationId;
+    private String officeId;
     private ZoneId timeZoneId;
     private final Map<String, Consumer<Object>> propertyFunctionMap = new HashMap<>();
 
@@ -77,27 +76,27 @@ public class LocationLevel
     {
         propertyFunctionMap.clear();
         propertyFunctionMap.put("locationId", nameVal -> setLocationId((String)nameVal));
-        propertyFunctionMap.put("seasonalTimeSeriesId", tsId -> setSeasonalTimeSeriesId((String)tsId));
+        propertyFunctionMap.put("seasonalTimeSeriesId", tsIdVal -> setSeasonalTimeSeriesId((String)tsIdVal));
         propertyFunctionMap.put("seasonalValues", seasonalVals -> setSeasonalValues((List)seasonalVals));
         propertyFunctionMap.put("officeId", officeIdVal -> setOfficeId((String)officeIdVal));
-        propertyFunctionMap.put("specifiedLevelId", specifiedLevelId -> setSpecifiedLevelId((String)specifiedLevelId));
-        propertyFunctionMap.put("parameterTypeId", parameterTypeId -> setParameterTypeId((String)parameterTypeId));
-        propertyFunctionMap.put("parameterId", parameterId -> setParameterId((String)parameterId));
+        propertyFunctionMap.put("specifiedLevelId", specifiedLevelIdVal -> setSpecifiedLevelId((String)specifiedLevelIdVal));
+        propertyFunctionMap.put("parameterTypeId", parameterTypeIdVal -> setParameterTypeId((String)parameterTypeIdVal));
+        propertyFunctionMap.put("parameterId", parameterIdVal -> setParameterId((String)parameterIdVal));
         propertyFunctionMap.put("siParameterUnitsConstantValue", paramUnitsConstVal -> setSiParameterUnitsConstantValue((Double)paramUnitsConstVal));
-        propertyFunctionMap.put("levelUnitsId", levelUnitsId -> setLevelUnitsId((String)levelUnitsId));
-        propertyFunctionMap.put("levelDate", levelDate -> setLevelDate((Date)levelDate));
-        propertyFunctionMap.put("levelComment", levelComment -> setLevelComment((String)levelComment));
-        propertyFunctionMap.put("intervalOrigin", intervalOrigin -> setIntervalOrigin((Date)intervalOrigin));
+        propertyFunctionMap.put("levelUnitsId", levelUnitsIdVal -> setLevelUnitsId((String)levelUnitsIdVal));
+        propertyFunctionMap.put("levelDate", levelDateVal -> setLevelDate((Date)levelDateVal));
+        propertyFunctionMap.put("levelComment", levelCommentVal -> setLevelComment((String)levelCommentVal));
+        propertyFunctionMap.put("intervalOrigin", intervalOriginVal -> setIntervalOrigin((Date)intervalOriginVal));
         propertyFunctionMap.put("intervalMonths", months -> setIntervalMonths((Integer)months));
         propertyFunctionMap.put("intervalMinutes", mins -> setIntervalMinutes((Integer)mins));
         propertyFunctionMap.put("interpolateString", interpolateStr -> setInterpolateString((String)interpolateStr));
-        propertyFunctionMap.put("durationId", durationId -> setDurationId((String)durationId));
+        propertyFunctionMap.put("durationId", durationIdVal -> setDurationId((String)durationIdVal));
         propertyFunctionMap.put("attributeValue", attributeVal -> setAttributeValue(BigDecimal.valueOf((Double)attributeVal)));
-        propertyFunctionMap.put("attributeUnitsId", attributeUnitsId -> setAttributeUnitsId((String)attributeUnitsId));
-        propertyFunctionMap.put("attributeParameterTypeId", attributeParameterTypeId -> setAttributeParameterTypeId((String)attributeParameterTypeId));
-        propertyFunctionMap.put("attributeParameterId", attributeParameterId -> setAttributeParameterId((String)attributeParameterId));
-        propertyFunctionMap.put("attributeDurationId", attributeDurationId -> setAttributeDurationId((String)attributeDurationId));
-        propertyFunctionMap.put("attributeComment", attributeComment -> setAttributeComment((String)attributeComment));
+        propertyFunctionMap.put("attributeUnitsId", attributeUnitsIdVal -> setAttributeUnitsId((String)attributeUnitsIdVal));
+        propertyFunctionMap.put("attributeParameterTypeId", attributeParameterTypeIdVal -> setAttributeParameterTypeId((String)attributeParameterTypeIdVal));
+        propertyFunctionMap.put("attributeParameterId", attributeParameterIdVal -> setAttributeParameterId((String)attributeParameterIdVal));
+        propertyFunctionMap.put("attributeDurationId", attributeDurationIdVal -> setAttributeDurationId((String)attributeDurationIdVal));
+        propertyFunctionMap.put("attributeComment", attributeCommentVal -> setAttributeComment((String)attributeCommentVal));
     }
 
     public void setProperty(String propertyName, Object value)
@@ -121,222 +120,222 @@ public class LocationLevel
 
     public String getSeasonalTimeSeriesId()
     {
-        return _seasonalTimeSeriesId;
+        return seasonalTimeSeriesId;
     }
 
     public void setSeasonalTimeSeriesId(String seasonalTimeSeriesId)
     {
-        _seasonalTimeSeriesId = seasonalTimeSeriesId;
+        this.seasonalTimeSeriesId = seasonalTimeSeriesId;
     }
 
     public List<SeasonalValueBean> getSeasonalValues()
     {
-        return _seasonalValues;
+        return seasonalValues;
     }
 
     public void setSeasonalValues(List<SeasonalValueBean> seasonalValues)
     {
-        _seasonalValues = seasonalValues;
+        this.seasonalValues = seasonalValues;
     }
 
     public String getSpecifiedLevelId()
     {
-        return _specifiedLevelId;
+        return specifiedLevelId;
     }
 
     public void setSpecifiedLevelId(String specifiedLevelId)
     {
-        _specifiedLevelId = specifiedLevelId;
+        this.specifiedLevelId = specifiedLevelId;
     }
 
     public String getParameterTypeId()
     {
-        return _parameterTypeId;
+        return parameterTypeId;
     }
 
     public void setParameterTypeId(String parameterTypeId)
     {
-        _parameterTypeId = parameterTypeId;
+        this.parameterTypeId = parameterTypeId;
     }
 
     public String getParameterId()
     {
-        return _parameterId;
+        return parameterId;
     }
 
     public void setParameterId(String parameterId)
     {
-        _parameterId = parameterId;
+        this.parameterId = parameterId;
     }
 
     public Double getSiParameterUnitsConstantValue()
     {
-        return _siParameterUnitsConstantValue;
+        return siParameterUnitsConstantValue;
     }
 
     public void setSiParameterUnitsConstantValue(Double siParameterUnitsConstantValue)
     {
-        _siParameterUnitsConstantValue = siParameterUnitsConstantValue;
+        this.siParameterUnitsConstantValue = siParameterUnitsConstantValue;
     }
 
     public String getLevelUnitsId()
     {
-        return _levelUnitsId;
+        return levelUnitsId;
     }
 
     public void setLevelUnitsId(String levelUnitsId)
     {
-        _levelUnitsId = levelUnitsId;
+        this.levelUnitsId = levelUnitsId;
     }
 
     public Date getLevelDate()
     {
-        return _levelDate;
+        return levelDate;
     }
 
     public void setLevelDate(Date levelDate)
     {
         // java.sql.Timestamp fails on equals against a Date.
-        _levelDate = new Date(levelDate.getTime());
+        this.levelDate = new Date(levelDate.getTime());
     }
 
     public String getLevelComment()
     {
-        return _levelComment;
+        return levelComment;
     }
 
     public void setLevelComment(String levelComment)
     {
-        _levelComment = levelComment;
+        this.levelComment = levelComment;
     }
 
     public Date getIntervalOrigin()
     {
-        return _intervalOrigin;
+        return intervalOrigin;
     }
 
     public void setIntervalOrigin(Date intervalOrigin)
     {
-        _intervalOrigin = intervalOrigin;
+        this.intervalOrigin = intervalOrigin;
     }
 
     public Integer getIntervalMonths()
     {
-        return _intervalMonths;
+        return intervalMonths;
     }
 
     public void setIntervalMonths(Integer intervalMonths)
     {
-        _intervalMonths = intervalMonths;
+        this.intervalMonths = intervalMonths;
     }
 
     public Integer getIntervalMinutes()
     {
-        return _intervalMinutes;
+        return intervalMinutes;
     }
 
     public void setIntervalMinutes(Integer intervalMinutes)
     {
-        _intervalMinutes = intervalMinutes;
+        this.intervalMinutes = intervalMinutes;
     }
 
     public String getInterpolateString()
     {
-        return _interpolateString;
+        return interpolateString;
     }
 
     public void setInterpolateString(String interpolateString)
     {
-        _interpolateString = interpolateString;
+        this.interpolateString = interpolateString;
     }
 
     public String getDurationId()
     {
-        return _durationId;
+        return durationId;
     }
 
     public void setDurationId(String durationId)
     {
-        _durationId = durationId;
+        this.durationId = durationId;
     }
 
     public BigDecimal getAttributeValue()
     {
-        return _attributeValue;
+        return attributeValue;
     }
 
     public void setAttributeValue(BigDecimal attributeValue)
     {
-        _attributeValue = attributeValue;
+        this.attributeValue = attributeValue;
     }
 
     public String getAttributeUnitsId()
     {
-        return _attributeUnitsId;
+        return attributeUnitsId;
     }
 
     public void setAttributeUnitsId(String attributeUnitsId)
     {
-        _attributeUnitsId = attributeUnitsId;
+        this.attributeUnitsId = attributeUnitsId;
     }
 
     public String getAttributeParameterTypeId()
     {
-        return _attributeParameterTypeId;
+        return attributeParameterTypeId;
     }
 
     public void setAttributeParameterTypeId(String attributeParameterTypeId)
     {
-        _attributeParameterTypeId = attributeParameterTypeId;
+        this.attributeParameterTypeId = attributeParameterTypeId;
     }
 
     public String getAttributeParameterId()
     {
-        return _attributeParameterId;
+        return attributeParameterId;
     }
 
     public void setAttributeParameterId(String attributeParameterId)
     {
-        _attributeParameterId = attributeParameterId;
+        this.attributeParameterId = attributeParameterId;
     }
 
     public String getAttributeDurationId()
     {
-        return _attributeDurationId;
+        return attributeDurationId;
     }
 
     public void setAttributeDurationId(String attributeDurationId)
     {
-        _attributeDurationId = attributeDurationId;
+        this.attributeDurationId = attributeDurationId;
     }
 
     public String getAttributeComment()
     {
-        return _attributeComment;
+        return attributeComment;
     }
 
     public void setAttributeComment(String attributeComment)
     {
-        _attributeComment = attributeComment;
+        this.attributeComment = attributeComment;
     }
 
     public String getLocationId()
     {
-        return _locationId;
+        return locationId;
     }
 
     public void setLocationId(String locationId)
     {
-        _locationId = locationId;
+        this.locationId = locationId;
     }
 
     public String getOfficeId()
     {
-        return _officeId;
+        return officeId;
     }
 
     public void setOfficeId(String officeId)
     {
-        _officeId = officeId;
+        this.officeId = officeId;
     }
 }
