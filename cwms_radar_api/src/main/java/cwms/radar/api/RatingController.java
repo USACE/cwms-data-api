9be5903d42877716173f78626f9f752619d6ff58
ryan <ryan@rmanet.com>
2021-10-26 10:09:10 -0700

    Moved getRatings method out of CwmsDataManager and into RatingDao.  Added RatingDao interface. MetricRegistry already does a getOrCreate internally so we don't need to hold meter/timer references.

--- cwms_radar_api/src/main/java/cwms/radar/api/RatingController.java
+++ cwms_radar_api/src/main/java/cwms/radar/api/RatingController.java
@@ -1,64 +1,167 @@
 package cwms.radar.api;
 
-import java.sql.SQLException;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.servlet.http.HttpServletResponse;
 
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
-import static com.codahale.metrics.MetricRegistry.*;
 import com.codahale.metrics.Timer;
-
 import cwms.radar.api.errors.RadarError;
-import cwms.radar.data.CwmsDataManager;
+import cwms.radar.data.dao.RatingDao;
+import cwms.radar.data.dao.RatingSetDao;
+import cwms.radar.formatters.ContentType;
 import cwms.radar.formatters.Formats;
+import cwms.radar.formatters.FormattingException;
 import io.javalin.apibuilder.CrudHandler;
 import io.javalin.http.Context;
+import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.annotations.OpenApi;
+import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiParam;
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+import org.jetbrains.annotations.NotNull;
+import org.jooq.DSLContext;
+
+import hec.data.RatingException;
+import hec.data.cwmsRating.RatingSet;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static cwms.radar.data.dao.JooqDao.getDslContext;
 
 
 public class RatingController implements CrudHandler {
     private static final Logger logger = Logger.getLogger(RatingController.class.getName());
-    private final MetricRegistry metrics;// = new MetricRegistry();
-    private final Meter getAllRequests;// = metrics.meter(OfficeController.class.getName()+"."+"getAll.count");
-    private final Timer getAllRequestsTime;// =metrics.timer(OfficeController.class.getName()+"."+"getAll.time");
-    private final Meter getOneRequest;
-    private final Timer getOneRequestTime;
+    private final MetricRegistry metrics;
+
     private final Histogram requestResultSize;
 
     public RatingController(MetricRegistry metrics){
         this.metrics=metrics;
         String className = this.getClass().getName();
-        getAllRequests = this.metrics.meter(name(className,"getAll","count"));
-        getAllRequestsTime = this.metrics.timer(name(className,"getAll","time"));
-        getOneRequest = this.metrics.meter(name(className,"getOne","count"));
-        getOneRequestTime = this.metrics.timer(name(className,"getOne","time"));
         requestResultSize = this.metrics.histogram((name(className,"results","size")));
     }
 
-    @OpenApi(ignore = true)
-    @Override
-    public void create(Context ctx) {
-        ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED);
+    public Timer.Context markAndTime(String subject)
+    {
+        String className = this.getClass().getName();
+        Meter meter = this.metrics.meter(name(className,subject,"count"));
+        meter.mark();
+        Timer timer = this.metrics.timer(name(className,subject,"time"));
+        return timer.time();
     }
 
-    @OpenApi(ignore = true)
+    @NotNull
+    private RatingDao getRatingDao(DSLContext dsl)
+    {
+        return new RatingSetDao(dsl);
+    }
+
+    @OpenApi(
+            description = "Create new RatingSet",
+            requestBody = @OpenApiRequestBody(
+                    content = {
+                            @OpenApiContent(type = Formats.XML )
+                    },
+                    required = true
+            ),
+            method = HttpMethod.POST,
+            path = "/ratings",
+            tags = {"Ratings"}
+    )
+    public void create(Context ctx) {
+
+        try(final Timer.Context timeContext = markAndTime("create");
+            DSLContext dsl = getDslContext(ctx))
+        {
+            RatingDao ratingDao = getRatingDao(dsl);
+            RatingSet ratingSet = deserializeRatingSet(ctx);
+            ratingDao.create(ratingSet);
+            ctx.status(HttpServletResponse.SC_ACCEPTED).json("Created RatingSet");
+        }
+        catch(IOException | RatingException ex)
+        {
+            RadarError re = new RadarError("failed to process request");
+            logger.log(Level.SEVERE, re.toString(), ex);
+            ctx.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).json(re);
+        }
+    }
+
+    private RatingSet deserializeRatingSet(Context ctx) throws IOException, RatingException
+    {
+        return deserializeRatingSet(ctx.body(), getContentType(ctx));
+    }
+
+    @NotNull
+    private ContentType getContentType(Context ctx)
+    {
+        String requestContent = ctx.req.getContentType();
+        String formatHeader = requestContent != null ? requestContent : Formats.JSON;
+        ContentType contentType = Formats.parseHeader(formatHeader);
+        if(contentType == null)
+        {
+            throw new FormattingException("Format header could not be parsed:" + formatHeader);
+        }
+        return contentType;
+    }
+
+    private RatingSet deserializeRatingSet(String body, ContentType contentType) throws IOException, RatingException
+    {
+        return deserializeRatingSet(body, contentType.getType());
+    }
+
+    public static RatingSet deserializeRatingSet(String body, String contentType) throws IOException, RatingException
+    {
+        RatingSet retval;
+
+        if((Formats.XMLV2).equals(contentType))
+        {
+            retval = RatingSet.fromXml(body);
+        } else if((Formats.JSONV2).equals(contentType)){
+            throw new IOException("JSONv2 RatingSet not implemented" );
+        } else {
+            throw new IOException("Unexpected format:" + contentType);
+        }
+
+        return retval;
+    }
+
+    @OpenApi(
+            queryParams = {
+                    @OpenApiParam(name = "office", required = true, description = "Specifies the owning office of the rating to be deleted.")
+            },
+            method = HttpMethod.DELETE,
+            tags = {"Ratings"}
+    )
     @Override
-    public void delete(Context ctx, String rating) {
-        ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED);
+    public void delete(Context ctx, String ratingSpecId) {
+        String office = ctx.queryParam("office");
+
+        try(final Timer.Context timeContext = markAndTime("delete");
+            DSLContext dsl = getDslContext(ctx))
+        {
+            RatingDao ratingDao = getRatingDao(dsl);
+            ratingDao.delete(office, ratingSpecId);
+
+            ctx.status(HttpServletResponse.SC_ACCEPTED).json("Deleted RatingSet");
+        }
+        catch(IOException | RatingException ex)
+        {
+            RadarError re = new RadarError("failed to process request");
+            logger.log(Level.SEVERE, re.toString(), ex);
+            ctx.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).json(re);
+        }
     }
 
     @OpenApi(
         queryParams = {
-            @OpenApiParam(name="name", required=false, description="Specifies the name(s) of the time series whose data is to be included in the response. A case insensitive comparison is used to match names."),
-            @OpenApiParam(name="office", required=false, description="Specifies the owning office of the location level(s) whose data is to be included in the response. If this field is not specified, matching location level information from all offices shall be returned."),
-            @OpenApiParam(name="unit", required=false, description="Specifies the unit or unit system of the response. Valid values for the unit field are:\r\n 1. EN.   Specifies English unit system.  Location level values will be in the default English units for their parameters.\r\n2. SI.   Specifies the SI unit system.  Location level values will be in the default SI units for their parameters.\r\n3. Other. Any unit returned in the response to the units URI request that is appropriate for the requested parameters."),
-            @OpenApiParam(name="datum", required=false, description="Specifies the elevation datum of the response. This field affects only elevation location levels. Valid values for this field are:\r\n1. NAVD88.  The elevation values will in the specified or default units above the NAVD-88 datum.\r\n2. NGVD29.  The elevation values will be in the specified or default units above the NGVD-29 datum."),
+            @OpenApiParam(name="name", required=false, description="Specifies the name(s) of the rating whose data is to be included in the response. A case insensitive comparison is used to match names."),
+            @OpenApiParam(name="office", required=false, description="Specifies the owning office of the Rating(s) whose data is to be included in the response. If this field is not specified, matching rating information from all offices shall be returned."),
+            @OpenApiParam(name="unit", required=false, description="Specifies the unit or unit system of the response. Valid values for the unit field are:\r\n 1. EN.   Specifies English unit system.  Rating values will be in the default English units for their parameters.\r\n2. SI.   Specifies the SI unit system.  Rating values will be in the default SI units for their parameters.\r\n3. Other. Any unit returned in the response to the units URI request that is appropriate for the requested parameters."),
+            @OpenApiParam(name="datum", required=false, description="Specifies the elevation datum of the response. This field affects only elevation Ratings. Valid values for this field are:\r\n1. NAVD88.  The elevation values will in the specified or default units above the NAVD-88 datum.\r\n2. NGVD29.  The elevation values will be in the specified or default units above the NGVD-29 datum."),
             @OpenApiParam(name="at", required=false, description="Specifies the start of the time window for data to be included in the response. If this field is not specified, any required time window begins 24 hours prior to the specified or default end time."),
             @OpenApiParam(name="end", required=false, description="Specifies the end of the time window for data to be included in the response. If this field is not specified, any required time window ends at the current time"),
             @OpenApiParam(name="timezone", required=false, description="Specifies the time zone of the values of the begin and end fields (unless otherwise specified), as well as the time zone of any times in the response. If this field is not specified, the default time zone of UTC shall be used."),
@@ -74,63 +177,112 @@ public class RatingController implements CrudHandler {
     )
     @Override
     public void getAll(Context ctx) {
-        getAllRequests.mark();
+
         try (
-            final Timer.Context time_context = getAllRequestsTime.time();
-                CwmsDataManager cdm = new CwmsDataManager(ctx);
-            ) {
-                String format = ctx.queryParamAsClass("format",String.class).getOrDefault("json");
-                String names = ctx.queryParam("names");
-                String unit = ctx.queryParam("unit");
-                String datum = ctx.queryParam("datum");
-                String office = ctx.queryParam("office");
-                String start = ctx.queryParam("at");
-                String end = ctx.queryParam("end");
-                String timezone = ctx.queryParamAsClass("timezone",String.class).getOrDefault("UTC");
-                String size = ctx.queryParam("size");
+            final Timer.Context timeContext = markAndTime("getAll");
+            DSLContext dsl = getDslContext(ctx)
+            )
+        {
+            RatingDao ratingDao = getRatingDao(dsl);
 
-                switch(format){
-                    case "json": {ctx.contentType(Formats.JSON); break;}
-                    case "tab": {ctx.contentType(Formats.TAB); break;}
-                    case "csv": {ctx.contentType(Formats.CSV); break;}
-                    case "xml": {ctx.contentType(Formats.XML); break;}
-                    case "wml2": {ctx.contentType(Formats.WML2); break;}
-                    case "jpg": // same handler
-                    case "png":
-                     default: {
-                        ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED).json(RadarError.notImplemented());
-                        return;
-                     }
+            String format = ctx.queryParamAsClass("format", String.class).getOrDefault("json");
+            String names = ctx.queryParam("names");
+            String unit = ctx.queryParam("unit");
+            String datum = ctx.queryParam("datum");
+            String office = ctx.queryParam("office");
+            String start = ctx.queryParam("at");
+            String end = ctx.queryParam("end");
+            String timezone = ctx.queryParamAsClass("timezone", String.class).getOrDefault("UTC");
+
+            switch(format)
+            {
+                case "json":
+                {
+                    ctx.contentType(Formats.JSON);
+                    break;
                 }
+                case "tab":
+                {
+                    ctx.contentType(Formats.TAB);
+                    break;
+                }
+                case "csv":
+                {
+                    ctx.contentType(Formats.CSV);
+                    break;
+                }
+                case "xml":
+                {
+                    ctx.contentType(Formats.XML);
+                    break;
+                }
+                case "wml2":
+                {
+                    ctx.contentType(Formats.WML2);
+                    break;
+                }
+                case "jpg": // same handler
+                case "png":
+                default:
+                {
+                    ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED).json(RadarError.notImplemented());
+                    return;
+                }
+            }
 
-                String results = cdm.getRatings(names,format,unit,datum,office,start,end,timezone,size);
-                ctx.status(HttpServletResponse.SC_OK);
-                ctx.result(results);
-                requestResultSize.update(results.length());
-        } catch (SQLException ex) {
-            RadarError re = new RadarError("Failed to process request");
-            logger.log(Level.SEVERE, re.toString(), ex);
-            ctx.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            ctx.json(re);
+            String results = ratingDao.retrieveRatings(format, names, unit, datum, office, start, end, timezone);
+            ctx.status(HttpServletResponse.SC_OK);
+            ctx.result(results);
+            requestResultSize.update(results.length());
         }
-
     }
+
+
 
     @OpenApi(ignore = true)
     @Override
     public void getOne(Context ctx, String rating) {
-        getOneRequest.mark();
+
         try (
-            final Timer.Context time_context = getOneRequestTime.time();
+            final Timer.Context timeContext = markAndTime("getOne")
             ){
                 ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED).json(RadarError.notImplemented());
         }
     }
 
-    @OpenApi(ignore = true)
-    @Override
-    public void update(Context ctx, String rating) {
-        ctx.status(HttpServletResponse.SC_NOT_IMPLEMENTED).json(RadarError.notImplemented());
+
+    @OpenApi(
+            description = "Update a RatingSet",
+            requestBody = @OpenApiRequestBody(
+                    content = {
+                            @OpenApiContent(type = Formats.XML )
+                    },
+                    required = true
+            ),
+            method = HttpMethod.PATCH,
+            path = "/ratings",
+            tags = {"Ratings"}
+    )
+    public void update(Context ctx, String ratingId) {
+
+        try(final Timer.Context timeContext = markAndTime("update");
+            DSLContext dsl = getDslContext(ctx))
+        {
+            RatingDao ratingDao = getRatingDao(dsl);
+
+            // Retrieve the rating specified by ratingId and then somehow apply the update?
+            // RatingSet ratingSet = ratingDao.retrieve(officeId, ratingId);
+            // Or just store what they sent us?
+            RatingSet ratingSet = deserializeRatingSet(ctx);
+            ratingDao.store(ratingSet);
+            ctx.status(HttpServletResponse.SC_ACCEPTED).json("Updated RatingSet");
+        }
+        catch(IOException | RatingException ex)
+        {
+            RadarError re = new RadarError("failed to process request to update RatingSet");
+            logger.log(Level.SEVERE, re.toString(), ex);
+            ctx.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).json(re);
+        }
     }
 
 }
---
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/RatingSetDao.java
@@ -0,0 +1,125 @@
+package cwms.radar.data.dao;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.jooq.DSLContext;
+import org.jooq.exception.DataAccessException;
+
+import hec.data.RatingException;
+import hec.data.cwmsRating.RatingSet;
+import usace.cwms.db.jooq.codegen.packages.CWMS_RATING_PACKAGE;
+
+public class RatingSetDao extends JooqDao<RatingSet> implements RatingDao
+{
+	private static final Logger logger = Logger.getLogger(RatingSetDao.class.getName());
+
+	public RatingSetDao(DSLContext dsl)
+	{
+		super(dsl);
+	}
+
+	@Override
+	public void create(RatingSet ratingSet) throws IOException
+	{
+		try
+		{
+			dsl.connection(c ->
+			{
+				// can't exist if we are creating, if it exists use store
+				boolean overwriteExisting = false;
+				ratingSet.storeToDatabase(c, overwriteExisting);
+			});
+		}
+		catch(DataAccessException ex)
+		{
+			throw new IOException("Failed to create Rating");
+		}
+	}
+
+	@Override
+	public RatingSet retrieve(String officeId, String specificationId) throws IOException
+	{
+		final RatingSet[] retval = new RatingSet[1];
+		try
+		{
+			dsl.connection(c -> retval[0] = RatingSet.fromDatabase(c, officeId, specificationId));
+		}
+		catch(DataAccessException ex)
+		{
+			throw new IOException("Failed to create Rating");
+		}
+		return retval[0];
+	}
+
+	// store/update
+	@Override
+	public void store(RatingSet ratingSet) throws IOException
+	{
+		try
+		{
+			dsl.connection(c ->
+			{
+				boolean overwriteExisting = true;
+				ratingSet.storeToDatabase(c, overwriteExisting);
+			});
+		}
+		catch(DataAccessException ex)
+		{
+			throw new IOException("Failed to store Rating");
+		}
+	}
+
+	@Override
+	public void delete(String officeId, String ratingSpecId) throws IOException, RatingException
+	{
+
+		try
+		{
+			dsl.connection(c ->
+			{
+				RatingSet ratingSet = RatingSet.fromDatabase(c, officeId, ratingSpecId);
+				ratingSet.removeAllRatings();
+				final boolean overwriteExisting = true;
+				ratingSet.storeToDatabase(c, overwriteExisting);  // Does this actually delete?
+			});
+		}
+		catch(DataAccessException ex)
+		{
+			throw new IOException("Failed to delete Rating");
+		}
+
+	}
+
+	public void delete(String officeId, String specificationId, long[] effectiveDates) throws IOException
+	{
+
+		try
+		{
+			dsl.connection(c ->
+			{
+				RatingSet ratingSet = RatingSet.fromDatabase(c, officeId, specificationId);
+				for(final long effectiveDate : effectiveDates)
+				{
+					ratingSet.removeRating(effectiveDate);
+
+				}
+				final boolean overwriteExisting = true;
+				ratingSet.storeToDatabase(c, overwriteExisting);
+			});
+		}
+		catch(DataAccessException ex)
+		{
+			throw new IOException("Failed to delete Rating");
+		}
+
+	}
+
+
+	@Override
+	public String retrieveRatings(String format, String names, String unit, String datum, String office, String start, String end, String timezone)
+	{
+		return CWMS_RATING_PACKAGE.call_RETRIEVE_RATINGS_F(dsl.configuration(), names, format, unit, datum,
+				start, end, timezone, office);
+	}
+}
