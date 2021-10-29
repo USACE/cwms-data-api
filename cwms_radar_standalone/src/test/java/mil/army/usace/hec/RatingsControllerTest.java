7682aef4962fe4cd2ae591b56b932b3a745799a7
ryan <ryan@rmanet.com>
2021-10-28 19:06:53 -0700

    Stop the RadarAPI in a finally block.  There is probably a better way to do this.

--- cwms_radar_standalone/src/test/java/mil/army/usace/hec/RatingsControllerTest.java
+++ cwms_radar_standalone/src/test/java/mil/army/usace/hec/RatingsControllerTest.java
@@ -1,6 +1,7 @@
 package mil.army.usace.hec;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,7 @@ import hec.data.RatingException;
 
 import static io.javalin.apibuilder.ApiBuilder.crud;
 import static io.restassured.RestAssured.post;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -45,25 +47,32 @@ public class RatingsControllerTest {
 
     @BeforeEach
     public void baseLineDbMocks() throws SQLException, IOException{
+        InputStream stream = RatingsControllerTest.class.getResourceAsStream("/ratings_db.txt");
+        assertNotNull(stream);
         this.conn = new MockConnection(
-                                new MockFileDatabase(
-                                    RatingsControllerTest.class.getResourceAsStream("/ratings_db.txt")
+                                new MockFileDatabase(stream
                                 )
                     );
         when(ds.getConnection()).thenReturn(conn);
     }
 
     @Test
-    public void unimplemented_methods_return_501(){
-        new RadarAPI(ds,7000).start();
-        post("/ratings").then()
-        .statusCode(HttpServletResponse.SC_NOT_IMPLEMENTED);
+    void unimplemented_methods_return_501(){
+        RadarAPI radarAPI = new RadarAPI(ds, 7000);
+        try
+        {
+            radarAPI.start();
+            post("/ratings").then().statusCode(HttpServletResponse.SC_NOT_IMPLEMENTED);
+        }finally
+        {
+            radarAPI.stop();
+        }
     }
 
     // This is only supposed to test that when XML data is posted to create,
     // that data is forward to the method deserializeFromXml
     @Test
-    public void post_to_create_passed_to_deserializeXml() throws RatingException, IOException
+    void post_to_create_passed_to_deserializeXml() throws RatingException, IOException
     {
         RatingController controller = spy(new RatingController(new MetricRegistry()));
 
@@ -80,21 +89,23 @@ public class RatingsControllerTest {
             }
         }
         MyRadar radarAPI = new MyRadar(ds, 7000);
-        radarAPI.start();
 
-        final String testBody = "could be anything";
-        RequestSpecification request = RestAssured.given();
-        Response response = request
-                .config(RestAssuredConfig.config().encoderConfig(EncoderConfig.encoderConfig()
-                        .appendDefaultContentCharsetToContentTypeIfUndefined(false)))
-                .contentType(Formats.XMLV2)
-                .body(testBody)
-                .post("/ratings");
+        try
+        {
+            radarAPI.start();
 
-        // For this test, its ok that the server throws a RatingException
-        // Only want to check that the controller accessed our mock dao in the expected way
-        verify(controller, times(1)).
-                deserializeRatingSet(eq(testBody), eq(Formats.XML));  // Curious that is is XML and not XMLv2
+            final String testBody = "could be anything";
+            RequestSpecification request = RestAssured.given();
+            Response response = request.config(RestAssuredConfig.config().encoderConfig(
+                    EncoderConfig.encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false))).contentType(Formats.XMLV2).body(testBody).post("/ratings");
+
+            // For this test, its ok that the server throws a RatingException
+            // Only want to check that the controller accessed our mock dao in the expected way
+            verify(controller, times(1)).deserializeRatingSet(testBody, Formats.XML);  // Curious that is is XML and not XMLv2
+        } finally
+        {
+            radarAPI.stop();
+        }
     }
 
 }
