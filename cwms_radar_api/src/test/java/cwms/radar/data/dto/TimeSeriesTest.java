fb89b825f3942a4734d9f421d8d0c6ec01a7dd5f
ryan <ryan@rmanet.com>
2021-12-01 18:39:51 -0800

    Fixing merge conflict.

--- cwms_radar_api/src/test/java/cwms/radar/data/dto/TimeSeriesTest.java
+++ cwms_radar_api/src/test/java/cwms/radar/data/dto/TimeSeriesTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TimeSeriesTest
@@ -40,17 +41,59 @@ public class TimeSeriesTest
 		assertTrue(ts.getEnd().isEqual(ts2.getEnd()));
 		assertEquals(ts.getUnits(), ts2.getUnits());
 		assertEquals(ts.getValues(), ts2.getValues());
+		assertNull(ts.getVerticalDatumInfo());
 	}
+
+	@Test
+	void testRoundtripJsonVertical() throws JsonProcessingException
+	{
+		TimeSeries ts = buildTimeSeries(buildVerticalDatumInfo());
+
+		ObjectMapper om = buildObjectMapper();
+
+		String tsBody = om.writeValueAsString(ts);
+		assertNotNull(tsBody);
+
+		TimeSeries ts2 = om.readValue(tsBody, TimeSeries.class);
+		assertNotNull(ts2);
+
+		assertEquals(ts.getName(), ts2.getName());
+		assertEquals(ts.getOfficeId(), ts2.getOfficeId());
+		assertTrue(ts.getBegin().isEqual(ts2.getBegin()));
+		assertTrue(ts.getEnd().isEqual(ts2.getEnd()));
+		assertEquals(ts.getUnits(), ts2.getUnits());
+		assertEquals(ts.getValues(), ts2.getValues());
+		assertNotNull(ts.getVerticalDatumInfo());
+
+
+		assertEquals("NGVD-29", ts.getVerticalDatumInfo().getNativeDatum());
+	}
+
 
 	@NotNull
 	private TimeSeries buildTimeSeries()
+	{
+		return buildTimeSeries(null);
+	}
+
+	@NotNull
+	private TimeSeries buildTimeSeries(VerticalDatumInfo vdi)
 	{
 		String tsId = "RYAN3.Stage.Inst.5Minutes.0.ZSTORE_TS_TEST";
 
 		ZonedDateTime start = ZonedDateTime.parse("2021-06-21T14:00:00-07:00[PST8PDT]");
 		ZonedDateTime end = ZonedDateTime.parse("2021-06-22T14:00:00-07:00[PST8PDT]");
-		TimeSeries ts = new TimeSeries(null, -1, 0, tsId, "LRL", start, end, null, Duration.ZERO);
-		return ts;
+
+		return new TimeSeries(null, -1, 0, tsId, "LRL", start, end, null, Duration.ZERO, vdi);
+	}
+
+	VerticalDatumInfo buildVerticalDatumInfo()
+	{
+		VerticalDatumInfo.Builder builder = new VerticalDatumInfo.Builder()
+				.withOffice("LRL").withUnit("m").withLocation("Buckhorn")
+				.withNativeDatum("NGVD-29").withElevation(230.7).withOffset(
+						true, "NAVD-88", -.1666);
+		return builder.build();
 	}
 
 	@Test
@@ -63,8 +106,6 @@ public class TimeSeriesTest
 		ZonedDateTime rt1 = ZonedDateTime.parse(formatted1, formatter1);
 		assertTrue(start.isEqual(rt1));
 	}
-
-
 
 	@NotNull
 	public static ObjectMapper buildObjectMapper()
@@ -85,21 +126,21 @@ public class TimeSeriesTest
 	}
 
 
-    @Test
-    void test_xml_value_columns()
-    {
-	    TimeSeries ts = buildTimeSeries();
+	@Test
+	void test_xml_value_columns()
+	{
+		TimeSeries ts = buildTimeSeries();
 
-	    XMLv2 xmlV2 = new XMLv2();
-	    String xmlStr = xmlV2.format(ts);
-	    assertNotNull(xmlStr);
+		XMLv2 xmlV2 = new XMLv2();
+		String xmlStr = xmlV2.format(ts);
+		assertNotNull(xmlStr);
 
-	    assertFalse(xmlStr.contains("valueColumns"));
-	    assertTrue(xmlStr.contains("value-columns"));
+		assertFalse(xmlStr.contains("valueColumns"));
+		assertTrue(xmlStr.contains("value-columns"));
 
-	    assertFalse(xmlStr.contains("officeId"));
-	    assertTrue(xmlStr.contains("office-id"));
-    }
+		assertFalse(xmlStr.contains("officeId"));
+		assertTrue(xmlStr.contains("office-id"));
+	}
 }
 
 
