d353625a692db3305a9327509ab282aef3c3ff62
Mike Neilson <michael.a.neilson@usace.army.mil>
2022-01-07 13:58:21 -0800

    [timeseries:time] modified to allow relative times in ts retrieve.

--- cwms_radar_api/src/test/java/cwms/radar/helpers/DurationArguments.java
+++ cwms_radar_api/src/test/java/cwms/radar/helpers/DurationArguments.java
@@ -16,12 +16,13 @@ public class DurationArguments implements ArgumentsProvider{
         ZoneId utc = ZoneId.of("UTC");
         ZoneId pacific = ZoneId.of("US/Pacific");
         ZonedDateTime nowNormalDay = ZonedDateTime.of(2022,1,6,0,0,0,0,utc);
-        ZonedDateTime nowDstBoundary = ZonedDateTime.of(2022,3,14,0,0,0,0,pacific);
+        ZonedDateTime nowDstBoundaryMarch = ZonedDateTime.of(2022,3,14,0,0,0,0,pacific);
+        ZonedDateTime nowDstBoundaryNov = ZonedDateTime.of(2022,11,6,0,0,0,0,pacific);
 
         return Stream.of(
             Arguments.of(nowNormalDay,"P-1D","UTC",ZonedDateTime.of(2022,1,5,0,0,0,0,utc)),
-            Arguments.of(nowDstBoundary,"P-1D","US/Pacific",ZonedDateTime.of(2022,3,13,8,0,0,0,utc))
-
+            Arguments.of(nowDstBoundaryMarch,"P-1D","US/Pacific",ZonedDateTime.of(2022,3,13,8,0,0,0,utc)),
+            Arguments.of(nowDstBoundaryNov,"P-1D","US/Pacific",ZonedDateTime.of(2022,11,5,7,0,0,0,utc))
         );
     }
 
