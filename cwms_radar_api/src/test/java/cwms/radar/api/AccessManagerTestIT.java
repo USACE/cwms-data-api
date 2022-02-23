919e46555a31969844f835ab34983bd303388bd5
ryan <ryan@rmanet.com>
2022-02-23 12:14:08 -0800

    Minor cleanup

--- cwms_radar_api/src/test/java/cwms/radar/api/AccessManagerTestIT.java
+++ cwms_radar_api/src/test/java/cwms/radar/api/AccessManagerTestIT.java
@@ -1,12 +1,9 @@
 package cwms.radar.api;
 
-
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import cwms.radar.security.CwmsAccessManager;
 import fixtures.RadarApiSetupCallback;
-import fixtures.TestRealm;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -21,19 +18,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ExtendWith(RadarApiSetupCallback.class)
 public class AccessManagerTestIT
 {
-	public static final Logger logger = Logger.getLogger(AccessManagerTestIT.class.getName());
 
 	@Test
 	public void can_getOne_without_user(){
-		logger.info("can_getOne_without_user");
-
 		Response response = given()
 				.contentType("application/json")
 				.queryParam("office", "SPK")
 				.queryParam("names", "AR*")
 				.queryParam("unit", "EN")
 				.get(  "/locations");
-		String bodyStrig = response.body().asString();
 
 		response.then().assertThat()
 				.statusCode(is(200));
@@ -41,8 +34,6 @@ public class AccessManagerTestIT
 
 	@Test
 	public void can_getOne_with_user(){
-		logger.info("can_getOne_with_user");
-
 		Response response = given()
 				.contentType("application/json")
 				.queryParam("office", "SPK")
@@ -50,7 +41,6 @@ public class AccessManagerTestIT
 				.queryParam("unit", "EN")
 				.header("Authorization","name user1")
 				.get(  "/locations");
-		String bodyStrig = response.body().asString();
 
 		response.then().assertThat()
 				.statusCode(is(200));
@@ -59,12 +49,9 @@ public class AccessManagerTestIT
 	@Test
 	public void cant_create_without_user() throws IOException
 	{
-		logger.info("cant_create_without_user");
-
 		String json = loadResourceAsString("cwms/radar/api/location_create.json");
 		assertNotNull(json);
 
-		final String postBody = "";
 		given()
 				.contentType("application/json")
 				.queryParam("office", "SPK")
@@ -78,12 +65,10 @@ public class AccessManagerTestIT
 	@Test
 	public void can_create_with_user() throws IOException
 	{
-		logger.info("can_create_with_user");
-
 		String json = loadResourceAsString("cwms/radar/api/location_create.json");
 		assertNotNull(json);
 
-		final String postBody = "";
+
 		given()
 				.contentType("application/json")
 				.queryParam("office", "SPK")
@@ -98,8 +83,6 @@ public class AccessManagerTestIT
 	@Test
 	public void cant_create_with_user_without_role() throws IOException
 	{
-		logger.info("cant_create_with_user_without_role");
-
 		String json = loadResourceAsString("cwms/radar/api/location_create.json");
 		assertNotNull(json);
 
