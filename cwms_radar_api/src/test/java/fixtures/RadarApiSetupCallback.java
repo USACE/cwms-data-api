bec9d2564abf3f18e2ccb0f57ff2481e6e0e43ee
Mike Neilson <michael.a.neilson@usace.army.mil>
2022-06-23 16:27:51 +0000

    fixed typo.

--- cwms_radar_api/src/test/java/fixtures/RadarApiSetupCallback.java
+++ cwms_radar_api/src/test/java/fixtures/RadarApiSetupCallback.java
@@ -34,7 +34,7 @@ public class RadarApiSetupCallback implements BeforeAllCallback,AfterAllCallback
     private static TestRealm realm;
     private static TestAuthValve authValve;
     private static final String ORACLE_IMAGE = System.getProperty("RADAR.oracle.database.image", CwmsDatabaseContainer.ORACLE_18XE);
-    private static final String ORACLE_VOLUME = System.getProperty("RADAR>oracle.database.volume", "radar_cwmsdb");
+    private static final String ORACLE_VOLUME = System.getProperty("RADAR.oracle.database.volume", "radar_cwmsdb");
     private static final String CWMS_DB_IMAGE = System.getProperty("RADAR.cwms.database.image", "registry.hecdev.net/cwms_schema_installer:21.1.1");
 
     @Override
