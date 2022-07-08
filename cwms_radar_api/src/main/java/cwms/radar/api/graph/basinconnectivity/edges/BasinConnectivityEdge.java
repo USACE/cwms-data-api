6e572e0239e33b27db409c58eb46ac9698d3dad4
ryan <ryan@rmanet.com>
2022-07-08 15:01:03 -0700

    Formatted source to fix easy checkstyle issues.

--- cwms_radar_api/src/main/java/cwms/radar/api/graph/basinconnectivity/edges/BasinConnectivityEdge.java
+++ cwms_radar_api/src/main/java/cwms/radar/api/graph/basinconnectivity/edges/BasinConnectivityEdge.java
@@ -3,37 +3,32 @@ package cwms.radar.api.graph.basinconnectivity.edges;
 import cwms.radar.api.graph.Edge;
 import cwms.radar.api.graph.basinconnectivity.nodes.BasinConnectivityNode;
 
-public abstract class BasinConnectivityEdge implements Edge
-{
+public abstract class BasinConnectivityEdge implements Edge {
     private final String streamId;
     private final BasinConnectivityNode source;
     private final BasinConnectivityNode target;
 
-    protected BasinConnectivityEdge(String streamId, BasinConnectivityNode source, BasinConnectivityNode target)
-    {
+    protected BasinConnectivityEdge(String streamId, BasinConnectivityNode source,
+                                    BasinConnectivityNode target) {
         this.streamId = streamId;
         this.source = source;
         this.target = target;
     }
 
     @Override
-    public String getId()
-    {
+    public String getId() {
         return getStreamId();
     }
 
-    public String getStreamId()
-    {
+    public String getStreamId() {
         return streamId;
     }
 
-    public BasinConnectivityNode getSource()
-    {
+    public BasinConnectivityNode getSource() {
         return source;
     }
 
-    public BasinConnectivityNode getTarget()
-    {
+    public BasinConnectivityNode getTarget() {
         return target;
     }
 
