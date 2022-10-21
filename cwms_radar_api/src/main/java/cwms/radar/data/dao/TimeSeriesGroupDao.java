35bc9691b7bea016bbccd402b90a5754269a0de9
ryan <ryan@rmanet.com>
2022-10-21 12:44:56 -0700

    Join on the office_id as well b/c if multiple offices have the same category/groups we need to keep the assignments separate.

--- cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesGroupDao.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesGroupDao.java
@@ -3,13 +3,12 @@ package cwms.radar.data.dao;
 import cwms.radar.data.dto.AssignedTimeSeries;
 import cwms.radar.data.dto.TimeSeriesCategory;
 import cwms.radar.data.dto.TimeSeriesGroup;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.logging.Logger;
 import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jooq.Condition;
@@ -18,11 +17,14 @@ import org.jooq.Record;
 import org.jooq.RecordMapper;
 import org.jooq.SelectOnConditionStep;
 import org.jooq.SelectOrderByStep;
-
+import org.jooq.SelectSeekStep1;
+import org.jooq.conf.ParamType;
 import usace.cwms.db.jooq.codegen.tables.AV_TS_CAT_GRP;
 import usace.cwms.db.jooq.codegen.tables.AV_TS_GRP_ASSGN;
 
 public class TimeSeriesGroupDao extends JooqDao<TimeSeriesGroup> {
+    private static final Logger logger = Logger.getLogger(TimeSeriesGroupDao.class.getName());
+
     public TimeSeriesGroupDao(DSLContext dsl) {
         super(dsl);
     }
@@ -52,36 +54,40 @@ public class TimeSeriesGroupDao extends JooqDao<TimeSeriesGroup> {
         AV_TS_GRP_ASSGN grpAssgn = AV_TS_GRP_ASSGN.AV_TS_GRP_ASSGN;
 
         final RecordMapper<Record, Pair<TimeSeriesGroup, AssignedTimeSeries>> mapper =
-            queryRecord -> {
-                TimeSeriesGroup group = buildTimeSeriesGroup(queryRecord);
-                AssignedTimeSeries loc = buildAssignedTimeSeries(queryRecord);
+                queryRecord -> {
+                    TimeSeriesGroup group = buildTimeSeriesGroup(queryRecord);
+                    AssignedTimeSeries loc = buildAssignedTimeSeries(queryRecord);
 
-                return new Pair<>(group, loc);
-            };
+                    return new Pair<>(group, loc);
+                };
 
         SelectOnConditionStep<?> selectOn = dsl.select(catGrp.CAT_DB_OFFICE_ID,
-                catGrp.TS_CATEGORY_ID, catGrp.TS_CATEGORY_DESC, catGrp.GRP_DB_OFFICE_ID,
-                catGrp.TS_GROUP_ID, catGrp.TS_GROUP_DESC, catGrp.SHARED_TS_ALIAS_ID,
-                catGrp.SHARED_REF_TS_ID, grpAssgn.CATEGORY_ID, grpAssgn.DB_OFFICE_ID,
-                grpAssgn.GROUP_ID, grpAssgn.TS_ID, grpAssgn.TS_CODE, grpAssgn.ATTRIBUTE,
-                grpAssgn.ALIAS_ID, grpAssgn.REF_TS_ID)
+                        catGrp.TS_CATEGORY_ID, catGrp.TS_CATEGORY_DESC, catGrp.GRP_DB_OFFICE_ID,
+                        catGrp.TS_GROUP_ID, catGrp.TS_GROUP_DESC, catGrp.SHARED_TS_ALIAS_ID,
+                        catGrp.SHARED_REF_TS_ID, grpAssgn.CATEGORY_ID, grpAssgn.DB_OFFICE_ID,
+                        grpAssgn.GROUP_ID, grpAssgn.TS_ID, grpAssgn.TS_CODE, grpAssgn.ATTRIBUTE,
+                        grpAssgn.ALIAS_ID, grpAssgn.REF_TS_ID)
                 .from(catGrp).leftOuterJoin(grpAssgn)
-                .on(catGrp.TS_CATEGORY_ID.eq(grpAssgn.CATEGORY_ID)
+                .on(catGrp.GRP_DB_OFFICE_ID.eq(grpAssgn.DB_OFFICE_ID)
+                        .and(catGrp.TS_CATEGORY_ID.eq(grpAssgn.CATEGORY_ID))
                         .and(catGrp.TS_GROUP_ID.eq(grpAssgn.GROUP_ID)));
-
 
         SelectOrderByStep<?> select = selectOn;
         if (whereCond != null) {
             select = selectOn.where(whereCond);
         }
 
+        SelectSeekStep1<?, BigDecimal> query = select.orderBy(grpAssgn.ATTRIBUTE);
+
+        logger.info(() -> query.getSQL(ParamType.INLINED));
+
         List<Pair<TimeSeriesGroup, AssignedTimeSeries>> assignments =
-                select.orderBy(grpAssgn.ATTRIBUTE).fetch(mapper);
+                query.fetch(mapper);
 
         Map<TimeSeriesGroup, List<AssignedTimeSeries>> map = new LinkedHashMap<>();
         for (Pair<TimeSeriesGroup, AssignedTimeSeries> pair : assignments) {
             List<AssignedTimeSeries> list = map.computeIfAbsent(pair.component1(),
-                k -> new ArrayList<>());
+                    k -> new ArrayList<>());
             AssignedTimeSeries assignedTimeSeries = pair.component2();
             if (assignedTimeSeries != null) {
                 list.add(assignedTimeSeries);
