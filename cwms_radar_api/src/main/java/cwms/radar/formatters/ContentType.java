eccfecff65cdae9536d41e30a2bd0a744a3af6c3
ryan <ryan@rmanet.com>
2022-12-27 16:56:13 -0800

    Handle charset as a field.

--- cwms_radar_api/src/main/java/cwms/radar/formatters/ContentType.java
+++ cwms_radar_api/src/main/java/cwms/radar/formatters/ContentType.java
@@ -1,47 +1,69 @@
 package cwms.radar.formatters;
 
-import java.util.HashMap;
+import com.google.common.flogger.FluentLogger;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
 public class ContentType implements Comparable<ContentType> {
-    private static Logger logger = Logger.getLogger(ContentType.class.getName());
-    private String contentType;
-    private Map<String,String> parameters;
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    public static final String PARAM_DELIM = ";";
+    public static final String ELEM_DELIM = "=";
+    public static final String CHARSET = "charset";
+    private String mediaType;
+    private Map<String, String> parameters;
 
-    public ContentType(String contentTypeHeader){
-        parameters = new HashMap<>();
-        String parts[] = contentTypeHeader.split(";");
-        contentType = parts[0];
-        if( parts.length > 1){
-            for( int i = 1; i < parts.length; i++){
-                String key_val[] = parts[i].split("=");
-                parameters.put(key_val[0],key_val[1]);
+    private String charset = null;
+
+    public ContentType(String contentTypeHeader) {
+        parameters = new LinkedHashMap<>();
+        String[] parts = contentTypeHeader.split(PARAM_DELIM);
+        mediaType = parts[0];
+        if (parts.length > 1) {
+            for (int i = 1; i < parts.length; i++) {
+                String[] key_val = parts[i].split(ELEM_DELIM);
+                if (key_val.length == 2) {
+                    String key = key_val[0].trim();
+                    String value = key_val[1].trim();
+                    if (CHARSET.equalsIgnoreCase(key)) {
+                        charset = value;
+                    } else {
+                        parameters.put(key, value);
+                    }
+                }
             }
         }
-
     }
 
-    public String getType(){ return contentType; }
-    public Map<String,String> getParameters() {
-        return new HashMap<>(parameters);
+    public String getType() {
+        return mediaType;
+    }
+
+    public Map<String, String> getParameters() {
+        return new LinkedHashMap<>(parameters);
+    }
+
+    public String getCharset() {
+        return charset;
     }
 
     @Override
-    public boolean equals(Object other){
-        logger.finest("Checking + " + this.toString() + " vs " + other.toString());
-        if(!(other instanceof ContentType) ) return false;
-        ContentType o = (ContentType)other;
-        if(!(contentType.equals(o.contentType))) return false;
-        for( String key: parameters.keySet() ){
-            if( key.equals("q")) continue; // we don't care about q for equals
-            if( !parameters.get(key).equals(o.parameters.get(key))) return false;
+    public boolean equals(Object other) {
+        logger.atFinest().log("Checking %s vs %s", this, other);
+        if (!(other instanceof ContentType)) return false;
+        ContentType o = (ContentType) other;
+        if (!(mediaType.equals(o.mediaType))) return false;
+
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            String key = entry.getKey();
+            if (key.equals("q")) continue; // we don't care about q for equals
+            if (!entry.getValue().equals(o.parameters.get(key))) return false;
         }
+
         return true;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return this.toString().hashCode();
     }
 
@@ -49,18 +71,21 @@ public class ContentType implements Comparable<ContentType> {
     public int compareTo(ContentType o) {
         float myPriority = Float.parseFloat(parameters.getOrDefault("q", "1"));
         float otherPriority = Float.parseFloat(parameters.getOrDefault("q", "1"));
-        if( myPriority == otherPriority) return 0;
-        else if( myPriority > otherPriority ) return 1;
+        if (myPriority == otherPriority) return 0;
+        else if (myPriority > otherPriority) return 1;
         else return -1;
     }
 
     @Override
-    public String toString(){
-        StringBuilder builder = new StringBuilder(contentType);
-        for( String key: parameters.keySet()){
-            if( key.equals("q")) continue;
-            builder.append(";").append(key).append("=").append(parameters.get(key));
+    public String toString() {
+        StringBuilder builder = new StringBuilder(mediaType);
+
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            String key = entry.getKey();
+            if (key.equals("q")) continue;
+            builder.append(PARAM_DELIM).append(key).append(ELEM_DELIM).append(entry.getValue());
         }
+
         return builder.toString();
     }
 }
