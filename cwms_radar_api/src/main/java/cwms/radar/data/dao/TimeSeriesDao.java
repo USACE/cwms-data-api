3c0959b68090a53bc50d1c704b7dae803c6f4657
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2022-12-28 13:17:44 -0800

    Adding support for multiple delete types.

--- cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesDao.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/TimeSeriesDao.java
@@ -1,31 +1,47 @@
 package cwms.radar.data.dao;
 
+import cwms.radar.data.dto.Catalog;
+import cwms.radar.data.dto.RecentValue;
+import cwms.radar.data.dto.TimeSeries;
 import java.sql.Timestamp;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-import cwms.radar.data.dto.Catalog;
-import cwms.radar.data.dto.RecentValue;
-import cwms.radar.data.dto.TimeSeries;
+public interface TimeSeriesDao {
+    enum DeleteMethod {
+        DELETE_ALL, DELETE_KEY, DELETE_DATA
+    }
 
-public interface TimeSeriesDao
-{
-	Timestamp NON_VERSIONED = null;
-	Catalog getTimeSeriesCatalog(String cursor, int pageSize, String office);
-	Catalog getTimeSeriesCatalog(String cursor, int pageSize, String office, String idLike, String locCategoryLike, String locGroupLike, String tsCategoryLike, String tsGroupLike);
+    Timestamp NON_VERSIONED = null;
 
-	void create(TimeSeries timeSeries);
-	void store(TimeSeries timeSeries, Timestamp versionDate);
+    Catalog getTimeSeriesCatalog(String cursor, int pageSize, String office);
 
-	void delete(String office, String tsId);
+    Catalog getTimeSeriesCatalog(String cursor, int pageSize, String office, String idLike,
+                                 String locCategoryLike, String locGroupLike,
+                                 String tsCategoryLike, String tsGroupLike);
 
-	TimeSeries getTimeseries(String cursor, int pageSize, String names, String office, String unit, String datum, ZonedDateTime begin, ZonedDateTime end, ZoneId timezone);
-	String getTimeseries(String format, String names, String office, String unit, String datum, ZonedDateTime begin, ZonedDateTime end, ZoneId timezone);
+    void create(TimeSeries timeSeries);
+
+    void store(TimeSeries timeSeries, Timestamp versionDate);
+
+    void deleteAll(String office, String tsId);
+
+    void deleteData(String office, String tsId);
+
+    void deleteKey(String office, String tsId);
+
+    TimeSeries getTimeseries(String cursor, int pageSize, String names, String office,
+                             String unit, String datum, ZonedDateTime begin, ZonedDateTime end,
+                             ZoneId timezone);
+
+    String getTimeseries(String format, String names, String office, String unit, String datum,
+                         ZonedDateTime begin, ZonedDateTime end, ZoneId timezone);
 
 
+    List<RecentValue> findRecentsInRange(String office, String categoryId, String groupId,
+                                         Timestamp pastLimit, Timestamp futureLimit);
 
-	List<RecentValue> findRecentsInRange(String office, String categoryId, String groupId, Timestamp pastLimit, Timestamp futureLimit);
-	List<RecentValue> findMostRecentsInRange(List<String> tsIds, Timestamp pastLimit, Timestamp futureLimit);
+    List<RecentValue> findMostRecentsInRange(List<String> tsIds, Timestamp pastLimit, Timestamp futureLimit);
 
 }
