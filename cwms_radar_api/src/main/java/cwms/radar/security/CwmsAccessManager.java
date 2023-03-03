ced09e936b3bd75bf67c6453f470f061fe5d1aa2
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2023-03-02 17:46:51 -0800

    Make CwmsAccessManager tolerant of null user and/or office.

--- cwms_radar_api/src/main/java/cwms/radar/security/CwmsAccessManager.java
+++ cwms_radar_api/src/main/java/cwms/radar/security/CwmsAccessManager.java
@@ -8,7 +8,6 @@ import cwms.radar.datasource.ConnectionPreparingDataSource;
 import cwms.radar.datasource.DelegatingConnectionPreparer;
 import cwms.radar.datasource.DirectUserPreparer;
 import cwms.radar.datasource.SessionOfficePreparer;
-import cwms.radar.datasource.SessionUserPreparer;
 import cwms.radar.spi.RadarAccessManager;
 import io.javalin.core.security.RouteRole;
 import io.javalin.http.Context;
@@ -16,33 +15,29 @@ import io.javalin.http.Handler;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.security.Principal;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class CwmsAccessManager extends RadarAccessManager {
     public static final Logger logger = Logger.getLogger(CwmsAccessManager.class.getName());
-    public static final String DATABASE = "database";
 
     @Override
     public void manage(@NotNull Handler handler, @NotNull Context ctx,
-					   @NotNull Set<RouteRole> requiredRoles)
+                       @NotNull Set<RouteRole> requiredRoles)
             throws Exception {
         boolean shouldProceed = isAuthorized(ctx, requiredRoles);
 
-        if (shouldProceed) {            
+        if (shouldProceed) {
             buildDataSource(ctx);
 
             // Let the handler handle the request.
@@ -56,27 +51,67 @@ public class CwmsAccessManager extends RadarAccessManager {
     }
 
     private void buildDataSource(@NotNull Context ctx) {
-        String user = ctx.req.getUserPrincipal().getName();
-        String office =ctx.attribute("office");        
+        ConnectionPreparer userPreparer = null;
+        Optional<String> user = getUser(ctx);
+        if(user.isPresent()) {
+            userPreparer = new DirectUserPreparer(user.get());
+        }
 
-        ConnectionPreparer userPreparer = new DirectUserPreparer(user);
-        ConnectionPreparer officePrepare = new SessionOfficePreparer(office);
-        //ConnectionPreparer resetPreparer = new DirectUserPreparer("q0webtest");
-        DelegatingConnectionPreparer newPreparer = new DelegatingConnectionPreparer(officePrepare,userPreparer);
-        DataSource dataSource = ctx.attribute(ApiServlet.DATA_SOURCE);
-        if(dataSource instanceof ConnectionPreparingDataSource) {
-            ConnectionPreparingDataSource cpDs = (ConnectionPreparingDataSource)    dataSource;
-            ConnectionPreparer existingPreparer = cpDs.getPreparer();
+        ConnectionPreparer officePrepare = null;
+        Optional<String> office = getOffice(ctx);
+        if(office.isPresent()) {
+            officePrepare = new SessionOfficePreparer(office.get());
+        }
 
-            // Have it do our extra step last.
-            cpDs.setPreparer(new DelegatingConnectionPreparer(existingPreparer, newPreparer));
+        if(user.isPresent() || office.isPresent()) {
+            DelegatingConnectionPreparer newPreparer = new DelegatingConnectionPreparer(officePrepare, userPreparer);
+            DataSource dataSource = ctx.attribute(ApiServlet.DATA_SOURCE);
+            if (dataSource instanceof ConnectionPreparingDataSource) {
+                ConnectionPreparingDataSource cpDs = (ConnectionPreparingDataSource) dataSource;
+                ConnectionPreparer existingPreparer = cpDs.getPreparer();
+
+                // Have it do our extra step last.
+                cpDs.setPreparer(new DelegatingConnectionPreparer(existingPreparer, newPreparer));
+            } else {
+                ctx.attribute(ApiServlet.DATA_SOURCE, new ConnectionPreparingDataSource(newPreparer, dataSource));
+            }
         } else {
-            ctx.attribute(ApiServlet.DATA_SOURCE, new ConnectionPreparingDataSource(newPreparer, dataSource));
+            logger.log(Level.FINE, "No user or office found in request, not adding a DataSource Preparer.");
         }
     }
 
-    @NotNull
-    private String getFailMessage(@NotNull Context ctx, @NotNull Set<RouteRole> requiredRoles) {
+    private static Optional<String> getOffice(Context ctx) {
+        Optional<String> retval = Optional.empty();
+        Map<String, Object> attributeMap = Collections.emptyMap();
+        if (ctx != null) {
+            attributeMap = ctx.attributeMap();
+        }
+
+        if(attributeMap.containsKey("office")) {
+            String attr = String.valueOf(attributeMap.get("office"));
+            if(attr != null && !attr.isEmpty()) {
+                retval = Optional.of(attr);
+            } else {
+                logger.log(Level.FINE, "office attribute value was null or empty");
+            }
+        } else {
+            logger.log(Level.FINE, "No office attribute");
+        }
+
+        return retval;
+    }
+
+    private static Optional<String> getUser(Context ctx) {
+        Optional<String> retval = Optional.empty();
+        if (ctx != null && ctx.req != null && ctx.req.getUserPrincipal() != null) {
+            retval = Optional.of(ctx.req.getUserPrincipal().getName());
+        } else {
+            logger.log(Level.FINE, "No user principal found in request.");
+        }
+        return retval;
+    }
+
+    public static String getFailMessage(@NotNull Context ctx, @NotNull Set<RouteRole> requiredRoles) {
         Set<RouteRole> specifiedRoles = getRoles(ctx);
         Set<RouteRole> missing = new LinkedHashSet<>(requiredRoles);
         missing.removeAll(specifiedRoles);
@@ -96,7 +131,7 @@ public class CwmsAccessManager extends RadarAccessManager {
         return retval;
     }
 
-    public Set<RouteRole> getRoles(Context ctx) {
+    public static Set<RouteRole> getRoles(Context ctx) {
         Set<RouteRole> retval = new LinkedHashSet<>();
         if (ctx != null) {
             Principal principal = ctx.req.getUserPrincipal();
@@ -109,41 +144,8 @@ public class CwmsAccessManager extends RadarAccessManager {
         return retval;
     }
 
-    @Nullable
-    private String getSessionKey(@NotNull Context ctx) {
-        String sessionKey = null;
 
-        Principal principal = ctx.req.getUserPrincipal();
-        if (principal != null) {
-            try {
-                CwmsUserPrincipal cup = (CwmsUserPrincipal) principal;
-                sessionKey = cup.getSessionKey();
-            } catch (ClassCastException e) {
-                // The object is created by cwms_aaa with the cwms_aaa classloader.
-                // It's a CwmsUserPrincipal but it's not our CwmsUserPrincipal.
-                sessionKey = callGetSessionKeyReflectively(principal);
-            }
-        }
-        return sessionKey;
-    }
-
-    private String callGetSessionKeyReflectively(Principal principal) {
-        String sessionKey = null;
-        Method getSessionKeyMethod;
-        try {
-            getSessionKeyMethod = principal.getClass().getMethod("getSessionKey", new Class[]{});
-            Object retval = getSessionKeyMethod.invoke(principal, new Object[]{});
-            if (retval instanceof String) {
-                sessionKey = (String) retval;
-            }
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            logger.log(Level.WARNING, "Could not call getSessionKey() on principal.", e);
-        }
-
-        return sessionKey;
-    }
-
-    private Set<RouteRole> getRoles(Principal principal) {
+    private static Set<RouteRole> getRoles(Principal principal) {
         Set<RouteRole> retval = new LinkedHashSet<>();
         if (principal != null) {
             List<String> roleNames;
@@ -154,24 +156,24 @@ public class CwmsAccessManager extends RadarAccessManager {
                     roleNames.stream().map(CwmsAccessManager::buildRole).forEach(retval::add);
                 }
                 logger.fine("Principal had roles: " + retval);
-            } 
-            catch (ClassCastException e) {
-               logger.severe("cwmsaaa api and implementation jars should only be in the system classpath, not the war file. Verify and restart applciation");
+            } catch (ClassCastException e) {
+                logger.severe("cwmsaaa api and implementation jars should only be in the system "
+                        + "classpath, not the war file. Verify and restart application");
             }
         }
-        return retval;            
+        return retval;
     }
 
     public static RouteRole buildRole(String roleName) {
         return new Role(roleName);
     }
 
-	@Override
-	public SecurityScheme getScheme() {		
-		return new SecurityScheme()
-					.type(Type.APIKEY)
-					.in(In.COOKIE)					
-					.name("JSESSIONIDSSO");
-	}
+    @Override
+    public SecurityScheme getScheme() {
+        return new SecurityScheme()
+                .type(Type.APIKEY)
+                .in(In.COOKIE)
+                .name("JSESSIONIDSSO");
+    }
 
 }
