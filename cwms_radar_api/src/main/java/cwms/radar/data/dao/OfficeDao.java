7dcd5031e2f3596a33ee7647784fc3dc1af81efe
Charles <krowven@gmail.com>
2023-03-09 20:42:52 -0600

    has-data flag for getOffices

--- cwms_radar_api/src/main/java/cwms/radar/data/dao/OfficeDao.java
+++ cwms_radar_api/src/main/java/cwms/radar/data/dao/OfficeDao.java
@@ -6,36 +6,53 @@ import java.util.Optional;
 import cwms.radar.data.dto.Office;
 import org.jooq.DSLContext;
 import org.jooq.Record4;
+import org.jooq.Table;
 
 import usace.cwms.db.jooq.codegen.tables.AV_OFFICE;
+import usace.cwms.db.jooq.codegen.tables.AV_LOC2;
 
 public class OfficeDao extends JooqDao<Office>
 {
 	public OfficeDao(DSLContext dsl)
 	{
-		super(dsl);
-	}
+        super(dsl);
+    }
 
+    public List<Office> getOffices(Boolean hasData) {
+        List<Office> retval;
+        AV_OFFICE view = AV_OFFICE.AV_OFFICE;
 
-	public List<Office> getOffices() {
-		List<Office> retval;
-		AV_OFFICE view = AV_OFFICE.AV_OFFICE;
-		// The .as snippets lets it map directly into the Office ctor fields.
-		retval = dsl.select(view.OFFICE_ID.as("name"), view.LONG_NAME, view.OFFICE_TYPE.as("type"),
-				view.REPORT_TO_OFFICE_ID.as("reportsTo")).from(view).fetch().into(
-				Office.class);
+        if (hasData) {
+            // #321 - Return offices with operation data only i.e. hasData
+            AV_LOC2 locView = AV_LOC2.AV_LOC2;
+            Table<?> locViewDistinct = dsl.selectDistinct(locView.DB_OFFICE_ID)
+                    .from(locView)
+                    .where(locView.LOCATION_CODE.ne(0L).and(locView.DB_OFFICE_ID.ne("CWMS")))
+                    .asTable("locViewDistinct");
+            retval = dsl.select(view.OFFICE_ID.as("name"), view.LONG_NAME, view.OFFICE_TYPE.as("type"),
+                    view.REPORT_TO_OFFICE_ID.as("reportsTo"))
+                    .from(view)
+                    .join(locViewDistinct).on(view.OFFICE_ID.eq(locViewDistinct.field(locView.DB_OFFICE_ID)))
+                    .fetch()
+                    .into(Office.class);
+        } else {
+            // The .as snippets lets it map directly into the Office ctor fields.
+            retval = dsl.select(view.OFFICE_ID.as("name"), view.LONG_NAME, view.OFFICE_TYPE.as("type"),
+                    view.REPORT_TO_OFFICE_ID.as("reportsTo")).from(view).fetch().into(
+                            Office.class);
+        }
 
-		return retval;
-	}
+        return retval;
+    }
 
-	public Optional<Office> getOfficeById(String officeId) {
-		AV_OFFICE view = AV_OFFICE.AV_OFFICE;
-		// The .as snippets lets it map directly into the Office ctor fields.
+    public Optional<Office> getOfficeById(String officeId) {
+        AV_OFFICE view = AV_OFFICE.AV_OFFICE;
+        // The .as snippets lets it map directly into the Office ctor fields.
 		Record4<String, String, String, String> fetchOne = dsl.select(view.OFFICE_ID.as("name"), view.LONG_NAME, view.OFFICE_TYPE.as("type"),
 				view.REPORT_TO_OFFICE_ID.as("reportsTo")).from(view).where(view.OFFICE_ID.eq(officeId)).fetchOne();
 		return fetchOne != null ?
 				Optional.of(fetchOne.into(Office.class)) : Optional.empty();
-	}
+    }
 
 
 }
