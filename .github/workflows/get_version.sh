7cf59f32d7cc97e03f0a79743e317c71bd61de2e
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-06-12 17:27:26 +0000

    update build.yml for current actions environment design.

--- .github/workflows/get_version.sh
+++ .github/workflows/get_version.sh
@@ -1,4 +1,4 @@
 VERSION=`./gradlew --init-script init.gradle properties -q | grep "^version:" | awk '{ print $2}'`
 WARFILE="cwms-data-api/build/libs/cwms-data-api-tomcat-$VERSION.war"
-echo "::set-output name=VERSION::$VERSION"
-echo "::set-output name=WAR_FILE_NAME::$WARFILE"
+echo "VERSION=$VERSION" >> $GITHUB_ENV
+echo "WAR_FILE_NAME=$WARFILE" >> $GITHUB_ENV
