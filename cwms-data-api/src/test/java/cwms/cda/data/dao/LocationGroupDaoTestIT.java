bc43bda261855876137f1b0eb4514be5792e9b0a
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2023-09-13 16:37:14 -0700

    Added location category regex to LocationGroupDao

--- cwms-data-api/src/test/java/cwms/cda/data/dao/LocationGroupDaoTestIT.java
+++ cwms-data-api/src/test/java/cwms/cda/data/dao/LocationGroupDaoTestIT.java
@@ -1,7 +1,10 @@
 package cwms.cda.data.dao;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import cwms.cda.api.DataApiTestIT;
+import cwms.cda.data.dto.LocationGroup;
 import fixtures.CwmsDataApiSetupCallback;
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +13,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import cwms.cda.api.DataApiTestIT;
-import cwms.cda.data.dto.LocationGroup;
 
 @Tag("integration")
 class LocationGroupDaoTestIT extends DataApiTestIT {
@@ -43,6 +43,71 @@ class LocationGroupDaoTestIT extends DataApiTestIT {
             }
         });
     }
+
+    @Test
+    void testGetAllWithCategoryRegex() throws Exception {
+        CwmsDatabaseContainer<?> db = CwmsDataApiSetupCallback.getDatabaseLink();
+        db.connection(c -> {
+            try {
+                LocationGroupDao dao = new LocationGroupDao(dslContext(c));
+                List<LocationGroup> groups = dao.getLocationGroups("SPK", "Test Category3");
+                Optional<LocationGroup> group3 = groups.stream()
+                        .filter(g -> "Test Group3".equals(g.getId()))
+                        .findFirst();
+                Optional<LocationGroup> group2 = groups.stream()
+                        .filter(g -> "Test Group2".equals(g.getId()))
+                        .findFirst();
+                assertTrue(group3.isPresent());
+                assertFalse(group2.isPresent());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Test
+    void testGetAllWithCategoryRegexStar() throws Exception {
+        CwmsDatabaseContainer<?> db = CwmsDataApiSetupCallback.getDatabaseLink();
+        db.connection(c -> {
+            try {
+                LocationGroupDao dao = new LocationGroupDao(dslContext(c));
+                List<LocationGroup> groups = dao.getLocationGroups("SPK", ".*Category3");
+                Optional<LocationGroup> group3 = groups.stream()
+                        .filter(g -> "Test Group3".equals(g.getId()))
+                        .findFirst();
+                Optional<LocationGroup> group2 = groups.stream()
+                        .filter(g -> "Test Group2".equals(g.getId()))
+                        .findFirst();
+                assertTrue(group3.isPresent());
+                assertFalse(group2.isPresent());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Test
+    void testGetAllWithCategoryRegexStarFindsBoth() throws Exception {
+        CwmsDatabaseContainer<?> db = CwmsDataApiSetupCallback.getDatabaseLink();
+        db.connection(c -> {
+            try {
+                LocationGroupDao dao = new LocationGroupDao(dslContext(c));
+                List<LocationGroup> groups = dao.getLocationGroups("SPK", "Test Category.*");
+                Optional<LocationGroup> group3 = groups.stream()
+                        .filter(g -> "Test Group3".equals(g.getId()))
+                        .findFirst();
+                Optional<LocationGroup> group2 = groups.stream()
+                        .filter(g -> "Test Group2".equals(g.getId()))
+                        .findFirst();
+                assertTrue(group3.isPresent());
+                assertTrue(group2.isPresent());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
 
     @Test
     void testGetOneWithoutAssignedLocations() throws Exception {
