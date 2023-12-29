b227e734c7eaf379f6380219add238ed5e85fa46
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2023-12-28 16:23:52 -0800

    Added documentation to StoreRule usage and switched to using arguments with space ("DELETE INSERT" and not "DELETE_INSERT")

--- cwms-data-api/src/test/java/cwms/cda/data/dao/StoreRuleTest.java
+++ cwms-data-api/src/test/java/cwms/cda/data/dao/StoreRuleTest.java
@@ -14,4 +14,19 @@ class StoreRuleTest {
         assertEquals("REPLACE ALL", rule.getRule());
     }
 
+    @Test
+    public void testGetStoreRuleSpace() {
+        StoreRule rule = StoreRule.getStoreRule("REPLACE ALL");
+        assertNotNull(rule);
+        assertEquals(StoreRule.REPLACE_ALL, rule);
+        assertEquals("REPLACE ALL", rule.getRule());
+    }
+
+    @Test
+    public void testToStringSpaces() {
+        String replaceAllStr = StoreRule.REPLACE_ALL.toString();
+
+        assertEquals("REPLACE ALL", replaceAllStr);
+    }
+
 }
