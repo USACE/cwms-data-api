5f7504fcf7784267d82b89d0a60dd19c61f477b6
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-01-31 10:33:20 -0800

    get ws example working

--- cwms-data-api/src/main/java/cwms/cda/api/ClobAsyncController.java
+++ cwms-data-api/src/main/java/cwms/cda/api/ClobAsyncController.java
@@ -1,6 +1,7 @@
 package cwms.cda.api;
 
 import com.google.common.flogger.FluentLogger;
+import cwms.cda.ApiServlet;
 import cwms.cda.data.dao.JooqDao;
 import io.javalin.http.Context;
 import io.javalin.http.sse.SseClient;
@@ -12,9 +13,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
+import javax.sql.DataSource;
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 public class ClobAsyncController  {
     public static final FluentLogger logger = FluentLogger.forEnclosingClass();
@@ -24,25 +29,92 @@ public class ClobAsyncController  {
     }
 
     public Consumer<WsConfig> getWsConsumer() {
-        logger.atInfo().log("getWsConsumer");
         return this::acceptWs;
     }
 
     private void acceptWs(WsConfig ws) {
-        logger.atInfo().log("acceptWs");
 
         ws.onConnect(ctx -> {
-            logger.atInfo().log("ws onConnect");
+            logger.atInfo().log("#5--controller ws onConnect"); //#5 happens after #4
 
             String clobId = ctx.queryParam("id");
             String officeId= ctx.queryParam("officeId");
 
-            ctx.send("Hello clob Client! ");
+            Map<String, Object> servletAttributes = ctx.getUpgradeReq$javalin().getServletAttributes();
 
-            new Thread(() -> sendClob(null, officeId, clobId, reader -> {
-                sendToClient(reader, str -> ctx.send( str));
-            })).start();
+            Object ds = servletAttributes.get(ApiServlet.DATA_SOURCE);
 
+            DSLContext dsl = DSL.using((DataSource) ds, SQLDialect.ORACLE18C);
+
+//            sendClob(dsl, officeId, clobId, reader -> {
+//                sendToClient(reader, ctx::send);
+//            });
+
+
+
+
+
+            new Thread(() -> {
+                // For websockets this thread eventually does this, which might not be an error, it might just be what happens when the socket times out?
+                // Notice that the clob is "42,303,148 bytes" according to the filesystem.
+                // This could just be because I have jetty logging turned too high.
+                // Jan 31, 2024 9:49:04 AM cwms.cda.api.ClobAsyncController sendToClient
+                //INFO: Sending 32428 bytes to client. Total sent: 42303148
+                //2024-01-31 09:49:04.000:DBUG:oejwc.WebSocketSession:Thread-15: [SERVER] WebSocketSession.getRemote()
+                //2024-01-31 09:49:04.000:DBUG:oejwc.WebSocketRemoteEndpoint:Thread-15: sendStringByFuture with HeapByteBuffer@62df1514[p=0,l=32428,c=32428,r=32428]={<<<21-08-04_Batch_File_Utili...ort was removed in 8.0\n>>>}
+                //2024-01-31 09:49:04.000:DBUG:oejwce.ExtensionStack:Thread-15: Queuing TEXT[len=32428,fin=true,rsv=...,masked=false]
+                //2024-01-31 09:49:04.000:DBUG:oejwce.ExtensionStack:Thread-15: Processing TEXT[len=32428,fin=true,rsv=...,masked=false]
+                //2024-01-31 09:49:04.000:DBUG:oejwcec.PerMessageDeflateExtension:Thread-15: nextOutgoingFrame(TEXT[len=2016,fin=true,rsv=1..,masked=false])
+                //2024-01-31 09:49:04.000:DBUG:oejwci.AbstractWebSocketConnection:Thread-15: outgoingFrame(TEXT[len=2016,fin=true,rsv=1..,masked=false], org.eclipse.jetty.websocket.common.extensions.compress.CompressExtension$Flusher$1@1c316666)
+                //2024-01-31 09:49:04.000:DBUG:oejwci.FrameFlusher:Thread-15: Enqueued FrameEntry[TEXT[len=2016,fin=true,rsv=1..,masked=false],org.eclipse.jetty.websocket.common.extensions.compress.CompressExtension$Flusher$1@1c316666,AUTO,null] to Flusher@352c2c26[IDLE][queueSize=1,aggregateSize=-1,terminated=null]
+                //2024-01-31 09:49:04.000:DBUG:oejwci.FrameFlusher:Thread-15: Flushing Flusher@352c2c26[PROCESSING][queueSize=1,aggregateSize=-1,terminated=null]
+                //2024-01-31 09:49:04.000:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=-1,terminated=null] processing 1 entries: [FrameEntry[TEXT[len=2016,fin=true,rsv=1..,masked=false],org.eclipse.jetty.websocket.common.extensions.compress.CompressExtension$Flusher$1@1c316666,AUTO,null]]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] acquired aggregate buffer java.nio.DirectByteBuffer[pos=0 lim=0 cap=32768]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] aggregated 1 frames: [FrameEntry[TEXT[len=0,fin=true,rsv=1..,masked=false],org.eclipse.jetty.websocket.common.extensions.compress.CompressExtension$Flusher$1@1c316666,AUTO,null]]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FutureWriteCallback:Thread-15: .writeSuccess
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flushing Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] processing 0 entries: []
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] auto flushing
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] flushing aggregate java.nio.DirectByteBuffer[pos=0 lim=2020 cap=32768]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=0,terminated=null] flushing 0 frames: []
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flushing Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=2020,terminated=null]
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=2020,terminated=null] processing 0 entries: []
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=2020,terminated=null] auto flushing
+                //2024-01-31 09:49:04.001:DBUG:oejwci.FrameFlusher:Thread-15: Flusher@352c2c26[PROCESSING][queueSize=0,aggregateSize=2020,terminated=null] flushing 0 frames: []
+                //2024-01-31 09:49:04.001:DBUG:oejwce.ExtensionStack:Thread-15: Entering IDLE
+                //2024-01-31 09:54:04.007:DBUG:oejwc.WebSocketSession:Connector-Scheduler-2141a12-1: callApplicationOnError()
+                //org.eclipse.jetty.websocket.api.CloseException: java.util.concurrent.TimeoutException: Idle timeout expired: 300005/300000 ms
+                //	at org.eclipse.jetty.websocket.common.io.AbstractWebSocketConnection.onReadTimeout(AbstractWebSocketConnection.java:564)
+                //	at org.eclipse.jetty.io.AbstractConnection.onFillInterestedFailed(AbstractConnection.java:172)
+                //	at org.eclipse.jetty.websocket.common.io.AbstractWebSocketConnection.onFillInterestedFailed(AbstractWebSocketConnection.java:539)
+                //	at org.eclipse.jetty.io.AbstractConnection$ReadCallback.failed(AbstractConnection.java:317)
+                //	at org.eclipse.jetty.io.FillInterest.onFail(FillInterest.java:140)
+                //	at org.eclipse.jetty.io.AbstractEndPoint.onIdleExpired(AbstractEndPoint.java:407)
+                //	at org.eclipse.jetty.io.IdleTimeout.checkIdleTimeout(IdleTimeout.java:171)
+                //	at org.eclipse.jetty.io.IdleTimeout.idleCheck(IdleTimeout.java:113)
+                //	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)
+                //	at java.util.concurrent.FutureTask.run(FutureTask.java:266)
+                //	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$201(ScheduledThreadPoolExecutor.java:180)
+                //	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:293)
+                //	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+                //	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+                //	at java.lang.Thread.run(Thread.java:748)
+                //Caused by:
+                //java.util.concurrent.TimeoutException: Idle timeout expired: 300005/300000 ms
+                //	at org.eclipse.jetty.io.IdleTimeout.checkIdleTimeout(IdleTimeout.java:171)
+                //	at org.eclipse.jetty.io.IdleTimeout.idleCheck(IdleTimeout.java:113)
+                //	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)
+                //	at java.util.concurrent.FutureTask.run(FutureTask.java:266)
+                //	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$201(ScheduledThreadPoolExecutor.java:180)
+                //	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:293)
+                //	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+                //	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+                //	at java.lang.Thread.run(Thread.java:748)
+                //2024-01-31 09:54:04.007:DBUG:oejwce.JettyAnnotatedEventDriver:Connector-Scheduler-2141a12-1: onError(org.eclipse.jetty.websocket.api.CloseException) - events.onError=OptionalSessionCallableMethod[pojo=io.javalin.websocket.WsConnection,method=public final void io.javalin.websocket.WsConnection.onError(org.eclipse.jetty.websocket.api.Session,java.lang.Throwable),wantsSession=true,streaming=false]
+                //Jan 31, 2024 9:54:04 AM cwms.cda.api.ClobAsyncController lambda$acceptWs$5
+                //INFO: ws onError
+                sendClob(dsl, officeId, clobId, reader -> sendToClient(reader, ctx::send));
+            }).start();
         });
         ws.onClose(ctx -> {
            logger.atInfo().log("ws onClose");
@@ -68,8 +140,8 @@ public class ClobAsyncController  {
         client.onClose(() -> logger.atInfo().log("Clob client left."));
 
         // call sendClob on new thread
-        new Thread(() -> sendClob(client.ctx, officeId, clobId, reader -> {
-//            client.sendEvent("message", "clob data would go here\n");
+        new Thread(() -> sendClob(JooqDao.getDslContext(client.ctx), officeId, clobId, reader -> {
+
             sendToClient(reader, str -> client.sendEvent("message", str));
             client.sendEvent("close", "");  // normally the server just closing the connection would make the browser restart their side.
 //            client.close();
@@ -95,8 +167,8 @@ public class ClobAsyncController  {
 
 
 
-    private static void sendClob(Context ctx, String officeId, String clobId, Consumer<Reader> readerConsumer) {
-        DSLContext dslContext = JooqDao.getDslContext(ctx);
+    private static void sendClob(DSLContext dslContext, String officeId, String clobId, Consumer<Reader> readerConsumer) {
+
         dslContext.connection(
                 connection -> {
                     String sql = "select cwms_20.AV_CLOB.VALUE from "
