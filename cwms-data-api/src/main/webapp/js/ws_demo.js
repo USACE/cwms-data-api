963e98d95f5bf8a51eb5aad75cf655aa83b8540f
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-01-31 10:36:26 -0800

    ws_demo files.

--- cwms-data-api/src/main/webapp/js/ws_demo.js
+++ cwms-data-api/src/main/webapp/js/ws_demo.js
@@ -1,31 +1,97 @@
-// small helper function for selecting element by id
-let id = id => document.getElementById(id);
 
-let clobId = "/TIME SERIES TEXT/6261044";
+var webSocket;
+var messageDiv;
+var textInput;
 
-let addr = "ws://localhost:7000/cwms-data/ws/clob"
-    // +  "?id=" + encodeURI(clobId) + "&officeId=SPK"
-let ws = new WebSocket(addr);
-ws.onmessage = msg => updateChat(msg);
-// ws.onclose = () => alert("WebSocket connection closed");
+var websocketReadyStateArray;
+var connectBtn;
+var sendTextBtn;
+var sendJSONObjectBtn;
+var disconnectBtn;
+function init(){
+    messageDiv = document.getElementById("message");
+    textInput = document.getElementById("text");
 
-// Add event listeners to button and input field
-id("send").addEventListener("click", () => sendAndClear(id("message").value));
-id("message").addEventListener("keypress", function (e) {
-    if (e.keyCode === 13) { // Send message if enter is pressed in input field
-        sendAndClear(e.target.value);
-    }
-});
+    websocketReadyStateArray = new Array('Connecting', 'Connected', 'Closing', 'Closed');
+    connectBtn = document.getElementById('connect');
+    sendTextBtn = document.getElementById('sendText');
+    sendJSONObjectBtn = document.getElementById('sendJSONObject');
+    disconnectBtn = document.getElementById('disconnect');
+    connectBtn.disabled = false;
+    sendTextBtn.disabled = true;
+    sendJSONObjectBtn.disabled = true;
+    disconnectBtn.disabled = true;
+}
+function connect(){
+    try{
 
-function sendAndClear(message) {
-    if (message !== "") {
-        ws.send(message);
-        id("message").value = "";
+        var clobId = "/TIME SERIES TEXT/6261044";
+        webSocket = new WebSocket("ws://localhost:7000/cwms-data/ws/clob"   + "?id=" + encodeURI(clobId) + "&officeId=SPK");
+        messageDiv.innerHTML = "<p>Socket status:" + websocketReadyStateArray[webSocket.readyState] + "</p>";
+        webSocket.onopen = function(){
+            messageDiv.innerHTML += "<p>Socket status:" + websocketReadyStateArray[webSocket.readyState] + "</p>";
+            connectBtn.disabled = true;
+            sendTextBtn.disabled = false;
+            sendJSONObjectBtn.disabled = false;
+            disconnectBtn.disabled = false;
+        }
+        webSocket.onmessage = function(msg){
+            messageDiv.innerHTML += "<p>Server response : " + msg.data + "</p>";
+        }
+        webSocket.onclose = function(){
+            messageDiv.innerHTML += "<p>Socket status:" + websocketReadyStateArray[webSocket.readyState] + "</p>";
+            connectBtn.disabled = false;
+            sendTextBtn.disabled = true;
+            sendJSONObjectBtn.disabled = true;
+            disconnectBtn.disabled = true;
+        }
+    }catch(exception){
+        messageDiv.innerHTML += 'Exception happen, ' + exception;
     }
 }
+function sendText(){
+    var sendText = textInput.value.trim();
+    if(sendText==''){
 
-function updateChat(msg) { // Update chat-panel and list of connected users
-    let data = JSON.parse(msg.data);
-    id("chat").insertAdjacentHTML("afterbegin", data.userMessage);
-    id("userlist").innerHTML = data.userlist.map(user => "<li>" + user + "</li>").join("");
+        messageDiv.innerHTML = '<p>Please input some text to send.</p>';
+        return;
+    }else{
+        try{
+
+            webSocket.send(sendText);
+            messageDiv.innerHTML = '<p>Send text : ' + sendText + '</p>'
+        }catch(exception){
+            messageDiv.innerHTML = '<p>Send error : ' + exception + '</p>'
+        }
+    }
+}
+function sendJSONOjbect(){
+    var sendText = textInput.value.trim();
+    if(sendText==''){
+
+        messageDiv.innerHTML = '<p>Please input some text to send.</p>';
+        return;
+    }else{
+        try{
+            currDate = new Date();
+            currHour = currDate.getHours();
+            currMinutes = currDate.getMinutes();
+            currSeconds = currDate.getSeconds();
+
+            currentTime = currHour + ':' + currMinutes + ':' + currSeconds;
+            jsonObj = {time:currentTime, text:sendText}
+            tmpSendText = JSON.stringify(jsonObj)
+            webSocket.send(tmpSendText);
+            messageDiv.innerHTML = '<p>Send JSON object : ' + tmpSendText + '</p>'
+        }catch(exception){
+            messageDiv.innerHTML = '<p>Send error : ' + exception + '</p>'
+        }
+    }
+}
+// When you focus on the input text box, it will call this function to select all the text in the input text box.
+function selectAll(){
+    textInput.select();
+}
+function disconnect(){
+    webSocket.close();
 }
