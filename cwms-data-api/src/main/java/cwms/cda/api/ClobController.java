c16e8485baf51442f2fd4266b669bb285577eae5
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-02-19 13:02:31 -0800

    Added ability to build urls to value instead of embedding large values. Read from the ctx.body as an InputStream. Added an png example. Added url fields Added methods to get requiredParams and required ZonedDateTimes.  These methods throw an exception if the required param isn't found.  Added exception block to ApiServlet that lets the users know what parameter wasn't found. Added some tests. Switching to UnaryOperator and Predicate<ResultSet>. Including url-template for clobs Passing contextPath to TextTimeSeriesController in-order to allow controller to build up url to clobs. Util for doing simple {name} string replacement

--- cwms-data-api/src/main/java/cwms/cda/api/ClobController.java
+++ cwms-data-api/src/main/java/cwms/cda/api/ClobController.java
@@ -51,6 +51,7 @@ import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiParam;
 import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.Objects;
@@ -260,7 +261,7 @@ public class ClobController implements CrudHandler {
             boolean failIfExists = ctx.queryParamAsClass(FAIL_IF_EXISTS, Boolean.class).getOrDefault(true);
 
             try {
-                Clob clob = deserialize(ctx.body(), formatHeader);
+                Clob clob = deserializeBody(ctx.bodyAsInputStream(), formatHeader);
 
                 if (clob.getOfficeId() == null) {
                     throw new FormattingException("An officeId is required when creating a clob");
@@ -278,10 +279,15 @@ public class ClobController implements CrudHandler {
                 ClobDao dao = new ClobDao(dsl);
                 dao.create(clob, failIfExists);
                 ctx.status(HttpCode.CREATED);
-            } catch (JsonProcessingException e) {
+            } catch (IOException e) {
                 throw new HttpResponseException(HttpCode.NOT_ACCEPTABLE.getStatus(),"Unable to parse request body");
             }
         }
+    }
+
+    private Clob deserializeBody(InputStream bodyStream, String formatHeader) throws IOException {
+        ObjectMapper om = getObjectMapperForFormat(formatHeader);
+        return om.readValue(bodyStream, Clob.class);
     }
 
     public Clob deserialize(String body, String formatHeader) throws JsonProcessingException {
@@ -348,7 +354,7 @@ public class ClobController implements CrudHandler {
             ClobDao dao = new ClobDao(dsl);
 
             try {
-                Clob clob = deserialize(ctx.body(), formatHeader);
+                Clob clob = deserializeBody(ctx.bodyAsInputStream(), formatHeader);
 
                 if (clob.getOfficeId() == null) {
                     throw new HttpResponseException(HttpCode.BAD_REQUEST.getStatus(),
@@ -363,7 +369,7 @@ public class ClobController implements CrudHandler {
                 }
 
                 dao.update(clob, ignoreNulls);
-            } catch (JsonProcessingException e) {
+            } catch (IOException e) {
                 throw new HttpResponseException(HttpCode.NOT_ACCEPTABLE.getStatus(),
                         "Unable to parse request body");
             }
