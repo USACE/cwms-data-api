f7ef532d68b7861874f820e1efa8033d8c3b44ab
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-02-21 14:48:14 +0000

    Tweaks to fix new and existing tests on fresh schema install.

--- cwms-data-api/src/test/java/cwms/cda/api/OfficeControllerIT.java
+++ cwms-data-api/src/test/java/cwms/cda/api/OfficeControllerIT.java
@@ -25,10 +25,13 @@
 package cwms.cda.api;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.withArgs;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
+
+import java.util.Arrays;
 
 import cwms.cda.formatters.Formats;
 import io.javalin.core.util.Header;
@@ -59,7 +62,8 @@ public class OfficeControllerIT extends DataApiTestIT {
                 .statusCode(is(HttpServletResponse.SC_OK))
                 .header(Header.ETAG, not(isEmptyOrNullString()))
                 .headers(Header.CACHE_CONTROL.toLowerCase(), containsString("max-age="))
-                .body("[0].name", CoreMatchers.equalTo("CPC"))
+                .rootPath("find {it.name == '%s'}", withArgs("CPC"))
+                .body("longname", CoreMatchers.equalTo("Central Processing Center"))
                 ;
     }
 
