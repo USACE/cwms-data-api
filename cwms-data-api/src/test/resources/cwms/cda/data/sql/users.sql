08cf37cb42cd62f0cf3489fc2300dd4a49100f15
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-02-21 14:48:14 +0000

    Tweaks to fix new and existing tests on fresh schema install.

--- cwms-data-api/src/test/resources/cwms/cda/data/sql/users.sql
+++ cwms-data-api/src/test/resources/cwms/cda/data/sql/users.sql
@@ -1,12 +1,24 @@
 begin
-  --cwms_sec.add_user_to_group('&user.','CWMS Users', 'HQ');
+  begin
+    cwms_sec.create_user('&webuser','&password',NULL,NULL);
+  exception
+    when dup_val_on_index then null; -- user already exists
+  end;
   
-  cwms_sec.add_user_to_group('l2webtest','CWMS Users', 'HQ');
-  cwms_sec.add_user_to_group('l2webtest','CWMS PD Users', 'HQ');
-  cwms_sec.add_user_to_group('l2webtest','CWMS DBA Users', 'HQ');
-  cwms_sec.add_user_to_group('l2webtest','CWMS Users', 'SPK');
-  cwms_sec.add_user_to_group('l2webtest','CWMS PD Users', 'SPK');
-  cwms_sec.add_user_to_group('l2webtest','CWMS DBA Users', 'SPK');
+  cwms_sec.add_user_to_group('&webuser','CWMS Users', 'HQ');
+  cwms_sec.add_user_to_group('&webuser','CWMS PD Users', 'HQ');
+  cwms_sec.add_user_to_group('&webuser','CWMS DBA Users', 'HQ');
+  cwms_sec.add_user_to_group('&webuser','CWMS Users', 'SPK');
+  cwms_sec.add_user_to_group('&webuser','CWMS PD Users', 'SPK');
+  cwms_sec.add_user_to_group('&webuser','CWMS DBA Users', 'SPK');
+  cwms_sec.add_user_to_group('&user','All Users', 'HQ');
+  cwms_sec.add_user_to_group('&user','CWMS Users', 'HQ');
+  cwms_sec.add_user_to_group('&user','CWMS PD Users', 'HQ');
+  cwms_sec.add_user_to_group('&user','CWMS DBA Users', 'HQ');
+  cwms_sec.add_user_to_group('&user','All Users', 'SPK');
+  cwms_sec.add_user_to_group('&user','CWMS Users', 'SPK');
+  cwms_sec.add_user_to_group('&user','CWMS PD Users', 'SPK');
+  cwms_sec.add_user_to_group('&user','CWMS DBA Users', 'SPK');
   
 
   /** Add a couple of districts*/
