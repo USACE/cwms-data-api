dafe80d42868d91c00270eed2845abc87dc422dc
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-02-27 11:15:38 -0800

    Delete twice because some tests were creating multiple versions

--- cwms-data-api/src/test/resources/cwms/cda/data/sql/delete_reg_text_timeseries.sql
+++ cwms-data-api/src/test/resources/cwms/cda/data/sql/delete_reg_text_timeseries.sql
@@ -14,4 +14,18 @@ begin
             P_MAX_ATTRIBUTE => null,
             P_OFFICE_ID => 'SPK'
     );
+
+--     call it again in-case two versions were stored in tests.
+    CWMS_20.CWMS_TEXT.DELETE_TS_TEXT(
+                P_TSID => 'TsTextTestLoc.Flow.Inst.1Hour.0.raw',
+                P_TEXT_MASK => '*',
+                P_START_TIME => START_TIME,
+                P_END_TIME => END_TIME,
+                P_VERSION_DATE => null,
+                P_TIME_ZONE => 'UTC',
+                P_MAX_VERSION => 'T',
+                P_MIN_ATTRIBUTE => null,
+                P_MAX_ATTRIBUTE => null,
+                P_OFFICE_ID => 'SPK'
+        );
 end;
