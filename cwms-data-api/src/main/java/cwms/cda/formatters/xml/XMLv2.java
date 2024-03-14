d8e176e7a18e69cf987c2026a4641de0df5373ac
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-03-14 09:27:19 -0700

    Formatting

--- cwms-data-api/src/main/java/cwms/cda/formatters/xml/XMLv2.java
+++ cwms-data-api/src/main/java/cwms/cda/formatters/xml/XMLv2.java
@@ -1,19 +1,17 @@
 package cwms.cda.formatters.xml;
 
+import cwms.cda.data.dto.CwmsDTOBase;
+import cwms.cda.formatters.Formats;
+import cwms.cda.formatters.OutputFormatter;
+import io.javalin.http.InternalServerErrorResponse;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-
-import cwms.cda.data.dto.CwmsDTOBase;
-import cwms.cda.formatters.Formats;
-import cwms.cda.formatters.OutputFormatter;
-import io.javalin.http.InternalServerErrorResponse;
 
 public class XMLv2 implements OutputFormatter {
     private static Logger logger = Logger.getLogger(XMLv2.class.getName());
@@ -31,11 +29,11 @@ public class XMLv2 implements OutputFormatter {
         try {
             final JAXBContext context = JAXBContext.newInstance(dto.getClass());
             final Marshaller mar = context.createMarshaller();
-            mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
+            mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
-            mar.marshal(dto,pw);
+            mar.marshal(dto, pw);
             return sw.toString();
         } catch (JAXBException jaxb) {
             String msg = dto != null ?
