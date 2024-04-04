b854cf7f75a30eec50006e8f789b82d1afc0cfa8
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-04-04 15:26:09 -0700

    Changing to builder style

--- cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastSpec.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastSpec.java
@@ -1,5 +1,10 @@
 package cwms.cda.data.dto.forecast;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import cwms.cda.api.errors.FieldException;
 import cwms.cda.data.dto.CwmsDTO;
 import cwms.cda.data.dto.TimeSeriesIdentifierDescriptor;
@@ -17,46 +22,43 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "forecast-spec")
 @XmlAccessorType(XmlAccessType.FIELD)
 @FormattableWith(contentType = Formats.JSONV2, formatter = JsonV2.class)
+@JsonDeserialize(builder = ForecastSpec.Builder.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
 public class ForecastSpec extends CwmsDTO {
     @Schema(description = "Forecast Spec ID")
     @XmlElement(name = "spec-id")
-    private String specId;
-
-    @Schema(description = "Location ID")
-    @XmlElement(name = "location-id")
-    private String locationId;
-
-    @Schema(description = "Source Entity ID")
-    @XmlElement(name = "source-entity-id")
-    private String sourceEntityId;
+    private final String specId;
 
     @Schema(description = "Forecast Designator")
     @XmlAttribute
-    private String designator;
+    private final String designator;
+
+    @Schema(description = "Location ID")
+    @XmlElement(name = "location-id")
+    private final String locationId;
+
+    @Schema(description = "Source Entity ID")
+    @XmlElement(name = "source-entity-id")
+    private final String sourceEntityId;
 
     @Schema(description = "Description of Forecast")
     @XmlAttribute
-    private String description;
+    private final String description;
 
     @Schema(description = "List of Time Series IDs belonging to this Forecast Spec")
     @XmlAttribute(name = "time-series-ids")
-    private List<TimeSeriesIdentifierDescriptor> timeSeriesIds;
+    private final List<TimeSeriesIdentifierDescriptor> timeSeriesIds;
 
-    @SuppressWarnings("unused") // required so JAXB can initialize and marshal
-    private ForecastSpec() {
-        super(null);
-    }
 
-    public ForecastSpec(String specId, String officeId, String locationId, String sourceEntityId,
-                        String designator,
-                        String description, List<TimeSeriesIdentifierDescriptor> timeSeriesIds) {
-        super(officeId);
-        this.specId = specId;
-        this.locationId = locationId;
-        this.sourceEntityId = sourceEntityId;
-        this.designator = designator;
-        this.description = description;
-        this.timeSeriesIds = timeSeriesIds;
+    private ForecastSpec(Builder builder) {
+        super(builder.officeId);
+        this.specId = builder.specId;
+        this.designator = builder.designator;
+        this.locationId = builder.locationId;
+        this.sourceEntityId = builder.sourceEntityId;
+        this.description = builder.description;
+        this.timeSeriesIds = builder.timeSeriesIds;
     }
 
     public String getSpecId() {
@@ -85,6 +87,72 @@ public class ForecastSpec extends CwmsDTO {
 
     public void validate() throws FieldException {
         //TODO
+    }
+
+    @JsonPOJOBuilder
+    @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
+    public static class Builder {
+        private String officeId;
+        private String specId;
+        private String designator;
+        private String locationId;
+        private String sourceEntityId;
+        private String description;
+        private List<TimeSeriesIdentifierDescriptor> timeSeriesIds;
+
+        public Builder(){
+
+        }
+
+        public Builder withOfficeId(String officeId) {
+            this.officeId = officeId;
+            return this;
+        }
+
+        public Builder withSpecId(String specId) {
+            this.specId = specId;
+            return this;
+        }
+
+        public Builder withDesignator(String designator) {
+            this.designator = designator;
+            return this;
+        }
+
+        public Builder withLocationId(String locationId) {
+            this.locationId = locationId;
+            return this;
+        }
+
+        public Builder withSourceEntityId(String sourceEntityId) {
+            this.sourceEntityId = sourceEntityId;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder withTimeSeriesIds(List<TimeSeriesIdentifierDescriptor> timeSeriesIds) {
+            this.timeSeriesIds = timeSeriesIds;
+            return this;
+        }
+
+        public Builder from(ForecastSpec forecastSpec) {
+
+            return withOfficeId(forecastSpec.getOfficeId())
+                    .withSpecId(forecastSpec.getSpecId())
+                    .withDesignator(forecastSpec.getDesignator())
+                    .withLocationId(forecastSpec.getLocationId())
+                    .withSourceEntityId(forecastSpec.getSourceEntityId())
+                    .withDescription(forecastSpec.getDescription())
+                    .withTimeSeriesIds(forecastSpec.getTimeSeriesIds());
+        }
+
+        public ForecastSpec build() {
+            return new ForecastSpec(this);
+        }
     }
 
 }
