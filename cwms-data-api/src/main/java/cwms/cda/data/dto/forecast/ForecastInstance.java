220396b9e1d6f4e7f2377eca352a8e20f17e447a
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-04-04 13:57:20 -0700

    Fixing indentation

--- cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastInstance.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastInstance.java
@@ -27,257 +27,257 @@ import javax.xml.bind.annotation.XmlRootElement;
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
 public class ForecastInstance extends CwmsDTO {
 
-  // unique identifier: spec + issueDateTime
-  // to find spec we need officeId, specId and locationId
-  // we get officeId from CwmsDTO
+    // unique identifier: spec + issueDateTime
+    // to find spec we need officeId, specId and locationId
+    // we get officeId from CwmsDTO
 
-  @Schema(description = "Forecast Spec ID")
-  @XmlElement(name = "spec-id")
-  private final String specId;
+    @Schema(description = "Forecast Spec ID")
+    @XmlElement(name = "spec-id")
+    private final String specId;
 
-  @Schema(description = "Location ID")
-  @XmlElement(name = "location-id")
-  private final String locationId;
+    @Schema(description = "Location ID")
+    @XmlElement(name = "location-id")
+    private final String locationId;
 
-  @XmlAttribute(name = "date-time")
-  private final Instant dateTime;
+    @XmlAttribute(name = "date-time")
+    private final Instant dateTime;
 
-  @XmlAttribute(name = "issue-date-time")
-  private final Instant issueDateTime;
+    @XmlAttribute(name = "issue-date-time")
+    private final Instant issueDateTime;
 
-  @XmlAttribute(name = "first-date-time")
-  private final Instant firstDateTime;
+    @XmlAttribute(name = "first-date-time")
+    private final Instant firstDateTime;
 
-  @XmlAttribute(name = "last-date-time")
-  private final Instant lastDateTime;
+    @XmlAttribute(name = "last-date-time")
+    private final Instant lastDateTime;
 
-  @XmlAttribute(name = "max-age")
-  private final Integer maxAge;
+    @XmlAttribute(name = "max-age")
+    private final Integer maxAge;
 
-  @XmlAttribute(name = "time-series-count")
-  private final Integer timeSeriesCount;
+    @XmlAttribute(name = "time-series-count")
+    private final Integer timeSeriesCount;
 
-  @Schema(description = "Forecast Instance Notes")
-  @XmlAttribute
-  private final String notes;
+    @Schema(description = "Forecast Instance Notes")
+    @XmlAttribute
+    private final String notes;
 
-  @XmlAttribute(name = "metadata")
-  private final Map<String, String> metadata;
+    @XmlAttribute(name = "metadata")
+    private final Map<String, String> metadata;
 
-  @Schema(description = "Forecast Filename")
-  @XmlAttribute(name = "filename")
-  private final String filename;
+    @Schema(description = "Forecast Filename")
+    @XmlAttribute(name = "filename")
+    private final String filename;
 
-  @Schema(description = "Description of Forecast File")
-  @XmlAttribute
-  private final String fileDescription;
+    @Schema(description = "Description of Forecast File")
+    @XmlAttribute
+    private final String fileDescription;
 
-  @Schema(description = "Forecast File binary data")
-  @XmlElement(name = "forecast-data")
-  private final byte[] fileData;
+    @Schema(description = "Forecast File binary data")
+    @XmlElement(name = "forecast-data")
+    private final byte[] fileData;
 
-  @Schema(description = "Link to Forecast File binary data")
-  @XmlAttribute
-  private final String fileDataUrl;
+    @Schema(description = "Link to Forecast File binary data")
+    @XmlAttribute
+    private final String fileDataUrl;
 
 
-  private ForecastInstance(ForecastInstance.Builder builder) {
-    super(builder.officeId);
-    this.specId = builder.specId;
-    this.locationId = builder.locationId;
-    this.dateTime = builder.dateTime;
-    this.issueDateTime = builder.issueDateTime;
-    this.firstDateTime = builder.firstDateTime;
-    this.lastDateTime = builder.lastDateTime;
-    this.maxAge = builder.maxAge;
-    this.timeSeriesCount = builder.timeSeriesCount;
-    this.notes = builder.notes;
-    this.metadata = builder.metadata;
-    this.filename = builder.filename;
-    this.fileDescription = builder.fileDescription;
-    this.fileData = builder.fileData;
-    this.fileDataUrl = builder.fileDataUrl;
-  }
-
-  public String getSpecId() {
-    return specId;
-  }
-
-  public String getLocationId() {
-    return locationId;
-  }
-
-  public Instant getDateTime() {
-    return dateTime;
-  }
-
-  public Instant getIssueDateTime() {
-    return issueDateTime;
-  }
-
-  public Instant getFirstDateTime() {
-    return firstDateTime;
-  }
-
-  public Instant getLastDateTime() {
-    return lastDateTime;
-  }
-
-  public Integer getMaxAge() {
-    return maxAge;
-  }
-
-  public Integer getTimeSeriesCount() {
-    return timeSeriesCount;
-  }
-
-  public String getNotes() {
-    return notes;
-  }
-
-  public Map<String, String> getMetadata() {
-    return metadata;
-  }
-
-  public String getFilename() {
-    return filename;
-  }
-
-  public String getFileDescription() {
-    return fileDescription;
-  }
-
-  public byte[] getFileData() {
-    return fileData;
-  }
-
-  public String getFileDataUrl() {
-    return fileDataUrl;
-  }
-
-  public void validate() throws FieldException {
-    //TODO
-  }
-
-  @JsonPOJOBuilder
-  @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
-  public static class Builder {
-    private String officeId;
-    private String specId;
-    private String locationId;
-    private Instant dateTime;
-    private Instant issueDateTime;
-    private Instant firstDateTime;
-    private Instant lastDateTime;
-    private Integer maxAge;
-    private Integer timeSeriesCount;
-    private String notes;
-    private Map<String, String> metadata;
-    private String filename;
-    private String fileDescription;
-    private byte[] fileData;
-    private String fileDataUrl;
-
-    public Builder() {
+    private ForecastInstance(ForecastInstance.Builder builder) {
+        super(builder.officeId);
+        this.specId = builder.specId;
+        this.locationId = builder.locationId;
+        this.dateTime = builder.dateTime;
+        this.issueDateTime = builder.issueDateTime;
+        this.firstDateTime = builder.firstDateTime;
+        this.lastDateTime = builder.lastDateTime;
+        this.maxAge = builder.maxAge;
+        this.timeSeriesCount = builder.timeSeriesCount;
+        this.notes = builder.notes;
+        this.metadata = builder.metadata;
+        this.filename = builder.filename;
+        this.fileDescription = builder.fileDescription;
+        this.fileData = builder.fileData;
+        this.fileDataUrl = builder.fileDataUrl;
     }
 
-    public Builder withOfficeId(String officeId) {
-      this.officeId = officeId;
-      return this;
+    public String getSpecId() {
+        return specId;
     }
 
-    public Builder withSpecId(String specId) {
-      this.specId = specId;
-      return this;
+    public String getLocationId() {
+        return locationId;
     }
 
-    public Builder withLocationId(String locationId) {
-      this.locationId = locationId;
-      return this;
+    public Instant getDateTime() {
+        return dateTime;
     }
 
-    public Builder withDateTime(Instant dateTime) {
-      this.dateTime = dateTime;
-      return this;
+    public Instant getIssueDateTime() {
+        return issueDateTime;
     }
 
-    public Builder withIssueDateTime(Instant issueDateTime) {
-      this.issueDateTime = issueDateTime;
-      return this;
+    public Instant getFirstDateTime() {
+        return firstDateTime;
     }
 
-    public Builder withFirstDateTime(Instant firstDateTime) {
-      this.firstDateTime = firstDateTime;
-      return this;
+    public Instant getLastDateTime() {
+        return lastDateTime;
     }
 
-    public Builder withLastDateTime(Instant lastDateTime) {
-      this.lastDateTime = lastDateTime;
-      return this;
+    public Integer getMaxAge() {
+        return maxAge;
     }
 
-    public Builder withMaxAge(Integer maxAge) {
-      this.maxAge = maxAge;
-      return this;
+    public Integer getTimeSeriesCount() {
+        return timeSeriesCount;
     }
 
-    public Builder withTimeSeriesCount(Integer timeSeriesCount) {
-      this.timeSeriesCount = timeSeriesCount;
-      return this;
+    public String getNotes() {
+        return notes;
     }
 
-    public Builder withNotes(String notes) {
-      this.notes = notes;
-      return this;
+    public Map<String, String> getMetadata() {
+        return metadata;
     }
 
-    public Builder withMetadata(Map<String, String> metadata) {
-      this.metadata = metadata;
-      return this;
+    public String getFilename() {
+        return filename;
     }
 
-    public Builder withFilename(String filename) {
-      this.filename = filename;
-      return this;
+    public String getFileDescription() {
+        return fileDescription;
     }
 
-    public Builder withFileDescription(String fileDescription) {
-      this.fileDescription = fileDescription;
-      return this;
+    public byte[] getFileData() {
+        return fileData;
     }
 
-    public Builder withFileData(byte[] fileData) {
-      this.fileData = fileData;
-      return this;
+    public String getFileDataUrl() {
+        return fileDataUrl;
     }
 
-    public Builder withFileDataUrl(String fileDataUrl) {
-      this.fileDataUrl = fileDataUrl;
-      return this;
+    public void validate() throws FieldException {
+        //TODO
     }
 
-    public Builder from(ForecastInstance other) {
-      return withOfficeId(other.officeId)
-              .withSpecId(other.specId)
-              .withLocationId(other.locationId)
-              .withDateTime(other.dateTime)
-              .withIssueDateTime(other.issueDateTime)
-              .withFirstDateTime(other.firstDateTime)
-              .withLastDateTime(other.lastDateTime)
-              .withMaxAge(other.maxAge)
-              .withTimeSeriesCount(other.timeSeriesCount)
-              .withNotes(other.notes)
-              .withMetadata(other.metadata)
-              .withFilename(other.filename)
-              .withFileDescription(other.fileDescription)
-              .withFileData(other.fileData)
-              .withFileDataUrl(other.fileDataUrl)
-              ;
-    }
+    @JsonPOJOBuilder
+    @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
+    public static class Builder {
+        private String officeId;
+        private String specId;
+        private String locationId;
+        private Instant dateTime;
+        private Instant issueDateTime;
+        private Instant firstDateTime;
+        private Instant lastDateTime;
+        private Integer maxAge;
+        private Integer timeSeriesCount;
+        private String notes;
+        private Map<String, String> metadata;
+        private String filename;
+        private String fileDescription;
+        private byte[] fileData;
+        private String fileDataUrl;
 
-    public ForecastInstance build() {
-      return new ForecastInstance(this);
+        public Builder() {
+        }
+
+        public Builder withOfficeId(String officeId) {
+            this.officeId = officeId;
+            return this;
+        }
+
+        public Builder withSpecId(String specId) {
+            this.specId = specId;
+            return this;
+        }
+
+        public Builder withLocationId(String locationId) {
+            this.locationId = locationId;
+            return this;
+        }
+
+        public Builder withDateTime(Instant dateTime) {
+            this.dateTime = dateTime;
+            return this;
+        }
+
+        public Builder withIssueDateTime(Instant issueDateTime) {
+            this.issueDateTime = issueDateTime;
+            return this;
+        }
+
+        public Builder withFirstDateTime(Instant firstDateTime) {
+            this.firstDateTime = firstDateTime;
+            return this;
+        }
+
+        public Builder withLastDateTime(Instant lastDateTime) {
+            this.lastDateTime = lastDateTime;
+            return this;
+        }
+
+        public Builder withMaxAge(Integer maxAge) {
+            this.maxAge = maxAge;
+            return this;
+        }
+
+        public Builder withTimeSeriesCount(Integer timeSeriesCount) {
+            this.timeSeriesCount = timeSeriesCount;
+            return this;
+        }
+
+        public Builder withNotes(String notes) {
+            this.notes = notes;
+            return this;
+        }
+
+        public Builder withMetadata(Map<String, String> metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public Builder withFilename(String filename) {
+            this.filename = filename;
+            return this;
+        }
+
+        public Builder withFileDescription(String fileDescription) {
+            this.fileDescription = fileDescription;
+            return this;
+        }
+
+        public Builder withFileData(byte[] fileData) {
+            this.fileData = fileData;
+            return this;
+        }
+
+        public Builder withFileDataUrl(String fileDataUrl) {
+            this.fileDataUrl = fileDataUrl;
+            return this;
+        }
+
+        public Builder from(ForecastInstance other) {
+            return withOfficeId(other.officeId)
+                    .withSpecId(other.specId)
+                    .withLocationId(other.locationId)
+                    .withDateTime(other.dateTime)
+                    .withIssueDateTime(other.issueDateTime)
+                    .withFirstDateTime(other.firstDateTime)
+                    .withLastDateTime(other.lastDateTime)
+                    .withMaxAge(other.maxAge)
+                    .withTimeSeriesCount(other.timeSeriesCount)
+                    .withNotes(other.notes)
+                    .withMetadata(other.metadata)
+                    .withFilename(other.filename)
+                    .withFileDescription(other.fileDescription)
+                    .withFileData(other.fileData)
+                    .withFileDataUrl(other.fileDataUrl)
+                    ;
+        }
+
+        public ForecastInstance build() {
+            return new ForecastInstance(this);
+        }
     }
-  }
 
 }
--- cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastSpec.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/forecast/ForecastSpec.java
@@ -7,81 +7,84 @@ import cwms.cda.formatters.Formats;
 import cwms.cda.formatters.annotations.FormattableWith;
 import cwms.cda.formatters.json.JsonV2;
 import io.swagger.v3.oas.annotations.media.Schema;
-
-import javax.xml.bind.annotation.*;
 import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "forecast-spec")
 @XmlAccessorType(XmlAccessType.FIELD)
 @FormattableWith(contentType = Formats.JSONV2, formatter = JsonV2.class)
 public class ForecastSpec extends CwmsDTO {
+    @Schema(description = "Forecast Spec ID")
+    @XmlElement(name = "spec-id")
+    private String specId;
 
-  @Schema(description = "Forecast Spec ID")
-  @XmlElement(name = "spec-id")
-  private String specId;
+    @Schema(description = "Location ID")
+    @XmlElement(name = "location-id")
+    private String locationId;
 
-  @Schema(description = "Location ID")
-  @XmlElement(name = "location-id")
-  private String locationId;
+    @Schema(description = "Source Entity ID")
+    @XmlElement(name = "source-entity-id")
+    private String sourceEntityId;
 
-  @Schema(description = "Source Entity ID")
-  @XmlElement(name = "source-entity-id")
-  private String sourceEntityId;
+    @Schema(description = "Forecast Designator")
+    @XmlAttribute
+    private String designator;
 
-  @Schema(description = "Forecast Designator")
-  @XmlAttribute
-  private String designator;
+    @Schema(description = "Description of Forecast")
+    @XmlAttribute
+    private String description;
 
-  @Schema(description = "Description of Forecast")
-  @XmlAttribute
-  private String description;
+    @Schema(description = "List of Time Series IDs belonging to this Forecast Spec")
+    @XmlAttribute(name = "time-series-ids")
+    private List<TimeSeriesIdentifierDescriptor> timeSeriesIds;
 
-  @Schema(description = "List of Time Series IDs belonging to this Forecast Spec")
-  @XmlAttribute(name = "time-series-ids")
-  private List<TimeSeriesIdentifierDescriptor> timeSeriesIds;
+    @SuppressWarnings("unused") // required so JAXB can initialize and marshal
+    private ForecastSpec() {
+        super(null);
+    }
 
-  @SuppressWarnings("unused") // required so JAXB can initialize and marshal
-  private ForecastSpec() {
-    super(null);
-  }
+    public ForecastSpec(String specId, String officeId, String locationId, String sourceEntityId,
+                        String designator,
+                        String description, List<TimeSeriesIdentifierDescriptor> timeSeriesIds) {
+        super(officeId);
+        this.specId = specId;
+        this.locationId = locationId;
+        this.sourceEntityId = sourceEntityId;
+        this.designator = designator;
+        this.description = description;
+        this.timeSeriesIds = timeSeriesIds;
+    }
 
-  public ForecastSpec(String specId, String officeId, String locationId, String sourceEntityId, String designator,
-                      String description, List<TimeSeriesIdentifierDescriptor> timeSeriesIds) {
-    super(officeId);
-    this.specId = specId;
-    this.locationId = locationId;
-    this.sourceEntityId = sourceEntityId;
-    this.designator = designator;
-    this.description = description;
-    this.timeSeriesIds = timeSeriesIds;
-  }
+    public String getSpecId() {
+        return specId;
+    }
 
-  public String getSpecId() {
-    return specId;
-  }
+    public String getLocationId() {
+        return locationId;
+    }
 
-  public String getLocationId() {
-    return locationId;
-  }
+    public String getSourceEntityId() {
+        return sourceEntityId;
+    }
 
-  public String getSourceEntityId() {
-    return sourceEntityId;
-  }
+    public String getDesignator() {
+        return designator;
+    }
 
-  public String getDesignator() {
-    return designator;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public List<TimeSeriesIdentifierDescriptor> getTimeSeriesIds() {
+        return timeSeriesIds;
+    }
 
-  public List<TimeSeriesIdentifierDescriptor> getTimeSeriesIds() {
-    return timeSeriesIds;
-  }
-
-  public void validate() throws FieldException {
-    //TODO
-  }
+    public void validate() throws FieldException {
+        //TODO
+    }
 
 }
