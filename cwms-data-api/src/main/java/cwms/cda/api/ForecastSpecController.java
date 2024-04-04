a2afb28104df1b0daa214abe6d587babb3a15c53
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-04-04 13:00:04 -0700

    Making Spec controller us the SpecDao class

--- cwms-data-api/src/main/java/cwms/cda/api/ForecastSpecController.java
+++ cwms-data-api/src/main/java/cwms/cda/api/ForecastSpecController.java
@@ -1,32 +1,5 @@
 package cwms.cda.api;
 
-import com.codahale.metrics.Histogram;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import cwms.cda.api.errors.CdaError;
-import cwms.cda.data.dao.JooqDao;
-import cwms.cda.data.dto.forecast.ForecastSpec;
-import cwms.cda.formatters.ContentType;
-import cwms.cda.formatters.Formats;
-import cwms.cda.formatters.json.JsonV2;
-import io.javalin.apibuilder.CrudHandler;
-import io.javalin.http.Context;
-import io.javalin.plugin.openapi.annotations.HttpMethod;
-import io.javalin.plugin.openapi.annotations.OpenApi;
-import io.javalin.plugin.openapi.annotations.OpenApiContent;
-import io.javalin.plugin.openapi.annotations.OpenApiParam;
-import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
-import io.javalin.plugin.openapi.annotations.OpenApiResponse;
-import org.jetbrains.annotations.NotNull;
-import org.jooq.DSLContext;
-import org.jooq.exception.DataAccessException;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import static com.codahale.metrics.MetricRegistry.name;
 import static cwms.cda.api.Controllers.CREATE;
 import static cwms.cda.api.Controllers.DELETE;
@@ -36,7 +9,6 @@ import static cwms.cda.api.Controllers.ID_MASK;
 import static cwms.cda.api.Controllers.LOCATION_ID;
 import static cwms.cda.api.Controllers.LOCATION_MASK;
 import static cwms.cda.api.Controllers.NAME;
-import static cwms.cda.api.Controllers.NOT_SUPPORTED_YET;
 import static cwms.cda.api.Controllers.OFFICE;
 import static cwms.cda.api.Controllers.RESULTS;
 import static cwms.cda.api.Controllers.SIZE;
@@ -47,6 +19,35 @@ import static cwms.cda.api.Controllers.STATUS_404;
 import static cwms.cda.api.Controllers.STATUS_501;
 import static cwms.cda.api.Controllers.UPDATE;
 import static cwms.cda.api.Controllers.requiredParam;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cwms.cda.api.errors.CdaError;
+import cwms.cda.data.dao.ForecastSpecDao;
+import cwms.cda.data.dao.JooqDao;
+import cwms.cda.data.dto.forecast.ForecastSpec;
+import cwms.cda.formatters.ContentType;
+import cwms.cda.formatters.Formats;
+import cwms.cda.formatters.json.JsonV2;
+import io.javalin.apibuilder.CrudHandler;
+import io.javalin.core.util.Header;
+import io.javalin.http.Context;
+import io.javalin.plugin.openapi.annotations.HttpMethod;
+import io.javalin.plugin.openapi.annotations.OpenApi;
+import io.javalin.plugin.openapi.annotations.OpenApiContent;
+import io.javalin.plugin.openapi.annotations.OpenApiParam;
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
+import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
+import org.jooq.DSLContext;
+import org.jooq.exception.DataAccessException;
 
 public class ForecastSpecController implements CrudHandler {
     private static final Logger logger = Logger.getLogger(ForecastSpecController.class.getName());
@@ -74,18 +75,22 @@ public class ForecastSpecController implements CrudHandler {
             description = "Used to create and save forecast spec data",
             requestBody = @OpenApiRequestBody(
                     content = {
-                            @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)
+                        @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)
                     },
                     required = true
             ),
             method = HttpMethod.POST,
-            path = "/forecast-spec",
             tags = TAG
     )
     @Override
     public void create(@NotNull Context ctx) {
         try (final Timer.Context ignored = markAndTime(CREATE)) {
+            DSLContext dsl = getDslContext(ctx);
+            ForecastSpecDao dao = new ForecastSpecDao(dsl);
             ForecastSpec forecastSpec = deserializeForecastSpec(ctx);
+
+            dao.create(forecastSpec);
+
             ctx.status(HttpServletResponse.SC_OK);
         } catch (IOException | DataAccessException ex) {
             CdaError re = new CdaError("Internal Error");
@@ -96,57 +101,59 @@ public class ForecastSpecController implements CrudHandler {
 
     @OpenApi(
             description = "Used to delete forecast spec data based on unique fields",
+            pathParams = {
+                @OpenApiParam(name = NAME, required = true, description = "Specifies the "
+                        + "spec id of the forecast spec whose data is to be deleted."),
+            },
             queryParams = {
-                    @OpenApiParam(name = OFFICE, required = true, description = "Specifies the " +
-                            "owning office of the forecast spec whose data is to be deleted."),
-                    @OpenApiParam(name = NAME, required = true, description = "Specifies the " +
-                            "spec id of the forecast spec whose data is to be deleted."),
-                    @OpenApiParam(name = LOCATION_ID, required = true, description = "Specifies the " +
-                            "location of the forecast spec whose data is to be deleted."),
+                @OpenApiParam(name = OFFICE, required = true, description = "Specifies the "
+                        + "owning office of the forecast spec whose data is to be deleted."),
+                @OpenApiParam(name = LOCATION_ID, required = true, description = "Specifies the "
+                        + "location of the forecast spec whose data is to be deleted."),
             },
             responses = {
-                    @OpenApiResponse(status = STATUS_404, description = "The provided combination of "
-                            + "parameters did not find a forecast spec."),
+                @OpenApiResponse(status = STATUS_404, description = "The provided combination of "
+                        + "parameters did not find a forecast spec."),
             },
-            path = "/forecast-spec",
             method = HttpMethod.DELETE,
             tags = TAG
     )
     @Override
-    public void delete(@NotNull Context ctx, @NotNull String forecastSpecId) {
+    public void delete(@NotNull Context ctx, @NotNull String name) {
         String office = requiredParam(ctx, OFFICE);
-        String specId = requiredParam(ctx, NAME);
         String locationId = requiredParam(ctx, LOCATION_ID);
 
         try (final Timer.Context ignored = markAndTime(DELETE)) {
-            throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+            DSLContext dsl = getDslContext(ctx);
+            ForecastSpecDao dao = new ForecastSpecDao(dsl);
+
+            dao.delete(office, name, locationId);
         }
     }
 
     @OpenApi(
             description = "Used to query multiple forecast specs",
             queryParams = {
-                    @OpenApiParam(name = OFFICE, description = "Specifies the " +
-                            "owning office of the forecast spec whose data is to be included in the " +
-                            "response."),
-                    @OpenApiParam(name = ID_MASK, description = "Posix "
-                            + "<a href=\"regexp.html\">regular expression</a>  that specifies "
-                            + "the spec IDs to be included in the response."),
-                    @OpenApiParam(name = LOCATION_MASK, description = "Specifies the " +
-                            "location of the forecast spec whose data to be included in the response."),
-                    @OpenApiParam(name = SOURCE_ENTITY, description = "Specifies the source identity " +
-                            "of the forecast spec whose data is to be included in the response.")
+                @OpenApiParam(name = OFFICE, description = "Specifies the "
+                        + "owning office of the forecast spec whose data is to be included in the "
+                        + "response."),
+                @OpenApiParam(name = ID_MASK, description = "Posix "
+                        + "<a href=\"regexp.html\">regular expression</a>  that specifies "
+                        + "the spec IDs to be included in the response."),
+                @OpenApiParam(name = LOCATION_MASK, description = "Specifies the "
+                        + "location of the forecast spec whose data to be included in the response."),
+                @OpenApiParam(name = SOURCE_ENTITY, description = "Specifies the source identity "
+                        + "of the forecast spec whose data is to be included in the response.")
             },
             responses = {
-                    @OpenApiResponse(status = STATUS_200,
-                            description = "A list of elements of the data set you've selected.",
-                            content = {
-                                    @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)}),
-                    @OpenApiResponse(status = STATUS_400, description = "Invalid parameter combination"),
-                    @OpenApiResponse(status = STATUS_501, description = "Requested format is not "
-                            + "implemented")
+                @OpenApiResponse(status = STATUS_200,
+                        description = "A list of elements of the data set you've selected.",
+                        content = {
+                            @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)}),
+                @OpenApiResponse(status = STATUS_400, description = "Invalid parameter combination"),
+                @OpenApiResponse(status = STATUS_501, description = "Requested format is not "
+                        + "implemented")
             },
-            path = "/forecast-spec/all",
             method = HttpMethod.GET,
             tags = TAG
     )
@@ -157,69 +164,99 @@ public class ForecastSpecController implements CrudHandler {
             String names = ctx.queryParamAsClass(ID_MASK, String.class).getOrDefault("*");
             String location = ctx.queryParam(LOCATION_MASK);
             String sourceEntity = ctx.queryParam(SOURCE_ENTITY);
-            throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+
+            DSLContext dsl = getDslContext(ctx);
+            ForecastSpecDao dao = new ForecastSpecDao(dsl);
+
+            List<ForecastSpec> specs = dao.getForecastSpecs(office, names, location, sourceEntity);
+
+            String formatHeader = ctx.header(Header.ACCEPT);
+            ContentType contentType = Formats.parseHeaderAndQueryParm(formatHeader, null);
+            String result = Formats.format(contentType, specs, ForecastSpec.class);
+
+            ctx.result(result).contentType(contentType.toString());
+            requestResultSize.update(result.length());
+
+            ctx.status(HttpServletResponse.SC_OK);
         }
     }
 
     @OpenApi(
             description = "Used to query a single forecast spec record",
+            pathParams = {
+                @OpenApiParam(name = NAME, required = true, description = "Specifies the "
+                        + "spec id of the forecast spec whose data is to be included in the response."),
+            },
             queryParams = {
-                    @OpenApiParam(name = OFFICE, required = true, description = "Specifies the " +
-                            "owning office of the forecast spec whose data is to be included in the " +
-                            "response."),
-                    @OpenApiParam(name = NAME, required = true, description = "Specifies the " +
-                            "spec id of the forecast spec whose data is to be included in the response."),
-                    @OpenApiParam(name = LOCATION_ID, required = true, description = "Specifies the " +
-                            "location of the forecast spec whose data to be included in the response.")
+                @OpenApiParam(name = OFFICE, required = true, description = "Specifies the "
+                        + "owning office of the forecast spec whose data is to be included in the "
+                        + "response."),
+                @OpenApiParam(name = LOCATION_ID, required = true, description = "Specifies the "
+                        + "location of the forecast spec whose data to be included in the response.")
             },
             responses = {
-                    @OpenApiResponse(status = STATUS_200,
-                            description = "Returns the requested forecast spec",
-                            content = {
-                                    @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)}),
-                    @OpenApiResponse(status = STATUS_400, description = "Invalid parameter combination"),
-                    @OpenApiResponse(status = STATUS_404, description = "The provided combination of "
-                            + "parameters did not find a forecast spec."),
-                    @OpenApiResponse(status = STATUS_501, description = "Requested format is not "
-                            + "implemented")
+                @OpenApiResponse(status = STATUS_200,
+                        description = "Returns the requested forecast spec",
+                        content = {
+                            @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)}),
+                @OpenApiResponse(status = STATUS_400, description = "Invalid parameter combination"),
+                @OpenApiResponse(status = STATUS_404, description = "The provided combination of "
+                        + "parameters did not find a forecast spec."),
+                @OpenApiResponse(status = STATUS_501, description = "Requested format is not "
+                        + "implemented")
             },
-            path = "/forecast-spec",
             method = HttpMethod.GET,
             tags = TAG
     )
     @Override
-    public void getOne(@NotNull Context ctx, @NotNull String id) {
+    public void getOne(@NotNull Context ctx, @NotNull String name) {
         try (final Timer.Context ignored = markAndTime(GET_ONE)) {
-            String office = ctx.queryParam(OFFICE);
-            String names = ctx.queryParam(NAME);
-            String location = ctx.queryParam(LOCATION_ID);
-            throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+            String office = requiredParam(ctx, OFFICE);
+            String location = requiredParam(ctx, LOCATION_ID);
+
+            DSLContext dsl = getDslContext(ctx);
+            ForecastSpecDao dao = new ForecastSpecDao(dsl);
+
+            ForecastSpec spec = dao.getForecastSpec(office, name, location);
+
+            String formatHeader = ctx.header(Header.ACCEPT);
+            ContentType contentType = Formats.parseHeaderAndQueryParm(formatHeader, null);
+            String result = Formats.format(contentType, spec);
+
+            ctx.result(result).contentType(contentType.toString());
+            requestResultSize.update(result.length());
+
+            ctx.status(HttpServletResponse.SC_OK);
         }
     }
 
     @OpenApi(
             description = "Update a forecast spec with provided values",
             pathParams = {
-                    @OpenApiParam(name = NAME, description = "Forecast spec id to be updated")
+                @OpenApiParam(name = NAME, description = "Forecast spec id to be updated")
             },
             requestBody = @OpenApiRequestBody(
                     content = {
-                            @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)
+                        @OpenApiContent(from = ForecastSpec.class, type = Formats.JSONV2)
                     },
                     required = true),
             responses = {
-                    @OpenApiResponse(status = STATUS_404, description = "Based on the combination of "
-                            + "inputs provided the location was not found.")
+                @OpenApiResponse(status = STATUS_404, description = "Based on the combination of "
+                        + "inputs provided the location was not found.")
             },
             method = HttpMethod.PATCH,
-            path = "/forecast-spec/{spec-id}",
             tags = TAG
     )
     @Override
-    public void update(@NotNull Context ctx, @NotNull String id) {
+    public void update(@NotNull Context ctx, @NotNull String name) {
         try (final Timer.Context ignored = markAndTime(UPDATE)) {
             ForecastSpec forecastSpec = deserializeForecastSpec(ctx);
-            throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+
+            DSLContext dsl = getDslContext(ctx);
+            ForecastSpecDao dao = new ForecastSpecDao(dsl);
+
+            dao.update(forecastSpec);
+
         } catch (IOException | DataAccessException ex) {
             CdaError re = new CdaError("Internal Error");
             logger.log(Level.SEVERE, re.toString(), ex);
