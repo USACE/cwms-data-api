32c78825b6c67c61bf109daa3261574a1d8bb075
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-04-04 12:59:45 -0700

    Adding a placeholder SpecDao class

---
+++ cwms-data-api/src/main/java/cwms/cda/data/dao/ForecastSpecDao.java
@@ -0,0 +1,38 @@
+package cwms.cda.data.dao;
+
+import static cwms.cda.api.Controllers.NOT_SUPPORTED_YET;
+
+import cwms.cda.data.dto.forecast.ForecastSpec;
+import java.util.List;
+import org.jooq.DSLContext;
+
+public class ForecastSpecDao extends JooqDao<ForecastSpec> {
+
+    public ForecastSpecDao(DSLContext dsl) {
+        super(dsl);
+    }
+
+
+    public void create(ForecastSpec forecastSpec) {
+        throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+
+    }
+
+    public void delete(String office, String specId, String locationId) {
+        throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+    }
+
+    public List<ForecastSpec> getForecastSpecs(String office, String specIdRegex, String location, String sourceEntity) {
+        throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+    }
+
+    public ForecastSpec getForecastSpec(String office, String name, String location) {
+        throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+
+
+    }
+
+    public void update(ForecastSpec forecastSpec) {
+        throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
+    }
+}
