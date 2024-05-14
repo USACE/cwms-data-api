ce736ed17a790017639effd00018ae7f2378cbc4
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-05-14 14:28:31 -0700

    Changes to read directly from user provided stream and avoid "Payload Too Large"

--- cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesControllerTest.java
+++ cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesControllerTest.java
@@ -21,7 +21,11 @@ import cwms.cda.formatters.json.JsonV2;
 import cwms.cda.formatters.xml.XMLv2;
 import io.javalin.core.util.Header;
 import io.javalin.http.Context;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.ZonedDateTime;
@@ -32,6 +36,9 @@ import java.util.Map;
 import java.util.TimeZone;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import org.jetbrains.annotations.NotNull;
 import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
@@ -142,7 +149,15 @@ class TimeSeriesControllerTest extends ControllerTest {
         XMLv2 out = new XMLv2();
         String str = out.format(fakeTs);
 
-        TimeSeries ts2 = TimeSeriesController.deserializeJaxb(str);
+        TimeSeries result;
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(TimeSeries.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            result = (TimeSeries) unmarshaller.unmarshal(new StringReader(str));
+        } catch (JAXBException e) {
+            throw new IOException(e);
+        }
+        TimeSeries ts2 = result;
         assertNotNull(ts2);
 
         assertSimilar(fakeTs, ts2);
@@ -156,7 +171,8 @@ class TimeSeriesControllerTest extends ControllerTest {
 
             String xml = loadResourceAsString("cwms/cda/api/timeseries_create.xml");
             assertNotNull(xml);
-            TimeSeries ts = TimeSeriesController.deserializeTimeSeries(xml, Formats.XMLV2);  // Should this be XMLv2?
+            InputStream inputStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+            TimeSeries ts = TimeSeriesController.deserializeTimeSeries(inputStream, Formats.XMLV2);  // Should this be XMLv2?
 
             assertNotNull(ts);
 
@@ -172,7 +188,8 @@ class TimeSeriesControllerTest extends ControllerTest {
             String xml = loadResourceAsString("cwms/cda/api/timeseries_create.xml");
             assertNotNull(xml);
 			 // Should this be XMLv2?
-            TimeSeries ts = TimeSeriesController.deserializeTimeSeries(xml, Formats.XMLV2);
+        InputStream inputStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+        TimeSeries ts = TimeSeriesController.deserializeTimeSeries(inputStream, Formats.XMLV2);
 
             assertNotNull(ts);
 
@@ -184,7 +201,8 @@ class TimeSeriesControllerTest extends ControllerTest {
     void testDeserializeTimeSeriesJSON() throws IOException     {
         String jsonV2 = loadResourceAsString("cwms/cda/api/timeseries_create.json");
         assertNotNull(jsonV2);
-        TimeSeries ts = TimeSeriesController.deserializeTimeSeries(jsonV2, Formats.JSONV2);
+        InputStream inputStream = new ByteArrayInputStream(jsonV2.getBytes(StandardCharsets.UTF_8));
+        TimeSeries ts = TimeSeriesController.deserializeTimeSeries(inputStream, Formats.JSONV2);
 
         assertNotNull(ts);
 
