08e5d2d99286e31a8885786c341973584cb0b97e
Ryan Miles <ryanm@rmanet.com>
2024-06-05 11:04:37 -0700

    Adding unit test for firefox header removing public scope Adding parameterized test for validating alias behavior from the Formats API

--- cwms-data-api/src/test/java/cwms/cda/formatters/FormatsTest.java
+++ cwms-data-api/src/test/java/cwms/cda/formatters/FormatsTest.java
@@ -1,10 +1,16 @@
 package cwms.cda.formatters;
 
 
+import java.util.Arrays;
 import java.util.Map;
 
+import cwms.cda.data.dto.County;
+import cwms.cda.data.dto.CwmsDTOBase;
+import cwms.cda.formatters.annotations.FormattableWith;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,11 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FormatsTest
+class FormatsTest
 {
 
 	@Test
-	public void testParseHeaderAndQueryParmJSON(){
+	void testParseHeaderAndQueryParmJSON(){
 		ContentType contentType =Formats.parseHeaderAndQueryParm("application/json", null);
 
 		assertNotNull(contentType);
@@ -27,7 +33,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseHeaderAndQueryParmJSONv2()
+	void testParseHeaderAndQueryParmJSONv2()
 	{
 		ContentType contentType = Formats.parseHeaderAndQueryParm("application/json;version=2", null);
 
@@ -41,7 +47,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseNullNull()
+	void testParseNullNull()
 	{
 		RuntimeException thrown = Assertions.assertThrows(FormattingException.class, () -> {
 			Formats.parseHeaderAndQueryParm(null, null);
@@ -49,7 +55,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseEmptyHeader(){
+	void testParseEmptyHeader(){
 
 		ContentType contentType =Formats.parseHeaderAndQueryParm("", "json");
 
@@ -60,7 +66,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseNullHeader(){
+	void testParseNullHeader(){
 
 		ContentType contentType =Formats.parseHeaderAndQueryParm(null, "json");
 
@@ -75,7 +81,7 @@ public class FormatsTest
 
 
 	@Test
-	public void testParseHeaderAndQueryParmXML(){
+	void testParseHeaderAndQueryParmXML(){
 		RuntimeException thrown = Assertions.assertThrows(FormattingException.class, () -> {
 			Formats.parseHeaderAndQueryParm(null, null);
 		});
@@ -101,7 +107,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseBoth(){
+	void testParseBoth(){
 		RuntimeException thrown = Assertions.assertThrows(FormattingException.class, () -> {
 			Formats.parseHeaderAndQueryParm("application/json", "json");
 		});
@@ -110,7 +116,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseBothv2(){
+	void testParseBothv2(){
 		RuntimeException thrown = Assertions.assertThrows(FormattingException.class, () -> {
 			Formats.parseHeaderAndQueryParm("application/json;version=2", "json");
 		});
@@ -120,7 +126,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseHeader(){
+	void testParseHeader(){
 		ContentType contentType;
 
 		contentType = Formats.parseHeader("application/json");
@@ -140,7 +146,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseQueryParam(){
+	void testParseQueryParam(){
 		ContentType contentType;
 		contentType	= Formats.parseQueryParam("json");
 		assertNotNull(contentType);
@@ -155,7 +161,7 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseHeaderAndQueryParmJSONv2WithCharset() {
+	void testParseHeaderAndQueryParmJSONv2WithCharset() {
 		ContentType contentType = Formats.parseHeaderAndQueryParm("application/json;version=2; charset=utf-8", null);
 
 		assertNotNull(contentType);
@@ -163,14 +169,55 @@ public class FormatsTest
 	}
 
 	@Test
-	public void testParseHeaderJSONv2WithCharset() {
+	void testParseHeaderJSONv2WithCharset() {
 		ContentType contentType = Formats.parseHeader("application/json;version=2; charset=utf-8");
 
 		assertNotNull(contentType);
 		assertEquals("application/json", contentType.getType());
 	}
 
+	@Test
+	void testParseHeaderFromFirefox()
+	{
+		//The following header comes from firefox
+		ContentType contentType = Formats.parseHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+		assertNotNull(contentType);
+		assertEquals(Formats.XML, contentType.toString());
+	}
 
+	@EnumSource(ParseHeaderClassAliasTest.class)
+	@ParameterizedTest
+	void testParseHeaderWithClass(ParseHeaderClassAliasTest test)
+	{
+		ContentType contentType = Formats.parseHeader(test._contentType, test._class);
+		assertNotNull(contentType);
+		assertEquals(test._expectedType, contentType.toString());
+	}
 
+	enum ParseHeaderClassAliasTest
+	{
+		COUNTY_DEFAULT(County.class, Formats.DEFAULT),
+		COUNTY_JSON(County.class, Formats.JSON);
 
+		final Class<? extends CwmsDTOBase> _class;
+		final String _contentType;
+		final String _expectedType;
+
+		ParseHeaderClassAliasTest(Class<? extends CwmsDTOBase> aClass, String contentType)
+		{
+			_class = aClass;
+			_contentType = contentType;
+
+			String expectedType = _contentType;
+
+			for (FormattableWith formattableWith : _class.getAnnotationsByType(FormattableWith.class))
+			{
+				if (Arrays.asList(formattableWith.aliases()).contains(_contentType))
+				{
+					expectedType = formattableWith.contentType();
+				}
+			}
+			_expectedType = expectedType;
+		}
+	}
 }
