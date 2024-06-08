8d9d75bdd905a9e4076e3e0ab2e85f9562a4637e
Ryan Miles <ryanm@rmanet.com>
2024-06-07 17:52:03 -0700

    Adding alias testing to BasinControllerTestIT

---
+++ cwms-data-api/src/test/java/cwms/cda/api/BasinControllerTestIT.java
@@ -0,0 +1,74 @@
+package cwms.cda.api;
+
+import cwms.cda.formatters.Formats;
+import io.restassured.filter.log.LogDetail;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.net.URLEncoder;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@Tag("integration")
+class BasinControllerTestIT extends DataApiTestIT
+{
+	private static final String SPK = "SPK";
+
+	@EnumSource(AliasTest.class)
+	@ParameterizedTest
+	void test_getOne_aliases(AliasTest test) throws Exception
+	{
+		String requestId = "TEST";
+		String urlencoded = URLEncoder.encode(requestId, "UTF-8");
+		given()
+			.log().ifValidationFails(LogDetail.ALL,true)
+			.accept(test._accept)
+			.queryParam(Controllers.OFFICE, SPK)
+		.when()
+			.redirects().follow(true)
+			.redirects().max(3)
+			.get("/basin/" + requestId)
+		.then()
+			.log().ifValidationFails(LogDetail.ALL,true)
+			.assertThat()
+			.contentType(is(test._expectedContentType));
+	}
+
+	@EnumSource(AliasTest.class)
+	@ParameterizedTest
+	void test_getAll_aliases(AliasTest test)
+	{
+		given()
+			.log().ifValidationFails(LogDetail.ALL,true)
+			.accept(test._accept)
+			.queryParam(Controllers.OFFICE, SPK)
+		.when()
+			.redirects().follow(true)
+			.redirects().max(3)
+			.get("/basin/")
+		.then()
+			.log().ifValidationFails(LogDetail.ALL,true)
+			.assertThat()
+			.contentType(is(test._expectedContentType));
+	}
+
+	enum AliasTest
+	{
+		DEFAULT(Formats.DEFAULT, Formats.NAMED_PGJSON),
+		PGJSON(Formats.PGJSON, Formats.NAMED_PGJSON),
+		NAMED_PGJSON(Formats.NAMED_PGJSON, Formats.NAMED_PGJSON),
+		;
+		final String _accept;
+		final String _expectedContentType;
+
+		AliasTest(String accept, String expectedContentType)
+		{
+			_accept = accept;
+			_expectedContentType = expectedContentType;
+		}
+	}
+}
