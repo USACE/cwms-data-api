bd449faae0df7de276ccbe054d1aea8d59ec313c
Ryan Miles <ryanm@rmanet.com>
2024-06-19 11:27:41 -0700

    Deprecating old parseHeader methods

--- cwms-data-api/src/main/java/cwms/cda/formatters/Formats.java
+++ cwms-data-api/src/main/java/cwms/cda/formatters/Formats.java
@@ -224,6 +224,7 @@ public class Formats {
      * @return an appropriate standard mimetype for lookup
      * @throws FormattingException if the header and queryParam are both supplied or neither are
      */
+    @Deprecated
     public static ContentType parseHeaderAndQueryParm(String header, String queryParam) {
         return parseHeaderAndQueryParm(header, queryParam, null);
     }
@@ -292,6 +293,7 @@ public class Formats {
         return retVal;
     }
 
+    @Deprecated
     public static ContentType parseHeader(String header) {
         return parseHeader(header, null);
     }
