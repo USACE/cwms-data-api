e6de2f42e2b9084cca2b6fb522369323f9bc4b5b
Bryson Spilman <bryson@rmanet.com>
2024-06-27 08:39:32 -0700

    Implementing StreamDao with tests

--- cwms-data-api/src/test/java/cwms/cda/data/dto/StreamNodeTest.java
+++ cwms-data-api/src/test/java/cwms/cda/data/dto/StreamNodeTest.java
@@ -36,7 +36,7 @@ import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StreamNodeTest {
+public final class StreamNodeTest {
 
     @Test
     void createStreamNode_allFieldsProvided_success() {
