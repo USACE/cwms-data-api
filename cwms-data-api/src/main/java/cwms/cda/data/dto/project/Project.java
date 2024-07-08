cdfffec760e15bac4dc202b4448d5c63ebac6ec9
rma-rripken <ryan@rmanet.com>
2024-07-08 10:39:01 -0700

    Updated Content-type for reservoir Project class.
    Making use of getRule method in DeleteMethod.
    ProjectControllerIT using Formats class instead of ObjectMapper.  ProjectTest using assertAll.

--- cwms-data-api/src/main/java/cwms/cda/data/dto/project/Project.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/project/Project.java
@@ -42,7 +42,7 @@ import java.time.Instant;
 @JsonDeserialize(builder = Project.Builder.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
-@FormattableWith(contentType = Formats.JSON, formatter = JsonV2.class)
+@FormattableWith(contentType = Formats.JSONV1, aliases = {Formats.DEFAULT, Formats.JSON}, formatter = JsonV2.class)
 public class Project implements CwmsDTOBase {
 
     private final Location location;
