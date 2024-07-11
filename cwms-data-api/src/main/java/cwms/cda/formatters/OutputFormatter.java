4a2a988c3c04373865746803139f7549dac8bcbc
Adam Korynta <adam@rmanet.com>
2024-07-11 07:35:25 -0700

    add content "type" to error message on deserialization

--- cwms-data-api/src/main/java/cwms/cda/formatters/OutputFormatter.java
+++ cwms-data-api/src/main/java/cwms/cda/formatters/OutputFormatter.java
@@ -6,19 +6,18 @@ import java.util.List;
 import cwms.cda.data.dto.CwmsDTOBase;
 
 public interface OutputFormatter {
+    String DESERIALIZE_CONTENT_MESSAGE = "Could not deserialize: %s of type: %s";
+    String UNSUPPORTED_MESSAGE = "Unable to process your request. Deserialization of %s not yet supported.";
     String getContentType();
     String format(CwmsDTOBase dto);
     String format(List<? extends CwmsDTOBase> dtoList);
     default <T extends CwmsDTOBase> T parseContent(String content, Class<T> type) {
-        throw new UnsupportedOperationException("Unable to process your request. Deserialization of "
-                + getContentType() + " not yet supported.");
+        throw new UnsupportedOperationException(String.format(UNSUPPORTED_MESSAGE, getContentType()));
     }
     default <T extends CwmsDTOBase> List<T> parseContentList(String content, Class<T> type) {
-        throw new UnsupportedOperationException("Unable to process your request. Deserialization of "
-            + getContentType() + " not yet supported.");
+        throw new UnsupportedOperationException(String.format(UNSUPPORTED_MESSAGE, getContentType()));
     }
     default <T extends CwmsDTOBase> T parseContent(InputStream content, Class<T> type) {
-        throw new UnsupportedOperationException("Unable to process your request. Deserialization of "
-                + getContentType() + " not yet supported.");
+        throw new UnsupportedOperationException(String.format(UNSUPPORTED_MESSAGE, getContentType()));
     }
 }
