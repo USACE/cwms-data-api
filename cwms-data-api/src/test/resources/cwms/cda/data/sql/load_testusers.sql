f2ce409e0e8fd83097fd2a9db79defefaca091b9
Adam Korynta <adam@rmanet.com>
2024-09-04 14:24:59 -0700

    revert change to load testusers

--- cwms-data-api/src/test/resources/cwms/cda/data/sql/load_testusers.sql
+++ cwms-data-api/src/test/resources/cwms/cda/data/sql/load_testusers.sql
@@ -1,9 +1,8 @@
+declare
+    group_list cwms_20.char_32_array_type;
 begin
-    create user "l2hectest" identified by "l2hectest";
-    create user "l2hectest_vt" identified by "l2hectestvt";
-    create user "m5hectest" identified by "swt99db";
-    create user "user2" identified by "user2";
-    create user "&webuser" identified by "&password";
-    create user "&user" identified by "&password";
-    create user "&pduser" identified by "&password";
+    group_list := cwms_20.CHAR_32_ARRAY_TYPE('CWMS Users');
+    cwms_20.cwms_sec.create_user('user','blah',group_list, 'HQ');
+    cwms_20.cwms_sec.create_user('user2','blah',NULL,'SPK');
+    cwms_20.cwms_sec.create_user('s0webtest','&password.',group_list,'HQ');
 end;
