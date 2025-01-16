acd253c61ae007b5eb85a4a5b1ed820e55de7842
Charles Graham SWT <krowven@gmail.com>
2025-01-16 16:50:47 -0600

    Use Relative base path

--- cda-gui/vite.config.js
+++ cda-gui/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd(), "");
-    const BASE_PATH = env?.BASE_PATH ?? "/cwms-data";
+    // const env = loadEnv(mode, process.cwd(), "");
+    // const BASE_PATH = env?.BASE_PATH ?? "/cwms-data";
     return {
-        base: BASE_PATH,
+        base: "./",
         plugins: [react()],
-        define: {
-            "import.meta.env.BASE_PATH": JSON.stringify(BASE_PATH)
-        },
+        // define: {
+        //     "import.meta.env.BASE_PATH": JSON.stringify(BASE_PATH)
+        // },
         experimental: {
             renderBuiltUrl(filename, { hostType }) {
                 console.log('render url', filename)
