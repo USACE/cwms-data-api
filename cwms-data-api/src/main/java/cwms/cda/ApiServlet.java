932703f629c2674ba525cbb1392db45ec5fd1355
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-02-21 10:52:47 -0800

    Feature/354 openid auth (#1007)
    Adds support for initial user creation from a trusted JWT.
    
    NOTES:
    - Requires "execute on cwms_upass" for the web_user role.
    - Still some work to be done on mapping claims, some will be ignored but
    the test keycloak just doesn't have several configured at the moment.
    
    Uses the "principle_name" (note: spelled incorrectly in database) field
    to store the combination of JWT issuer and subject claim . The subject
    is a UUID and so the combination of issue+subject is always unique. This
    was done after discovering that the one of the test infrastructure OIDC
    providers doesn't include the CAC EDIPI anywhere and it could not be
    used for user lookup.
    
    Using a full identity provider principal is better anyways, so may as
    well start here.
    
    Additionally updates the docker-compose to require no external setup.
    This should make testing and development far easier for newcomers to the
    project.

--- cwms-data-api/src/main/java/cwms/cda/ApiServlet.java
+++ cwms-data-api/src/main/java/cwms/cda/ApiServlet.java
@@ -457,6 +457,7 @@ public class ApiServlet extends HttpServlet {
                 })
                 .routes(this::configureRoutes)
                 .javalinServlet();
+        logger.atInfo().log("Javalin initialized.");
     }
 
     private String obtainFullVersion(ServletConfig servletConfig) throws ServletException {
@@ -479,7 +480,6 @@ public class ApiServlet extends HttpServlet {
         } catch (ServiceNotFoundException err) {
             throw new RuntimeException("Unable to initialize access manager",err);
         }
-
     }
 
     protected void configureRoutes() {
