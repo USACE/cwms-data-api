1eecd0e5eead477d678e712292ba30804e177d33
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-03-24 21:39:21 +0000

    Create app user, or just update password.

--- .github/actions/database-migration-image/cda-user.sql
+++ .github/actions/database-migration-image/cda-user.sql
@@ -1,2 +1,17 @@
--- TODO; need to update cdk setup before this is useful
--- File here created as placeholder and reminder.
+set define on
+@/tmp/cda-user-info
+
+begin
+    -- create user
+    cwms_sec.add_cwms_user('&&cda_user', NULL, 'HQ');
+    -- set permissions for all offices
+    for ofc in (select office_id from av_office where office_id not in ('CWMS', 'UNK'))
+    loop
+        cwms_sec.add_user_to_group('&&cda_user','All Users', ofc.office_id);
+        cwms_sec.add_user_to_group('&&cda_user','CWMS Users', ofc.office_id);
+        cwms_sec.add_user_to_group('&&cda_user','CWMS DBA Users', ofc.office_id);
+        cwms_sec.add_user_to_group('&&cda_user','CWMS User Admins', ofc.office_id);
+        cwms_sec.add_user_to_group('&&cda_user','CWMS PD Users', ofc.office_id);
+    end loop;
+end;
+/
