2cdd2fc1402665bbdfefb4a82d02bdc095985c82
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-03-25 20:42:24 +0000

    reset CWMS_20 password on subsequent runs.

--- .github/actions/database-migration-image/entry.sh
+++ .github/actions/database-migration-image/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export CWMS_PASSWORD=`tr -cd '[:alnum:]' < /dev/urandom | fold -w25 | head -n1`
 cat > /tmp/create-app-user.sql <<EOF
 declare
     user_count integer;
@@ -9,6 +9,11 @@ begin
         execute immediate 'create user &1 identified by "&2"';
     else
         execute immediate 'alter user &1 identified by "&2"';
+    end if;
+    -- assign a new CWMS_20 password for new operations
+    select count(*) into user_count from dba_users where upper(username)='CWMS_20';
+    if (user_count > 0) then
+        execute immediate 'alter user cwms_20 identified by "&3"';
     end if;
 end;
 /
@@ -20,6 +25,6 @@ EOF
 
 # Create app user first, one of the migration steps is to update the "cwms user" information for that user
 
-sqlplus $BUILDUSER/$BUILDUSER_PASSWORD@$DB_HOST_PORT$DB_NAME @/tmp/create-app-user "$CDA_USERNAME" "$CDA_PASSWORD"
+sqlplus $BUILDUSER/$BUILDUSER_PASSWORD@$DB_HOST_PORT$DB_NAME @/tmp/create-app-user "$CDA_USERNAME" "$CDA_PASSWORD" "$CWMS_PASSWORD"
 
 exec $*
