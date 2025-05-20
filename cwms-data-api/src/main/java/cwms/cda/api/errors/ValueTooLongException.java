93e4dcaf3db234801920c845e093a5a22fd0a391
zack-rma <zack@rmanet.com>
2025-05-20 14:29:59 -0700

    Added error checks to parsing

--- cwms-data-api/src/main/java/cwms/cda/api/errors/ValueTooLongException.java
+++ cwms-data-api/src/main/java/cwms/cda/api/errors/ValueTooLongException.java
@@ -60,8 +60,14 @@ public final class ValueTooLongException extends RuntimeException {
 			throw new IllegalArgumentException("The provided message does not appear to be an ORA-12899 error message.");
 		}
 		String[] parts = messageToParse.split("\"");
+		if (parts.length < 3) {
+			throw new IllegalArgumentException("The provided message does not contain the expected format.");
+		}
 		String parameter = parts[parts.length - 2];
 		String lengthString = parts[parts.length - 1];
+		if (!lengthString.contains("actual:") || !lengthString.contains("maximum:")) {
+			throw new IllegalArgumentException("The provided message does not contain the expected length information.");
+		}
 		int actualLength = Integer.parseInt(lengthString.split("actual:")[1].split(",")[0].trim());
 		int maxLength = Integer.parseInt(lengthString.split("maximum:")[1].split("\\)")[0].trim());
 		return new ValueTooLongException(parameter, actualLength, maxLength, cause, suppressIncidentId);
