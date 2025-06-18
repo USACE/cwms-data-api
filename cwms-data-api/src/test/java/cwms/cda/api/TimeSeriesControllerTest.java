8e8bfd0b068a2581f27b520c35a19f5eaf25b5e5
ryan <ryan@rmanet.com>
2025-06-18 15:56:03 -0700

    Removing broken mock test.
    Introducing the TimeSeriesRequestParameter object broke the mock but we never really adopted that style of test.  Removing rather than trying to fix

--- cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesControllerTest.java
+++ cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesControllerTest.java
@@ -1,24 +1,10 @@
 package cwms.cda.api;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNotNull;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import com.codahale.metrics.MetricRegistry;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import cwms.cda.data.dao.TimeSeriesDao;
 import cwms.cda.data.dto.TimeSeries;
 import cwms.cda.formatters.ContentType;
 import cwms.cda.formatters.Formats;
-import cwms.cda.formatters.json.JsonV2;
-import io.javalin.core.util.Header;
-import io.javalin.http.Context;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -26,103 +12,16 @@ import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZonedDateTime;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.TimeZone;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.jetbrains.annotations.NotNull;
-import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class TimeSeriesControllerTest extends ControllerTest {
-    @Test
-    void testDaoMock() throws JsonProcessingException     {
-        String officeId = "LRL";
-        String tsId = "RYAN3.Stage.Inst.5Minutes.0.ZSTORE_TS_TEST";
-        TimeSeries expected = buildTimeSeries(officeId, tsId);
 
-        // build a mock dao that returns a pre-built ts when called a certain way
-        TimeSeriesDao dao = mock(TimeSeriesDao.class);
-
-        //    String cursor,
-        //    int pageSize,
-        //    String names,
-        //    String office,
-        //    String unit,
-        //    ZonedDateTime begin,
-        //    ZonedDateTime end,
-        //    ZonedDateTime versionDate
-        //    boolean trim
-
-        when(
-                dao.getTimeseries(eq(""), eq(500), eq(tsId), eq(officeId), eq("EN"),
-                         isNotNull(), isNotNull(), isNull(), eq(true), eq(false))).thenReturn(expected);
-
-
-        // build mock request and response
-        final HttpServletRequest request= mock(HttpServletRequest.class);
-        final HttpServletResponse response = mock(HttpServletResponse.class);
-        final Map<String, ?> map = new LinkedHashMap<>();
-
-        when(request.getAttribute("office-id")).thenReturn(officeId);
-        when(request.getAttribute("database")).thenReturn(null);
-
-        when(request.getHeader(Header.ACCEPT)).thenReturn(Formats.JSONV2);
-
-        Map<String, String> urlParams = new LinkedHashMap<>();
-        urlParams.put("office", officeId);
-        urlParams.put("name", tsId);
-
-        String paramStr = buildParamStr(urlParams);
-
-        when(request.getQueryString()).thenReturn(paramStr);
-        when(request.getRequestURL()).thenReturn(new StringBuffer( "http://127.0.0.1:7001/timeseries"));
-
-        // build real context that uses the mock request/response
-        Context ctx = new Context(request, response, map);
-
-        // Build a controller that doesn't actually talk to database
-        TimeSeriesController controller = new TimeSeriesController(new MetricRegistry()){
-            @Override
-            protected DSLContext getDslContext(Context ctx) {
-                return null;
-            }
-
-            @NotNull
-            @Override
-            protected TimeSeriesDao getTimeSeriesDao(DSLContext dsl) {
-                return dao;
-            }
-        };
-        // make controller use our mock dao
-
-        // Do a controller getAll with our context
-        controller.getAll(ctx);
-
-        // Check that the controller accessed our mock dao in the expected way
-        verify(dao, times(1)).
-                getTimeseries(eq(""), eq(500), eq(tsId), eq(officeId), eq("EN"),
-                         isNotNull(), isNotNull(), isNull(), eq(true), eq(false));//
-
-        // Make sure controller thought it was happy
-        verify(response).setStatus(200);
-        // And make sure controller returned json
-        verify(response).setContentType(Formats.JSONV2);
-
-        String result = ctx.resultString();
-        assertNotNull(result);  // MAke sure we got some sort of response
-
-        // Turn json response back into a TimeSeries object
-        ObjectMapper om = JsonV2.buildObjectMapper();
-        TimeSeries actual = om.readValue(result, TimeSeries.class);
-
-        assertSimilar(expected, actual);
-    }
 
     private void assertSimilar(TimeSeries expected, TimeSeries actual) {
         // Make sure ts we got back resembles the fakeTS our mock dao was supposed to return.
