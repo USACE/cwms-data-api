ddf28acb13ac4fabfcf6ba176e228f427f527d62
zack-rma <zack@rmanet.com>
2025-06-23 09:35:30 -0700

    Reverted logging level change used for testing

--- cwms-data-api/src/main/java/cwms/cda/data/dao/LocationLevelsDaoImpl.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dao/LocationLevelsDaoImpl.java
@@ -239,7 +239,7 @@ public class LocationLevelsDaoImpl extends JooqDao<LocationLevel> implements Loc
 
         final SelectLimitPercentAfterOffsetStep<Record> queryFinal = query;
 
-        logger.severe(() -> "getLocationLevels query: " + queryFinal.getSQL(ParamType.INLINED));
+        logger.info(() -> "getLocationLevels query: " + queryFinal.getSQL(ParamType.INLINED));
 
         query.stream().forEach(r -> parseLevels(r, builderMap, unit));
 
