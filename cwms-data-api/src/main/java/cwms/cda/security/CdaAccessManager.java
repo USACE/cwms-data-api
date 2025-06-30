0c81b575884a4b074ba5aa0890d34e75e0026f40
Zack Olson <zack@rmanet.com>
2025-06-30 13:49:58 -0700

    Issue 1107: Add rate limiting to rate endpoints (#1161)
    Fixes #1107
    
    Adds rate limiting to ratings endpoints. Designed for future generalized
    application to unauthorized endpoints.

--- cwms-data-api/src/main/java/cwms/cda/security/CdaAccessManager.java
+++ cwms-data-api/src/main/java/cwms/cda/security/CdaAccessManager.java
@@ -1,5 +1,12 @@
 package cwms.cda.security;
 
+import com.google.common.flogger.FluentLogger;
+import io.javalin.http.HttpResponseException;
+import io.javalin.http.util.NaiveRateLimit;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import cwms.cda.ApiServlet;
@@ -10,15 +17,43 @@ import io.javalin.core.security.AccessManager;
 import io.javalin.core.security.RouteRole;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import java.util.concurrent.TimeUnit;
 
 public final class CdaAccessManager implements AccessManager {
+    public static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    // specify the maximum number of requests allowed per time unit
+    public static final String REQUEST_LIMIT_KEY = "cwms.dataapi.request.limit";
+    private static final int REQUEST_LIMIT = Integer.parseInt(System.getProperty(REQUEST_LIMIT_KEY, "100"));
+    private static final TimeUnit REQUEST_LIMIT_UNIT = TimeUnit.MINUTES;
+    private final Map<String, RouteRole[]> rateLimitedPaths = new HashMap<>();
 
     @Override
-    public void manage(Handler handler, Context ctx, Set<RouteRole> routeRoles) throws Exception {
-        DataApiPrincipal p = getApiPrincipal(ctx);
-        AuthDao.isAuthorized(ctx,p,routeRoles);
-        prepareContext(ctx, p);
+    public void  manage(Handler handler, Context ctx, Set<RouteRole> routeRoles) throws Exception {
+        DataApiPrincipal principal = getApiPrincipal(ctx);
+        AuthDao.isAuthorized(ctx, principal, routeRoles);
+        checkRateLimit(ctx);
+        prepareContext(ctx, principal);
         handler.handle(ctx);
+    }
+
+    private void checkRateLimit(Context ctx) {
+        String path = ctx.endpointHandlerPath();
+        RouteRole[] routeRoles = rateLimitedPaths.get(path);
+        if (routeRoles != null && routeRoles.length != 0) {
+            try {
+                NaiveRateLimit.requestPerTimeUnit(ctx, REQUEST_LIMIT, REQUEST_LIMIT_UNIT);
+            } catch (HttpResponseException ex) {
+                try {
+                    AuthDao.isAuthorized(ctx, getApiPrincipal(ctx), new HashSet<>(Arrays.asList(routeRoles)));
+                } catch (CwmsAuthException e) {
+                    // If user is unauthorized, rethrow the rate limit exception
+                    logger.atFinest().log("Unauthorized access to rate limited path: %s", path, e);
+                    throw new HttpResponseException(ex.getStatus(), "Rate limit exceeded. "
+                        + "Please try again later or contact an administrator if you believe this is an error.");
+                }
+            }
+        }
     }
 
     private DataApiPrincipal getApiPrincipal(Context ctx) {
@@ -32,5 +67,9 @@ public final class CdaAccessManager implements AccessManager {
         } else {
             AuthDao.prepareContextWithUser(ctx, p);
         }
+    }
+
+    public void addRateLimitedEndpoint(String path, RouteRole[] roles) {
+        rateLimitedPaths.put(path, roles);
     }
 }
