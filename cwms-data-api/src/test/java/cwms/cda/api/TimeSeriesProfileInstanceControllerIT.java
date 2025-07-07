d608110eba28ee7482f7ea24bc24d6b5cfc0e6c7
zack-rma <zack@rmanet.com>
2025-07-07 13:57:58 -0700

    Updated TS IDs per feedback

--- cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesProfileInstanceControllerIT.java
+++ cwms-data-api/src/test/java/cwms/cda/api/TimeSeriesProfileInstanceControllerIT.java
@@ -1130,17 +1130,14 @@ final class TimeSeriesProfileInstanceControllerIT extends DataApiTestIT {
 
         assertParserInDb(tspParserIndexed3);
 
-        String newTsId = "Sacramento River.Elev.Total.1DayLocal.0.Raw";
-
-        String data = tspData3.replace("Sacramento River.Elev.Total.0.1Hour.Raw",
-                newTsId);
+        String newTsId = "Sacramento River.Precip-Cumulative.Inst.1DayLocal.0.Raw";
 
         // Create instance
         given()
             .log().ifValidationFails(LogDetail.ALL, true)
             .accept(Formats.JSONV1)
             .contentType(Formats.JSONV1)
-            .body(data)
+            .body(tspData3)
             .header(AUTH_HEADER, user.toHeaderValue())
             .queryParam(METHOD, StoreRule.REPLACE_ALL)
             .queryParam(OVERRIDE_PROTECTION, false)
@@ -1162,7 +1159,7 @@ final class TimeSeriesProfileInstanceControllerIT extends DataApiTestIT {
             .log().ifValidationFails(LogDetail.ALL, true)
             .accept(Formats.JSONV1)
             .contentType(Formats.JSONV1)
-            .body(data)
+            .body(tspData3)
             .header(AUTH_HEADER, user.toHeaderValue())
             .queryParam(METHOD, StoreRule.REPLACE_ALL)
             .queryParam(OVERRIDE_PROTECTION, false)
