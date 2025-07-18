ca51af824f4a88d072b363b9aecc8dbc52cbef80
Charles Graham SWT <krowven@gmail.com>
2025-07-18 13:55:51 -0500

    Correct issue with innerHTML tags not being executed when appended tot he DOM.
    Reinsert them as script DOM nodes

--- cwms-data-api/src/main/webapp/js/header.js
+++ cwms-data-api/src/main/webapp/js/header.js
@@ -1,17 +1,24 @@
-window.addEventListener("load", function () {
-    let coll_mob = document.getElementsByClassName("collapsible-mobile");
-    for (const col_m_idx = 0; col_m_idx < coll_mob.length; col_m_idx++) {
-        coll_mob[col_m_idx].addEventListener("click", function (e) {
-            this.classList.toggle("active");
-            const content = this.nextElementSibling;
-            if (content.style.display == "none") {
-                content.style.display = "block";
-            } else {
-                content.style.display = "none";
-            }
-        });
-    }
-}, false);
+// Setup the page's collapses
+let coll_mob = document.getElementsByClassName("collapsible-mobile");
+for (const col_m_idx = 0; col_m_idx < coll_mob.length; col_m_idx++) {
+    coll_mob[col_m_idx].addEventListener("click", function (e) {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display == "none") {
+            content.style.display = "block";
+        } else {
+            content.style.display = "none";
+        }
+    });
+}
+const burgerBtn = document.getElementById("burgerBtn");
+if (burgerBtn) {
+    burgerBtn.addEventListener("click", openNav);
+}
+const closeBtn = document.querySelector(".closeBtn");
+if (closeBtn) {
+    closeBtn.addEventListener("click", closeNav);
+}
 
 // Mobile Burger Bar
 function openNav() {
@@ -19,12 +26,13 @@ function openNav() {
     mobileNav.classList.add("open");
     const child = document.getElementById("mobileNavContent");
     mobileNav.style.right = child.clientWidth - child.offsetWidth + "px";
-    const open = document.getElementById("burgerBtn");
-    open.style.display = "none";
+    const BURGER_BTN = document.getElementById("burgerBtn");
+    BURGER_BTN.style.display = "none";
 }
 
 function closeNav() {
+    const mobileNav = document.getElementById("mobileNav")
     mobileNav.classList.remove("open");
-    const open = document.getElementById("burgerBtn");
-    open.style.display = null;
+    const BURGER_BTN = document.getElementById("burgerBtn");
+    BURGER_BTN.style.display = null;
 }
