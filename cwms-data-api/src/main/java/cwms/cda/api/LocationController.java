dcc64629168a579ac70e603406f8a20ad341f495
Ryan Ripken <89810919+rma-rripken@users.noreply.github.com>
2025-07-29 14:10:09 -0700

    Checking for elevationUnits changes during patch. (#1199)

--- cwms-data-api/src/main/java/cwms/cda/api/LocationController.java
+++ cwms-data-api/src/main/java/cwms/cda/api/LocationController.java
@@ -499,6 +499,8 @@ public class LocationController implements CrudHandler {
                 ? existingLocation.getVerticalDatum() : updatedLocation.getVerticalDatum();
         Double updatedElevation = updatedLocation.getElevation() == null
                 ? existingLocation.getElevation() : updatedLocation.getElevation();
+        String updatedElevationUnits = updatedLocation.getElevationUnits() == null
+                ? existingLocation.getElevationUnits() : updatedLocation.getElevationUnits();
         String updatedMapLabel = updatedLocation.getMapLabel() == null
                 ? existingLocation.getMapLabel() : updatedLocation.getMapLabel();
         String updatedBoundingOfficeId = updatedLocation.getBoundingOfficeId() == null
@@ -521,6 +523,7 @@ public class LocationController implements CrudHandler {
                 .withPublishedLatitude(updatedPublishedLatitude)
                 .withVerticalDatum(updatedVerticalDatum)
                 .withElevation(updatedElevation)
+                .withElevationUnits(updatedElevationUnits)
                 .withMapLabel(updatedMapLabel)
                 .withBoundingOfficeId(updatedBoundingOfficeId)
                 .build();
