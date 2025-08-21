a594b55b93a288b99342ce6358688be7f4c10032
Zack Olson <zack@rmanet.com>
2025-08-21 06:58:43 -0700

    REGI-470: Added lightweight catalog endpoint for location + location kind (#1230)
    Adds support for lightweight data retrieval on locations getAll
    endpoint.
    
    Returns location ID, office ID, and location kind ID.
    
    To be used by REGI in
    AtLocationManager::retrieveAllLocationTemplatesWithKinds method.

--- cwms-data-api/src/main/java/cwms/cda/api/Controllers.java
+++ cwms-data-api/src/main/java/cwms/cda/api/Controllers.java
@@ -156,7 +156,6 @@ public final class Controllers {
     public static final String AGENCY = "agency";
     public static final String QUALITY = "quality";
     public static final String NAMES = "names";
-    public static final String EXCLUDE_KINDS = "exclude-kinds";
     public static final String FILTER_BASE_LOCATIONS = "filter-base-locations";
 
     public static final String GROUP_ID = "group-id";
