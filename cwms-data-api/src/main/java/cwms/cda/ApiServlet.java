ece2d2eef9e6a8c97acfe2fb9f60ce6c725836cb
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-09-04 06:39:08 -0700

    Respond to Options request with reasonable values. (#1289)
    Fixes #1175.
    
    (cherry picked from commit 6d008de736d3909e5d34cfdc1baa39fabf49f91a)

--- cwms-data-api/src/main/java/cwms/cda/ApiServlet.java
+++ cwms-data-api/src/main/java/cwms/cda/ApiServlet.java
@@ -503,6 +503,12 @@ public class ApiServlet extends HttpServlet {
                     ctx.json(errResponse);
                 })
                 .routes(this::configureRoutes)
+                .options("/*", ctx -> {
+                    ctx.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+                    ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Specify allowed methods
+                    ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Specify allowed headers
+                    ctx.status(200); // Respond with a 200 OK status
+                })
                 .javalinServlet();
         logger.atInfo().log("Javalin initialized.");
     }
