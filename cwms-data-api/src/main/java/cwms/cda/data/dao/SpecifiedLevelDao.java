93ffdd0669fd60a5e1f8aa47bdd88ed3e87801f4
Ryan Ripken <89810919+rma-rripken@users.noreply.github.com>
2025-09-19 15:06:08 -0700

    Bugfix/1209 ratings metadata (#1325)
    While looking at #1209 I saw that the session_id wasn't being set in two
    places where it was needed. This pr isn't a fix #1209
    
    (cherry picked from commit 0bc668bf13ad8edbc2c3f354f9bad1773182a968)

--- cwms-data-api/src/main/java/cwms/cda/data/dao/SpecifiedLevelDao.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dao/SpecifiedLevelDao.java
@@ -1,17 +1,25 @@
 package cwms.cda.data.dao;
 
 import cwms.cda.data.dto.SpecifiedLevel;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+
 import java.util.List;
+
+import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Result;
 import usace.cwms.db.jooq.codegen.packages.CWMS_LEVEL_PACKAGE;
-import usace.cwms.db.jooq.dao.CwmsDbLevelJooq;
+
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.name;
+
 
 public final class SpecifiedLevelDao extends JooqDao<SpecifiedLevel> {
-    public static final String OFFICE_ID = "OFFICE_ID";
-    public static final String SPECIFIED_LEVEL_ID = "SPECIFIED_LEVEL_ID";
-    public static final String DESCRIPTION = "DESCRIPTION";
+    private static final Field<String> OFFICE_ID_F = field(name("OFFICE_ID"), String.class);
+    private static final Field<String> SPECIFIED_LEVEL_ID_F = field(name("SPECIFIED_LEVEL_ID"), String.class);
+    private static final Field<String> DESCRIPTION_F = field(name("DESCRIPTION"), String.class);
+
 
     public SpecifiedLevelDao(DSLContext dsl) {
         super(dsl);
@@ -19,61 +27,41 @@ public final class SpecifiedLevelDao extends JooqDao<SpecifiedLevel> {
 
 
     public List<SpecifiedLevel> getSpecifiedLevels(String office, String templateIdMask) {
-        List<SpecifiedLevel> retval = new ArrayList<>();
-
-        CwmsDbLevelJooq jooq = new CwmsDbLevelJooq();
-        connection(dsl, c -> {
-            try (ResultSet rs = jooq.catSpecifiedLevels(c, templateIdMask, office)) {
-                while (rs.next()) {
-                    String officeId = rs.getString(OFFICE_ID);
-                    String id = rs.getString(SPECIFIED_LEVEL_ID);
-                    String desc = rs.getString(DESCRIPTION);
-
-                    SpecifiedLevel specLevel = new SpecifiedLevel(id, officeId, desc);
-
-                    retval.add(specLevel);
-                }
-            }
+        return connectionResult(dsl, c -> {
+            Configuration configuration = getDslContext(c, office).configuration();
+            Result<Record> records = CWMS_LEVEL_PACKAGE.call_CAT_SPECIFIED_LEVELS(configuration, templateIdMask, office);
+            return records.map(r -> new SpecifiedLevel(
+                    r.get(SPECIFIED_LEVEL_ID_F),
+                    r.get(OFFICE_ID_F),
+                    r.get(DESCRIPTION_F)
+            ));
         });
-
-        return retval;
     }
 
     public void create(SpecifiedLevel specifiedLevel, boolean failIfExists) {
-        try {
-        dsl.connection(c -> 
-            CWMS_LEVEL_PACKAGE.call_STORE_SPECIFIED_LEVEL(
-                getDslContext(c,specifiedLevel.getOfficeId()).configuration(),
-                specifiedLevel.getId(), specifiedLevel.getDescription(), formatBool(failIfExists),
-                specifiedLevel.getOfficeId())
+        connection(dsl, c ->
+                CWMS_LEVEL_PACKAGE.call_STORE_SPECIFIED_LEVEL(
+                        getDslContext(c, specifiedLevel.getOfficeId()).configuration(),
+                        specifiedLevel.getId(), specifiedLevel.getDescription(), formatBool(failIfExists),
+                        specifiedLevel.getOfficeId())
         );
-        } catch(RuntimeException ex) {
-            throw wrapException(ex);
-        }
-        
+
     }
 
     public void update(String oldSpecifiedLevelId, String newSpecifiedLevelId, String officeId) {
-        try {
-            dsl.connection(c -> CWMS_LEVEL_PACKAGE.call_RENAME_SPECIFIED_LEVEL(
-                    getDslContext(c, officeId).configuration(),
-                    oldSpecifiedLevelId, newSpecifiedLevelId, officeId)
-            );
-        } catch (RuntimeException ex) {
-            throw wrapException(ex);
-        }
+        connection(dsl, c -> CWMS_LEVEL_PACKAGE.call_RENAME_SPECIFIED_LEVEL(
+                getDslContext(c, officeId).configuration(),
+                oldSpecifiedLevelId, newSpecifiedLevelId, officeId)
+        );
+
 
     }
 
     public void delete(String specifiedLevelId, String office) {
         String failIfNotFound = formatBool(true);
-        try {
-            dsl.connection(c -> CWMS_LEVEL_PACKAGE.call_DELETE_SPECIFIED_LEVEL(
-                    getDslContext(c, office).configuration(),
-                    specifiedLevelId, failIfNotFound, office)
-            );
-        } catch (RuntimeException ex) {
-            throw wrapException(ex);
-        }
+        connection(dsl, c -> CWMS_LEVEL_PACKAGE.call_DELETE_SPECIFIED_LEVEL(
+                getDslContext(c, office).configuration(),
+                specifiedLevelId, failIfNotFound, office)
+        );
     }
 }
