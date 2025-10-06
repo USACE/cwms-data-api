deeed0ae20cfd18532329c35b9f2392e8b271682
Ryan Ripken <89810919+rma-rripken@users.noreply.github.com>
2025-10-06 15:26:39 -0700

    Add a cache busting parameter "_cb" to requests sent from swagger-ui page.   Also request proxies not to cache. (#1350)
    (cherry picked from commit 6d97a5509f2a56336265c4372eabb08994617b1b)

--- cda-gui/src/pages/swagger-ui/index.jsx
+++ cda-gui/src/pages/swagger-ui/index.jsx
@@ -1,25 +1,36 @@
 import SwaggerUIBundle from "swagger-ui-dist/swagger-ui-bundle";
 import "swagger-ui-dist/swagger-ui.css";
 
-import { useEffect } from "react";
-import { getBasePath } from "../../utils/base";
+import {useEffect} from "react";
+import {getBasePath} from "../../utils/base";
 
 export default function SwaggerUI() {
-  useEffect(() => {
-    // document.querySelector("#swagger-ui").prepend(Index)
-    // TODO: Add page index to top of page
-    // Alter the page title to match the swagger page
-    document.title = "CWMS Data API for Data Retrieval - Swagger UI";
-    // Begin Swagger UI call region
-    // TODO: add endpoint that dynamic returns swagger generated doc
-    SwaggerUIBundle({
-      url: getBasePath() + "/swagger-docs",
-      dom_id: "#swagger-ui",
-      deepLinking: false,
-      presets: [SwaggerUIBundle.presets.apis],
-      plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-    });
-  }, []);
+    useEffect(() => {
+        // document.querySelector("#swagger-ui").prepend(Index)
+        // TODO: Add page index to top of page
+        // Alter the page title to match the swagger page
+        document.title = "CWMS Data API for Data Retrieval - Swagger UI";
+        // Begin Swagger UI call region
+        // TODO: add endpoint that dynamic returns swagger generated doc
+        SwaggerUIBundle({
+            url: getBasePath() + "/swagger-docs",
+            dom_id: "#swagger-ui",
+            deepLinking: false,
+            presets: [SwaggerUIBundle.presets.apis],
+            plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+            requestInterceptor: (req) => {
+                // Add a cache-busting query param
+                const sep = req.url.includes("?") ? "&" : "?";
+                req.url = `${req.url}${sep}_cb=${Date.now()}`;
 
-  return <div id="swagger-ui"></div>;
+                // Also ask intermediaries not to serve from cache
+                req.headers["Cache-Control"] = "no-cache, no-store, max-age=0";
+                req.headers["Pragma"] = "no-cache";
+
+                return req;
+            },
+        });
+    }, []);
+
+    return <div id="swagger-ui"></div>;
 }
