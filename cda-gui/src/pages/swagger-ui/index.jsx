650cf633d2b8f3e8a556b0d5f07daa28d2d95b05
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-10-15 17:33:07 +0000

    Auth + PKCE from Swagger UI works.

--- cda-gui/src/pages/swagger-ui/index.jsx
+++ cda-gui/src/pages/swagger-ui/index.jsx
@@ -5,32 +5,42 @@ import { useEffect } from "react";
 import { getBasePath } from "../../utils/base";
 
 export default function SwaggerUI() {
-  useEffect(() => {
-    // document.querySelector("#swagger-ui").prepend(Index)
-    // TODO: Add page index to top of page
-    // Alter the page title to match the swagger page
-    document.title = "CWMS Data API for Data Retrieval - Swagger UI";
-    // Begin Swagger UI call region
-    // TODO: add endpoint that dynamic returns swagger generated doc
-    SwaggerUIBundle({
-      url: getBasePath() + "/swagger-docs",
-      dom_id: "#swagger-ui",
-      deepLinking: false,
-      presets: [SwaggerUIBundle.presets.apis],
-      plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-      requestInterceptor: (req) => {
-        // Add a cache-busting query param
-        const sep = req.url.includes("?") ? "&" : "?";
-        req.url = `${req.url}${sep}_cb=${Date.now()}`;
+    useEffect(() => {
+        // document.querySelector("#swagger-ui").prepend(Index)
+        // TODO: Add page index to top of page
+        // Alter the page title to match the swagger page
+        document.title = "CWMS Data API for Data Retrieval - Swagger UI";
+        // Begin Swagger UI call region
+        // TODO: add endpoint that dynamic returns swagger generated doc
+        const ui = SwaggerUIBundle({
+            url: getBasePath() + "/swagger-docs",
+            dom_id: "#swagger-ui",
+            deepLinking: false,
+            presets: [SwaggerUIBundle.presets.apis],
+            plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+            requestInterceptor: (req) => {
+                // Add a cache-busting query param... but only if it's to our api. Some
+                // external systems, like keycloak, don't allow random unknown parameters.
+                const origin = window.location.origin;                
+                const re = new RegExp(`^${origin}.*`)
+                if (re.test(req.url))
+                {
+                    const sep = req.url.includes("?") ? "&" : "?";
+                    req.url = `${req.url}${sep}_cb=${Date.now()}`;
 
-        // Also ask intermediaries not to serve from cache
-        req.headers["Cache-Control"] = "no-cache, no-store, max-age=0";
-        req.headers["Pragma"] = "no-cache";
-
-        return req;
-      },
-    });
-  }, []);
+                    // Also ask intermediaries not to serve from cache
+                    req.headers["Cache-Control"] = "no-cache, no-store, max-age=0";
+                    req.headers["Pragma"] = "no-cache";
+                }
+                return req;
+            },
+        });
+        // The client id may, in the future, need to be changed. When that happens deal with it then.
+        ui.initOAuth({
+            clientId:"cwms",
+            usePkceWithAuthorizationCodeGrant: true
+        })
+    }, []);
 
   return <div id="swagger-ui"></div>;
 }
