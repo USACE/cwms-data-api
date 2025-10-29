ed5a3bddfaa07865dcbc6b4ae847b34aa6f7547b
Ryan Miles <ryanm@rmanet.com>
2025-10-29 14:43:55 -0700

    Refactor OpenApiTestHelperTest to separate Handler and CrudHandler tests, add UsersController validation.

--- cwms-data-api/src/test/java/helpers/OpenApiTestHelperTest.java
+++ cwms-data-api/src/test/java/helpers/OpenApiTestHelperTest.java
@@ -3,10 +3,12 @@ package helpers;
 import cwms.cda.api.OfficeController;
 import cwms.cda.api.TextTimeSeriesValueController;
 import cwms.cda.api.TimeSeriesController;
+import cwms.cda.api.auth.users.UsersController;
 import cwms.cda.api.auth.users.roles.AddRoleController;
 import cwms.cda.api.rating.RatingController;
 import cwms.cda.api.watersupply.WaterUserDeleteController;
 import io.javalin.apibuilder.CrudHandler;
+import io.javalin.http.Handler;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -46,17 +48,19 @@ class OpenApiTestHelperTest {
 
     @Test
     void test_find_handlers() {
-        List<Class<CrudHandler>> classes = OpenApiTestHelper.findClassesOfType(CrudHandler.class, "cwms.cda.api");
-        assertNotNull(classes);
-        assertTrue(classes.contains(OfficeController.class));
-        assertTrue(classes.contains(TimeSeriesController.class));
-        assertTrue(classes.contains(RatingController.class));  // CrudHandler in sub package
-        assertTrue(classes.contains(TextTimeSeriesValueController.class));  // Handler
-        assertTrue(classes.contains(WaterUserDeleteController.class)); // Handler in sub-package
-        assertTrue(classes.contains(AddRoleController.class)); // Handler in sub-sub-package
+        List<Class<CrudHandler>> crudHandlers = OpenApiTestHelper.findClassesOfType(CrudHandler.class, "cwms.cda.api");
+        assertNotNull(crudHandlers);
+        assertTrue(crudHandlers.contains(OfficeController.class));  // CrudHandler
+        assertTrue(crudHandlers.contains(RatingController.class));  // CrudHandler in sub package
+        assertTrue(crudHandlers.contains(UsersController.class));   // CrudHandler in sub-sub package
+
+        List<Class<Handler>> handlers = OpenApiTestHelper.findClassesOfType(Handler.class, "cwms.cda.api");
+        assertTrue(handlers.contains(TextTimeSeriesValueController.class));  // Handler
+        assertTrue(handlers.contains(WaterUserDeleteController.class));      // Handler in sub-package
+        assertTrue(handlers.contains(AddRoleController.class));              // Handler in sub-sub-package
 
         // fail if any class starts with io.javalin
-        classes.forEach(c -> assertFalse(c.getPackage().getName().startsWith("io.javalin")));
+        crudHandlers.forEach(c -> assertFalse(c.getPackage().getName().startsWith("io.javalin")));
     }
 
 
