4f58e8fda7fd78ca345f59f45829acf5ad4a5321
Ryan Miles <ryanm@rmanet.com>
2025-11-24 15:12:36 -0800

    Add static analysis unit test for Controller classes (#1362)
    Fixes #1349, Adding a unit test for static analysis of the Controller implementations.
    
    ---------
    
    Co-authored-by: rma-rripken <89810919+rma-rripken@users.noreply.github.com>

---
+++ cwms-data-api/src/test/java/helpers/OpenApiTestHelperTest.java
@@ -0,0 +1,74 @@
+package helpers;
+
+import cwms.cda.api.Controllers;
+import cwms.cda.api.OfficeController;
+import cwms.cda.api.TextTimeSeriesValueController;
+import cwms.cda.api.auth.users.UsersController;
+import cwms.cda.api.auth.users.roles.AddRoleController;
+import cwms.cda.api.rating.RatingController;
+import cwms.cda.api.watersupply.WaterUserDeleteController;
+import io.javalin.apibuilder.CrudHandler;
+import io.javalin.http.Handler;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenApiTestHelperTest {
+
+    @Test
+    void test_helper_on_office() {
+        List<Method> methods = OpenApiTestHelper.findByName(OfficeController.class, "getAll");
+        assertEquals(1, methods.size());
+
+        Method m = OpenApiTestHelper.findOneByName(OfficeController.class, "getAll");
+
+        assertNotNull(m);
+        OpenApiDocInfo info = OpenApiTestHelper.readDocParams(m);
+        assertNotNull(info);
+        assertEquals(2, info.getQueryParameters().size());
+        assertTrue(info.getQueryParameters().contains(new OpenApiParamInfo(Controllers.FORMAT, false, String.class)));
+        assertTrue(info.getQueryParameters().contains(new OpenApiParamInfo(Controllers.HAS_DATA, false, String.class)));
+
+        assertEquals(0, info.getPathParameters().size());
+
+    }
+
+    @Test
+    void test_helper_on_office_bad_name() {
+        try{
+            OpenApiTestHelper.findOneByName(OfficeController.class, "bad");
+            fail("Should have thrown exception");
+        } catch(RuntimeException ex){
+            assertEquals("Did not find method with name bad", ex.getMessage());
+        }
+    }
+
+    @Test
+    void test_helper_for_interface() {
+        OpenApiDocTestInfo crudDocInfo = OpenApiTestHelper.readOpenApiDocs(CrudHandler.class, OfficeController.class);
+        assertEquals(5, crudDocInfo.getMethodDocs().size());
+
+        OpenApiDocTestInfo handlerDocInfo = OpenApiTestHelper.readOpenApiDocs(Handler.class, TextTimeSeriesValueController.class);
+        assertEquals(1, handlerDocInfo.getMethodDocs().size());
+    }
+
+    @Test
+    void test_find_handlers() {
+        List<Class<CrudHandler>> crudHandlers = OpenApiTestHelper.findClassesOfType(CrudHandler.class);
+        assertTrue(crudHandlers.contains(OfficeController.class));  // CrudHandler
+        assertTrue(crudHandlers.contains(RatingController.class));  // CrudHandler in sub-package
+        assertTrue(crudHandlers.contains(UsersController.class));   // CrudHandler in sub-sub-package
+
+        List<Class<Handler>> handlers = OpenApiTestHelper.findClassesOfType(Handler.class);
+        assertTrue(handlers.contains(TextTimeSeriesValueController.class));  // Handler
+        assertTrue(handlers.contains(WaterUserDeleteController.class));      // Handler in sub-package
+        assertTrue(handlers.contains(AddRoleController.class));              // Handler in sub-sub-package
+
+        // Using assertAll allows us to test all cases and provide feedback for all cases instead of failing on the first failure.
+        assertAll(crudHandlers.stream().map(handler -> () -> assertFalse(handler.getPackage().getName().startsWith("io.javalin"))));
+        assertAll(handlers.stream().map(handler -> () -> assertFalse(handler.getPackage().getName().startsWith("io.javalin"))));
+    }
+}
