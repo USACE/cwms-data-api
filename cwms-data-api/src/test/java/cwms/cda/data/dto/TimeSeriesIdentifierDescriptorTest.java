1253de47c199a459bc8befd9e3048ec40eecea8e
Zack Olson <zack@rmanet.com>
2025-12-01 09:14:01 -0800

    CDA-66: Updated TS identifier descriptor paging (#1481)
    Now includes total number of results in response.
    
    Contributes to resolve #1472
    
    More work to come for addition of alias inclusion query parameter to
    filter out aliased items.
    
    (cherry picked from commit 275b78dfc1ec99ee961b97a399cf83045837f05b)

--- cwms-data-api/src/test/java/cwms/cda/data/dto/TimeSeriesIdentifierDescriptorTest.java
+++ cwms-data-api/src/test/java/cwms/cda/data/dto/TimeSeriesIdentifierDescriptorTest.java
@@ -1,7 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Hydrologic Engineering Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package cwms.cda.data.dto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +38,8 @@ import cwms.cda.formatters.Formats;
 import cwms.cda.formatters.json.JsonV2;
 
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class TimeSeriesIdentifierDescriptorTest {
@@ -25,10 +53,28 @@ class TimeSeriesIdentifierDescriptorTest {
         ObjectWriter ow = om.writerWithDefaultPrettyPrinter();
         String result = ow.writeValueAsString(tsID);
         assertNotNull(result);
+        assertFalse(result.contains("aliases"));
+    }
 
+    @Test
+    void testSerializeWithAliases() throws JsonProcessingException {
+        TimeSeriesIdentifierDescriptor tsID = buildTsId(buildAliases());
+
+        ObjectMapper om = JsonV2.buildObjectMapper();
+
+        ObjectWriter ow = om.writerWithDefaultPrettyPrinter();
+        String result = ow.writeValueAsString(tsID);
+        assertNotNull(result);
+        assertTrue(result.contains("aliases"));
+        assertTrue(result.contains("alias"));
+        assertTrue(result.contains("alias2"));
     }
 
     private TimeSeriesIdentifierDescriptor buildTsId() {
+        return buildTsId(new ArrayList<>());
+    }
+
+    private TimeSeriesIdentifierDescriptor buildTsId(List<String> aliases) {
         TimeSeriesIdentifierDescriptor.Builder builder =
                 new TimeSeriesIdentifierDescriptor.Builder();
         builder = builder.withOfficeId("SWT");
@@ -36,6 +82,7 @@ class TimeSeriesIdentifierDescriptorTest {
         builder = builder.withZoneId(ZoneId.of("America/Los_Angeles"));
         builder = builder.withIntervalOffsetMinutes(0L);
         builder = builder.withActive(true);
+        builder = builder.withAliases(aliases);
 
         return builder.build();
     }
@@ -84,6 +131,31 @@ class TimeSeriesIdentifierDescriptorTest {
         assertEquals(tsID.getTimezoneName(), tsID2.getTimezoneName());
         assertEquals(tsID.getIntervalOffsetMinutes(), tsID2.getIntervalOffsetMinutes());
         assertEquals(tsID.isActive(), tsID2.isActive());
+
+        assertEquals(tsID.getAliases().size(), tsID2.getAliases().size());
+        if (!tsID.getAliases().isEmpty()) {
+            for (int i = 0; i < tsID.getAliases().size(); i++) {
+                assertEquals(tsID.getAliases().get(i), tsID2.getAliases().get(i));
+            }
+        }
+    }
+
+    @Test
+    void jsonRoundtripWithAliases() throws JsonProcessingException {
+
+        TimeSeriesIdentifierDescriptor tsID = buildTsId(buildAliases());
+        assertFalse(tsID.getAliases().isEmpty());
+        TimeSeriesIdentifierDescriptor.Builder builder =
+            new TimeSeriesIdentifierDescriptor.Builder();
+        builder = builder.withTimeSeriesIdentifierDescriptor(tsID);
+        jsonRoundtrip(builder.build());
+    }
+
+    private static List<String> buildAliases() {
+        List<String> aliases = new ArrayList<>();
+        aliases.add("alias");
+        aliases.add("alias2");
+        return aliases;
     }
 
 
