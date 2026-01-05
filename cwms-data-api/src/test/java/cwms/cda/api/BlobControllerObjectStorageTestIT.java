a5216edb295b9238e8e68f4aece661535e199d74
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2026-01-05 13:14:17 -0800

    Adding dependencies so that Minio-setup gets run once during docker-compose

--- cwms-data-api/src/test/java/cwms/cda/api/BlobControllerObjectStorageTestIT.java
+++ cwms-data-api/src/test/java/cwms/cda/api/BlobControllerObjectStorageTestIT.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 import org.togglz.core.context.FeatureContext;
 import org.togglz.core.manager.FeatureManager;
 
@@ -14,11 +17,53 @@ import org.togglz.core.manager.FeatureManager;
 @ExtendWith(BlobControllerObjectStorageTestIT.FeatureEnableExtension.class)
 public class BlobControllerObjectStorageTestIT extends BlobControllerTestIT{
 
+    public static final String MINIO_ADMIN = "minio_admin";
+    public static final String MINIO_ADMIN_SECRET = "saersdbewadfqewrbwreq12rfgweqrffw52354ec";
+    public static final String IMAGE_NAME = "minio/minio:RELEASE.2025-04-22T22-12-26Z";
+    public static final int PORT = 9000;
+    private static final GenericContainer<?> MINIO_CONTAINER = new GenericContainer<>(DockerImageName.parse(IMAGE_NAME))
+            .withExposedPorts(PORT)
+            .withEnv("MINIO_ROOT_USER", MINIO_ADMIN)
+            .withEnv("MINIO_ROOT_PASSWORD", MINIO_ADMIN_SECRET)
+            .withCommand("server /data")
+            .waitingFor(Wait.forHttp("/minio/health/live").forPort(PORT));
+    public static final String BUCKET = "cwms-test";
+    public static final String MINIO_USER = "cda_user";
+    public static final String MINIO_USER_SECRET = "cda_password";
+    public static final String CONTAINER_NAME = "myminio";
+
+
     static class FeatureEnableExtension implements Extension, BeforeAllCallback {
 
         @Override
         public void beforeAll(ExtensionContext context) {
+            if (!MINIO_CONTAINER.isRunning()) {
+                MINIO_CONTAINER.start();
+                setupMinioResources();
+            }
+
+
             setObjectStoreProperties();
+        }
+    }
+
+    private static void setupMinioResources() {
+        try {
+            String address = "http://" + MINIO_CONTAINER.getHost() + ":" + MINIO_CONTAINER.getMappedPort(PORT);
+            GenericContainer<?> mc = new GenericContainer<>(DockerImageName.parse("minio/mc:latest"))
+                    .withCreateContainerCmdModifier(cmd -> cmd.withEntrypoint("/bin/sh", "-c"))
+                    .withCommand(
+                            String.format("mc alias set %s %s %s %s;" , CONTAINER_NAME, address, MINIO_ADMIN, MINIO_ADMIN_SECRET) +
+                                    String.format(" mc admin user add %s %s %s;", CONTAINER_NAME, MINIO_USER, MINIO_USER_SECRET) +
+                                    String.format(" mc mb --ignore-existing %s/%s;", CONTAINER_NAME, BUCKET) +
+                                    String.format(" mc admin policy attach %s readwrite --user %s;", CONTAINER_NAME, MINIO_USER)
+                            )
+                    .withAccessToHost(true);
+            mc.start();
+            // wait for setup to complete
+            while (mc.isRunning()) { Thread.sleep(100); }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to setup MinIO resources", e);
         }
     }
 
@@ -27,15 +72,18 @@ public class BlobControllerObjectStorageTestIT extends BlobControllerTestIT{
     private static void setObjectStoreProperties() {
         // This is to get the feature enabled before we try and use the controller
         FeatureManager featureManager = FeatureContext.getFeatureManager();
+
         wasActive=featureManager.isActive(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
         featureManager.enable(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
         featureManager.isActive(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
 
-        // TODO: Need to figure out a cleaner way to do this
-        System.setProperty("blob.store.endpoint", "http://127.0.0.1:9000");
-        System.setProperty("blob.store.bucket", "cwms-test");
-        System.setProperty("blob.store.accessKey", "cda_user");
-        System.setProperty("blob.store.secretKey", "cda_password");
+        String host = MINIO_CONTAINER.getHost();
+        Integer port = MINIO_CONTAINER.getMappedPort(PORT);
+
+        System.setProperty("blob.store.endpoint", "http://" + host + ":" + port);
+        System.setProperty("blob.store.bucket", BUCKET);
+        System.setProperty("blob.store.accessKey", MINIO_USER);
+        System.setProperty("blob.store.secretKey", MINIO_USER_SECRET);
     }
 
     @AfterAll
