48b30a812cada18ecd45989d8e2a336bc4f65ef4
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2026-01-05 10:41:17 -0800

    Use AutoService to have CdaFeatureManagerProvider register as a FeatureManagerProvider.

--- cwms-data-api/src/main/java/cwms/cda/features/CdaFeatureManagerProvider.java
+++ cwms-data-api/src/main/java/cwms/cda/features/CdaFeatureManagerProvider.java
@@ -1,11 +1,13 @@
 package cwms.cda.features;
 
+import com.google.auto.service.AutoService;
 import org.togglz.core.manager.FeatureManager;
 import org.togglz.core.manager.FeatureManagerBuilder;
 import org.togglz.core.repository.file.FileBasedStateRepository;
 import java.io.File;
 import org.togglz.core.spi.FeatureManagerProvider;
 
+@AutoService(FeatureManagerProvider.class)
 public class CdaFeatureManagerProvider implements FeatureManagerProvider {
     public static final String DEFAULT_PROPERTIES_FILE = "features.properties";
     public static final String PROPERTIES_FILE = "properties.file";
