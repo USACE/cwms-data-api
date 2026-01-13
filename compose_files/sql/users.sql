bf98664a3a9fa0eb8d28e74b418033f8258d7300
Eric Novotny <nov00002@umn.edu>
2026-01-13 11:33:45 -0800

    format changes

--- compose_files/sql/users.sql
+++ compose_files/sql/users.sql
@@ -2,134 +2,38 @@
 define OFFICE_EROC=&1
 defin API_KEY=&2
 begin
-   cwms_sec.add_user_to_group(
-      '&&OFFICE_EROC.webtest',
-      'All Users',
-      'HQ'
-   );
-   cwms_sec.add_user_to_group(
-      '&&OFFICE_EROC.webtest',
-      'All Users',
-      'SPK'
-   );
-   cwms_sec.add_user_to_group(
-      '&&OFFICE_EROC.webtest',
-      'CWMS Users',
-      'HQ'
-   );
-   cwms_sec.add_user_to_group(
-      '&&OFFICE_EROC.webtest',
-      'CWMS User Admins',
-      'HQ'
-   );
-   cwms_sec.add_cwms_user(
-      'l2hectest',
-      null,
-      'SPK'
-   );
-   cwms_sec.update_edipi(
-      'l2hectest',
-      1234567890
-   );
-   cwms_sec.add_user_to_group(
-      'l2hectest',
-      'All Users',
-      'SPK'
-   );
-   cwms_sec.add_user_to_group(
-      'l2hectest',
-      'CWMS Users',
-      'SPK'
-   );
-   cwms_sec.add_user_to_group(
-      'l2hectest',
-      'TS ID Creator',
-      'SPK'
-   );
-   cwms_sec.add_cwms_user(
-      'l1hectest',
-      null,
-      'SPL'
-   );
+   cwms_sec.add_user_to_group('&&OFFICE_EROC.webtest', 'All Users', 'HQ');
+   cwms_sec.add_user_to_group('&&OFFICE_EROC.webtest', 'All Users', 'SPK');
+   cwms_sec.add_user_to_group('&&OFFICE_EROC.webtest', 'CWMS Users', 'HQ');
+   cwms_sec.add_user_to_group('&&OFFICE_EROC.webtest', 'CWMS User Admins', 'HQ');
+   cwms_sec.add_cwms_user('l2hectest', null, 'SPK');
+   cwms_sec.update_edipi('l2hectest', 1234567890);
+   cwms_sec.add_user_to_group('l2hectest', 'All Users', 'SPK');
+   cwms_sec.add_user_to_group('l2hectest', 'CWMS Users', 'SPK');
+   cwms_sec.add_user_to_group('l2hectest', 'TS ID Creator', 'SPK');
+   cwms_sec.add_cwms_user('l1hectest', null, 'SPL');
     -- intentionally no extra permissions.
     --cwms_sec.add_user_to_group('l2hectest','CWMS Users', 'SPL');
 
 
-   cwms_sec.add_cwms_user(
-      'm5hectest',
-      null,
-      'SWT'
-   );
-   cwms_sec.add_user_to_group(
-      'm5hectest',
-      'All Users',
-      'SWT'
-   );
-   cwms_sec.add_user_to_group(
-      'm5hectest',
-      'CWMS Users',
-      'SWT'
-   );
-   cwms_sec.add_cwms_user(
-      'q0hectest',
-      null,
-      'SWT'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'All Users',
-      'SWT'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS Users',
-      'SWT'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS PD Users',
-      'SWT'
-   );
-   cwms_sec.add_cwms_user(
-      'q0hectest',
-      null,
-      'MVP'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'All Users',
-      'MVP'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS Users',
-      'MVP'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS PD Users',
-      'MVP'
-   );
-   cwms_sec.add_cwms_user(
-      'q0hectest',
-      null,
-      'LRL'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'All Users',
-      'LRL'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS Users',
-      'LRL'
-   );
-   cwms_sec.add_user_to_group(
-      'q0hectest',
-      'CWMS PD Users',
-      'LRL'
-   );
+   cwms_sec.add_cwms_user('m5hectest', null, 'SWT');
+   cwms_sec.add_user_to_group('m5hectest', 'All Users', 'SWT');
+   cwms_sec.add_user_to_group('m5hectest', 'CWMS Users', 'SWT');
+   cwms_sec.add_cwms_user('q0hectest', null, 'SWT');
+   cwms_sec.add_user_to_group('q0hectest', 'All Users', 'SWT');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS Users', 'SWT');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS PD Users', 'SWT');
+   cwms_sec.add_user_to_group('q0hectest', 'TS ID Creator', 'SWT');
+   cwms_sec.add_cwms_user('q0hectest', null, 'MVP');
+   cwms_sec.add_user_to_group('q0hectest', 'All Users', 'MVP');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS Users', 'MVP');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS PD Users', 'MVP');
+   cwms_sec.add_user_to_group('q0hectest', 'TS ID Creator', 'MVP');
+   cwms_sec.add_cwms_user('q0hectest', null, 'LRL');
+   cwms_sec.add_user_to_group('q0hectest', 'All Users', 'LRL');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS Users', 'LRL');
+   cwms_sec.add_user_to_group('q0hectest', 'CWMS PD Users', 'LRL');
+   cwms_sec.add_user_to_group('q0hectest', 'TS ID Creator', 'LRL');
    execute immediate 'grant execute on cwms_20.cwms_upass to web_user';
    insert into "CWMS_20"."AT_API_KEYS" (
       userid,
