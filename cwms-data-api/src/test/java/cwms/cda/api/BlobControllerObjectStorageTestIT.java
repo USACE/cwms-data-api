0856dc33a0c9da0a3deb422167244d7b19d082bb
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2026-01-27 10:18:15 -0800

    Moved MinIO into its own extension.

--- cwms-data-api/src/test/java/cwms/cda/api/BlobControllerObjectStorageTestIT.java
+++ cwms-data-api/src/test/java/cwms/cda/api/BlobControllerObjectStorageTestIT.java
@@ -1,6 +1,5 @@
 package cwms.cda.api;
 
-import com.google.common.flogger.FluentLogger;
 import cwms.cda.features.CdaFeatures;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Tag;
@@ -8,82 +7,41 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.containers.MinIOContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.junit.jupiter.Container;
 
 import org.togglz.core.context.FeatureContext;
 import org.togglz.core.manager.FeatureManager;
 
 @Tag("integration")
 @ExtendWith(BlobControllerObjectStorageTestIT.FeatureEnableExtension.class)
-public class BlobControllerObjectStorageTestIT extends BlobControllerTestIT{
-    static FluentLogger logger = FluentLogger.forEnclosingClass();
-
-    public static final String IMAGE_NAME = "minio/minio:RELEASE.2025-04-22T22-12-26Z";
-    public static final int PORT = 9000;
-    public static final String MINIO_USER = "cda_user";
-    public static final String MINIO_USER_SECRET = "cda_password";
-
-    private static final Network NETWORK = Network.newNetwork();
-    @Container
-    private static final MinIOContainer MINIO_CONTAINER = new MinIOContainer(IMAGE_NAME)
-            .withUserName(MINIO_USER)
-            .withPassword(MINIO_USER_SECRET);
-
-    public static final String BUCKET = "cwms-test";
-
-
+public class BlobControllerObjectStorageTestIT extends BlobControllerTestIT {
 
     static class FeatureEnableExtension implements Extension, BeforeAllCallback {
 
         @Override
         public void beforeAll(ExtensionContext context) {
-            if (!MINIO_CONTAINER.isRunning()) {
-                MINIO_CONTAINER.start();
-                createTestBucket();
-            }
 
             setObjectStoreProperties();
-        }
-    }
-
-    private static void createTestBucket() {
-        try (var client = io.minio.MinioClient.builder()
-                .endpoint(MINIO_CONTAINER.getS3URL())
-                .credentials(MINIO_USER, MINIO_USER_SECRET)
-                .build()) {
-            if (!client.bucketExists(io.minio.BucketExistsArgs.builder().bucket(BUCKET).build())) {
-                client.makeBucket(io.minio.MakeBucketArgs.builder().bucket(BUCKET).build());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create test bucket", e);
         }
     }
 
     static boolean wasActive;
 
     private static void setObjectStoreProperties() {
-        // This is to get the feature enabled before we try and use the controller
+        // This test needs the object store feature enabled.
+        //
+        // So we make sure its enabled before the test and then restore the feature to however it was after
         FeatureManager featureManager = FeatureContext.getFeatureManager();
 
-        wasActive=featureManager.isActive(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
+        wasActive = featureManager.isActive(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
         featureManager.enable(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
         featureManager.isActive(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
-
-        String host = MINIO_CONTAINER.getHost();
-        Integer port = MINIO_CONTAINER.getMappedPort(PORT);
-
-        System.setProperty("blob.store.endpoint", "http://" + host + ":" + port);
-        System.setProperty("blob.store.bucket", BUCKET);
-        System.setProperty("blob.store.accessKey", MINIO_USER);
-        System.setProperty("blob.store.secretKey", MINIO_USER_SECRET);
     }
 
     @AfterAll
     public static void teardown() {
+        // restore the object store feature to however it initially was set.
         FeatureManager featureManager = FeatureContext.getFeatureManager();
-        if(wasActive){
+        if (wasActive) {
             featureManager.enable(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
         } else {
             featureManager.disable(CdaFeatures.USE_OBJECT_STORAGE_BLOBS);
