67fa3ec14e7a1d4b93bb2edf5ec97f0d230e5408
Zack Olson <zack@rmanet.com>
2026-02-04 14:33:54 -0800

    CDA-92: Fix to resolve TimeSeriesGroup DAO serializing AssignedTimeSeries nulls (#1566)
    Fix to resolve TimeSeriesGroup DAO serializing AssignedTimeSeries null
    attributes as strings.
    
    Includes integration test and serialization unit test.
    
    Resolves #1564
    
    (cherry picked from commit 576bd7ebdb0d32c49ad87db2a950726eda28d288)

--- cwms-data-api/src/test/java/cwms/cda/data/dto/TimeSeriesGroupTest.java
+++ cwms-data-api/src/test/java/cwms/cda/data/dto/TimeSeriesGroupTest.java
@@ -1,10 +1,13 @@
 package cwms.cda.data.dto;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import cwms.cda.formatters.ContentType;
 import cwms.cda.formatters.Formats;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -30,17 +33,42 @@ class TimeSeriesGroupTest
 		assertTrue(result.contains("grpSharedRefTsId"));
 	}
 
+    @Test
+    void test_serialize_with_nulls() {
+        TimeSeriesGroup group = buildTimeSeriesGroup();
+        List<AssignedTimeSeries> assignedTimeSeries = new ArrayList<>();
+        AssignedTimeSeries timeSeries = new AssignedTimeSeries("SPK",
+            "BIG MUDDY.Elev.Total.1Day.1Day.CWMS", null, null, 0);
+        assignedTimeSeries.add(timeSeries);
+        TimeSeriesGroup groupWithNulls = new TimeSeriesGroup(group, assignedTimeSeries);
+
+        ContentType contentType = Formats.parseHeader(Formats.JSON, TimeSeriesGroup.class);
+        String result = Formats.format(contentType, groupWithNulls);
+        assertNotNull(result);
+
+        assertTrue(result.contains("catOfficeId"));
+        assertTrue(result.contains("catId"));
+        assertTrue(result.contains("catDesc"));
+
+        assertTrue(result.contains("grpOfficeId"));
+        assertTrue(result.contains("grpId"));
+        assertTrue(result.contains("grpDesc"));
+        assertTrue(result.contains("grpSharedTsAliasId"));
+        assertTrue(result.contains("grpSharedRefTsId"));
+
+        assertFalse(result.contains("null"));
+    }
+
 
 	private TimeSeriesGroup buildTimeSeriesGroup()
 	{
 		TimeSeriesCategory category = new TimeSeriesCategory(
 				"catOfficeId", "catId",  "catDesc"
 		);
-		TimeSeriesGroup retval = new TimeSeriesGroup(category,
+
+		return new TimeSeriesGroup(category,
 				"grpOfficeId", "grpId", "grpDesc",
 				"grpSharedTsAliasId", "grpSharedRefTsId"
 				);
-
-		return retval;
 	}
 }
