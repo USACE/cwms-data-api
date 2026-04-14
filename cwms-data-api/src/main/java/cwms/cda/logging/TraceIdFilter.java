f2c33a8df79b8c2927572c96b33d388936e3ddcd
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-04-14 20:02:46 +0000

    Improved Trace ID error handling, with tests. Improve log output.

--- cwms-data-api/src/main/java/cwms/cda/logging/TraceIdFilter.java
+++ cwms-data-api/src/main/java/cwms/cda/logging/TraceIdFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.common.flogger.MetadataKey;
 import com.google.common.flogger.context.ScopedLoggingContexts;
@@ -20,8 +21,11 @@ public class TraceIdFilter implements Filter
 {
     public static final String CONTEXT_TRACE_ID = "traceID";
     public static final String HEADER_TRACE_ID = "X-Trace-ID";
+    public static final String MSG = "Invalid UUID value provided in X-Trace-Id header.";
+    public static final String ERROR_MESSAGE = "{\"message\": \"" + MSG + "\"}";
 
-    public static final Pattern UUID_MATCHER = Pattern.compile("[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}");
+    // accordning to https://www.rfc-editor.org/rfc/rfc4122.html (section 3) UUID string are case insensitive on input.
+    public static final Pattern UUID_MATCHER = Pattern.compile("[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}", Pattern.CASE_INSENSITIVE);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -35,7 +39,16 @@ public class TraceIdFilter implements Filter
             if (xTraceId == null || xTraceId.isBlank()) {
                 traceId = UUID.randomUUID().toString();
             } else {
+                try {
                 traceId = validate(xTraceId); //well that needs some validation.
+                } catch (IOException ex) {
+                    var httpResponse = (HttpServletResponse)response;
+                    httpResponse.setContentType("application/json");
+                    httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    var writer = httpResponse.getWriter();
+                    writer.println(ERROR_MESSAGE);
+                    return;
+                }
             }
 
             request.setAttribute(HEADER_TRACE_ID, traceId);
--- cwms-data-api/src/test/java/cwms/cda/api/BaseLineTestIT.java
+++ cwms-data-api/src/test/java/cwms/cda/api/BaseLineTestIT.java
@@ -1,17 +1,19 @@
 package cwms.cda.api;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import cwms.cda.logging.TraceIdFilter;
 import fixtures.CwmsDataApiSetupCallback;
 import io.restassured.filter.log.LogDetail;
 
@@ -50,5 +52,40 @@ class BaseLineTestIT extends DataApiTestIT {
             .log().ifValidationFails(LogDetail.ALL,true)
             .assertThat()
             .statusCode(is(HttpServletResponse.SC_OK));
+    }
+
+
+    @Test
+    void test_bad_trace_id_value() {
+        given()
+            .log().ifValidationFails(LogDetail.ALL,true)
+            .header("X-Trace-Id", "I'm a bad value")
+        .when()
+            .redirects().follow(true)
+            .redirects().max(3)
+            .options("/levels")
+        .then()
+            .log().ifValidationFails(LogDetail.ALL,true)
+            .assertThat()
+            .statusCode(is(HttpServletResponse.SC_BAD_REQUEST))
+            .body("message", is(TraceIdFilter.MSG));
+    }
+
+
+    @Test
+    void test_provided_uuid_returned_on_error() {
+        final var traceId = UUID.randomUUID().toString();
+        given()
+            .log().ifValidationFails(LogDetail.ALL,true)
+            .header("X-Trace-Id", traceId)
+        .when()
+            .redirects().follow(true)
+            .redirects().max(3)
+            .get("/levels/Bob")
+        .then()
+            .log().ifValidationFails(LogDetail.ALL,true)
+            .assertThat()
+            .statusCode(is(HttpServletResponse.SC_BAD_REQUEST))
+            .body("incidentIdentifier", is(traceId));
     }
 }
