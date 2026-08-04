7662d8b984175f29a2c7a73708a7c1af69ff6ea0
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-08-04 21:59:15 +0000

    Various corrections.

---
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/rating/Ratings.java
@@ -0,0 +1,12 @@
+package cwms.cda.data.dto.rating;
+
+import cwms.cda.data.dto.CwmsDTOBase;
+import cwms.cda.formatters.Formats;
+import cwms.cda.formatters.annotations.FormattableWith;
+import cwms.cda.formatters.xml.XMLv2;
+
+@FormattableWith(contentType = Formats.XMLV2, formatter = XMLv2.class, aliases = {Formats.DEFAULT, Formats.XML})
+@FormattableWith(contentType = Formats.JSONV2, formatter = XMLv2.class, aliases = {Formats.JSON})
+public class Ratings extends CwmsDTOBase {
+    /** marker interface for ContentType Annotations */
+}
