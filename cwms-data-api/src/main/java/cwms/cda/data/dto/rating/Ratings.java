dfe6f327baf41dbb2bcb09bcfc8d676829414de7
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-08-05 20:22:23 +0000

    Use dummy annotation for Ratings Marker.

--- cwms-data-api/src/main/java/cwms/cda/data/dto/rating/Ratings.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/rating/Ratings.java
@@ -2,11 +2,12 @@ package cwms.cda.data.dto.rating;
 
 import cwms.cda.data.dto.CwmsDTOBase;
 import cwms.cda.formatters.Formats;
+import cwms.cda.formatters.OutputFormatter;
 import cwms.cda.formatters.annotations.FormattableWith;
 import cwms.cda.formatters.xml.XMLv2;
 
 @FormattableWith(contentType = Formats.XMLV2, formatter = XMLv2.class, aliases = {Formats.DEFAULT, Formats.XML})
-@FormattableWith(contentType = Formats.JSONV2, formatter = XMLv2.class, aliases = {Formats.JSON})
+@FormattableWith(contentType = Formats.JSONV2, formatter = OutputFormatter.DUMMY.class, aliases = {Formats.JSON})
 public class Ratings extends CwmsDTOBase {
     /** marker interface for ContentType Annotations */
 }
