bbdb6c2825badaaabaf47f91ff4f9802adb627f7
Mike Neilson <michael.a.neilson@usace.army.mil>
2026-08-07 06:55:32 -0700

    Correct format lookup logic. And a few other things (#1862)
    ## Summary
    
    Process the provided header in such a way as to not accidentally attempt
    to use an incorrect content type.
    
    primary fix, always map the primary content type to itself.
    Remove the logic in parseHeader that allow creating an unmapped
    ContentType object.
    Then add appropriate annotations to deal with the fallout of doing that.
    
    ## Related Issue
    
    Closes #1861
    
    ## Validation
    
    Additional test for Formats, existing tests.
    
    ## Checklist
    
    - [ ] AI tools used

--- cwms-data-api/src/main/java/cwms/cda/data/dto/TimeZoneId.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/TimeZoneId.java
@@ -13,13 +13,17 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import cwms.cda.formatters.Formats;
 import cwms.cda.formatters.annotations.FormattableWith;
+import cwms.cda.formatters.json.JsonV1;
 import cwms.cda.formatters.json.JsonV2;
+import cwms.cda.formatters.xml.XMLv1;
 import cwms.cda.formatters.xml.XMLv2;
 import java.time.ZoneId;
 
 @JsonRootName("time-zone")
 @FormattableWith(contentType = Formats.JSONV2, formatter = JsonV2.class, aliases = {Formats.DEFAULT, Formats.JSON})
 @FormattableWith(contentType = Formats.XMLV2, formatter = XMLv2.class, aliases = {Formats.XML})
+@FormattableWith(contentType = Formats.JSONV1, formatter = JsonV1.class)
+@FormattableWith(contentType = Formats.XMLV1, formatter = XMLv1.class)
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
 public final class TimeZoneId extends CwmsDTOBase {
     @JsonProperty(required = true)
