be94f6df581bd7766f9990f87b25e71dc7d82264
Bryson Spilman <bryson@rmanet.com>
2026-09-04 14:36:24 -0700

    CDA-130 - Adds support for partial patches. Implements for text-timeseries. updates open api doc test to check abstractions of accessors for parameters

---
+++ cwms-data-api/src/main/java/cwms/cda/formatters/annotations/Identifier.java
@@ -0,0 +1,11 @@
+package cwms.cda.formatters.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Identifier {
+}
