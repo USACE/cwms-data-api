a1c9ea59a599d1fad73f56ca835d8b0c45eb6670
Bryson Spilman <87150647+rma-bryson@users.noreply.github.com>
2026-09-11 10:15:57 -0700

    CDA-135 - Supports unassignment of tsids from ts group in /v2/ of timeseries group PATCH (#1893)
    ## Summary
    
    Creates a v2 timeseries-group endpoint that support granular assignment
    and unassignment via patch
    
    ## Related Issue
    
    Closes https://github.com/USACE/cwms-data-api/issues/1838
    
    ## Validation
    
    Unit tests and Integration Tests
    
    ## Checklist
    
    - [x] AI tools used

--- cwms-data-api/src/main/java/cwms/cda/data/dto/timeseriesgroup/TimeSeriesGroup.java
+++ cwms-data-api/src/main/java/cwms/cda/data/dto/timeseriesgroup/TimeSeriesGroup.java
@@ -22,12 +22,15 @@
  * SOFTWARE.
  */
 
-package cwms.cda.data.dto;
+package cwms.cda.data.dto.timeseriesgroup;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import cwms.cda.data.dto.AssignedTimeSeries;
+import cwms.cda.data.dto.CwmsDTO;
+import cwms.cda.data.dto.TimeSeriesCategory;
 import cwms.cda.formatters.Formats;
 import cwms.cda.formatters.annotations.FormattableWith;
 import cwms.cda.formatters.json.JsonV1;
