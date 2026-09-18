f7d639477e6de0bda486da2a223051b0da2ef23d
Bryson Spilman <bryson@rmanet.com>
2026-09-18 10:05:52 -0700

    CDA-130 - Updated @Identifier javadoc, and added reverted name of patch adr

--- cwms-data-api/src/main/java/cwms/cda/formatters/annotations/Identifier.java
+++ cwms-data-api/src/main/java/cwms/cda/formatters/annotations/Identifier.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a field as an identifier for the object. This is used to determine which fields are used to uniquely identify an object
+ * when performing operations such as PATCH MERGE on collections. Fields marked with this annotation will be used to match
+ * existing objects in the collection to the incoming data.
+ **/
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Identifier {
